9c2fdaf9abc7997bb724c4b1f7f1fde9c4ff0fa1
Siva Karunakaran <siva.karunakaran@nttdata.com>
2021-03-25 13:38:05 -0700

    Added custom find one controller method

--- src/backend/api/region/controllers/region.js
+++ src/backend/api/region/controllers/region.js
@@ -1,8 +1,15 @@
-'use strict';
+"use strict";
+const { sanitizeEntity } = require("strapi-utils");
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async findOne(ctx) {
+    const { RegionNumber } = ctx.params;
+    const entity = await strapi.services.region.findOne({ RegionNumber });
+    return sanitizeEntity(entity, { model: strapi.models.region });
+  },
+};
