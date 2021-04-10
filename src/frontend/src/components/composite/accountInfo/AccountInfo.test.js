54f39405ffc5ca0083a4a9e185d8037f9298c4e0
Siva Karunakaran <siva.karunakaran@nttdata.com>
2021-04-09 17:48:36 -0700

    Code clean up

--- src/frontend/src/components/composite/accountInfo/AccountInfo.test.js
+++ src/frontend/src/components/composite/accountInfo/AccountInfo.test.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
-import AccountInfo from './AccountInfo';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import AccountInfo from "./AccountInfo";
 
-describe('<AccountInfo />', () => {
-  test('it should mount', () => {
+describe("<AccountInfo />", () => {
+  test("it should mount", () => {
     render(<AccountInfo />);
-    
-    const accountInfo = screen.getByTestId('AccountInfo');
+
+    const accountInfo = screen.getByTestId("AccountInfo");
 
     expect(accountInfo).toBeInTheDocument();
   });
