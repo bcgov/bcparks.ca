4b79128381427d205ca6dc80bc6d0fdf698869e4
Siva Karunakaran <siva.karunakaran@nttdata.com>
2021-04-12 10:17:54 -0700

    Added env config

--- src/frontend/src/keycloak.js
+++ src/frontend/src/keycloak.js
@@ -1,9 +1,9 @@
 import Keycloak from "keycloak-js";
 
 const keycloakConfig = {
-  url: "http://localhost:8080/auth",
-  realm: "g7v0xlf4",
-  clientId: "account",
+  url: process.env.REACT_APP_KEYCLOAK_AUTH_URL,
+  realm: process.env.REACT_APP_KEYCLOAK_REALM,
+  clientId: process.env.REACT_APP_KEYCLOAK_CLIENT_ID,
 };
 
 const keycloak = new Keycloak(keycloakConfig);
