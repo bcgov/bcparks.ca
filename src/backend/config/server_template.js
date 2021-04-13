1368db57568f3e26467529d97ddc5c2fba1378c2
Poggdogg <poggdogg@gmail.com>
2021-04-13 10:05:16 -0600

    Merge?

--- src/backend/config/server_template.js
+++ src/backend/config/server_template.js
@@ -1,9 +1,12 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: '$PUBLIC_URL/api',
   admin: {
     auth: {
+      url: '$PUBLIC_URL/dashboard',
       secret: env('ADMIN_JWT_SECRET', 'cde9090693ee5ce2b00542ee965a60d8'),
     },
   },
 });
+ 
