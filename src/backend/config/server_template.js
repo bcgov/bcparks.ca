d16ad953556eeead2ce80d5a0984d7258e9eed17
Poggdogg <poggdogg@gmail.com>
2021-04-16 13:26:16 -0600

    have to environmentalize some stuff

--- src/backend/config/server_template.js
+++ src/backend/config/server_template.js
@@ -1,11 +1,11 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: '$PUBLIC_URL/api',
+  url: '$REACT_APP_CMS_BASE_URL',
   admin: {
     auth: {
       // url: '$PUBLIC_URL/dashboard',
-      secret: env('ADMIN_JWT_SECRET', 'cde9090693ee5ce2b00542ee965a60d8'),
+      secret: env('ADMIN_JWT_SECRET', '$ADMIN_JWT_SECRET'),
     },
   },
 });
