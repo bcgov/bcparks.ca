8150ef43fc5c03511a80df3ce0373b4dd85eccc7
arbalisi12 <arbalisi12@users.noreply.github.com>
2021-04-20 14:29:58 -0400

    need to reload public advisory too

--- src/backend/config/functions/bootstrap.js
+++ src/backend/config/functions/bootstrap.js
@@ -291,10 +291,10 @@ const loadPublicAdvisory = async () => {
   const modelName = "public-advisory";
   const loadSetting = await getDataLoadSetting(modelName);
 
-  //if (loadSetting && loadSetting.purge)
+  // if (loadSetting && loadSetting.purge)
   await strapi.services["public-advisory"].delete();
 
-  //if (loadSetting && !loadSetting.reload) return;
+  // if (loadSetting && !loadSetting.reload) return;
 
   const currentData = await strapi.services["public-advisory"].find();
   if (currentData.length === 0) {
