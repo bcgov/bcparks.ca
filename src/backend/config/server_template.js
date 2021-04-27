49be9cd56c443ed74c071c7fbac884680ba04b7b
Siva Karunakaran <siva.karunakaran@nttdata.com>
2021-04-27 16:32:05 -0700

    Added publish/unpublish advisory scheduler

--- src/backend/config/server_template.js
+++ src/backend/config/server_template.js
@@ -1,12 +1,14 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  url: '$REACT_APP_CMS_BASE_URL',
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  url: "$REACT_APP_CMS_BASE_URL",
+  cron: {
+    enabled: env.bool("CRON_ENABLED", true),
+  },
   admin: {
     auth: {
       // url: '$PUBLIC_URL/dashboard',
-      secret: env('ADMIN_JWT_SECRET', '$ADMIN_JWT_SECRET'),
+      secret: env("ADMIN_JWT_SECRET", "$ADMIN_JWT_SECRET"),
     },
   },
 });
- 
