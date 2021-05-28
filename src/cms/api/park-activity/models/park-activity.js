e3f9811edef15c0cd3e53faf70c888d4f10006ed
Arnel Balisi <arnel.balisi@gmail.com>
2021-05-28 11:15:34 -0400

    load changes for activity and facility

--- src/cms/api/park-activity/models/park-activity.js
+++ src/cms/api/park-activity/models/park-activity.js
@@ -5,27 +5,42 @@
  * to customize this model
  */
 
+const updateName = async (data) => {
+  data.name = ":";
+  if (data.protectedArea) {
+    const { orcs = { orcs: "" } } = await strapi.services[
+      "protected-area"
+    ].findOne({
+      id: data.protectedArea,
+    });
+    data.name = orcs;
+  }
+  if (data.site) {
+    const { siteNumber = { siteNumber: null } } = await strapi.services[
+      "site"
+    ].findOne({
+      id: data.site,
+    });
+    if (siteNumber) data.name += `-${siteNumber}`;
+  }
+
+  data.name += ":";
+  if (data.activityType) {
+    const { activityName } = await strapi.services["activity-type"].findOne({
+      id: data.activityType,
+    });
+    data.name += activityName;
+  }
+  return data;
+};
+
 module.exports = {
   lifecycles: {
     async beforeCreate(data) {
-      if (typeof data.activityType !== "object") {
-        data.name = "unassigned";
-        if (data.activityType) {
-          const activityType = await strapi.services["activity-type"].findOne({
-            id: data.activityType,
-          });
-          data.name = activityType.activityName;
-        }
-      }
+      data = await updateName(data);
     },
     async beforeUpdate(params, data) {
-      data.name = "unassigned";
-      if (data.activityType) {
-        const activityType = await strapi.services["activity-type"].findOne({
-          id: data.activityType,
-        });
-        data.name = activityType.activityName;
-      }
+      data = await updateName(data);
     },
   },
 };
