ac9a8d746f4da35c9fc963f17a67510ecaca4c33
Arnel Balisi <arnel.balisi@gmail.com>
2021-05-28 14:18:03 -0400

    about page

---
+++ src/admin/src/components/page/about/About.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Header from "../../composite/header/Header";
+import styles from "./About.css";
+
+const About = () => {
+  return (
+    <main>
+      <Header
+        header={{
+          name: "",
+        }}
+      />
+      <div className={styles.Home} data-testid="Home">
+        <div className="container hm-container mt-5">
+          <h3 className="mb-5">BC Parks Staff Portal</h3>
+          <h4 className="m-2 p-0">Version: 0.1.0</h4>
+          <h4 className="m-2 p-0">Last Updated: May 28, 2021</h4>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+About.propTypes = {};
+
+About.defaultProps = {};
+
+export default About;
