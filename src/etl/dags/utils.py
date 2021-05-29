9d1775ff10865e73516e9f1f2eb112a549cb5782
Clement Onawole <dapo.onawole@gmail.com>
2021-05-28 20:32:15 -0600

    par able to persist now with dependencies

--- src/etl/dags/utils.py
+++ src/etl/dags/utils.py
@@ -109,7 +109,6 @@ def _dump_data(task_instance):
 
                     print(f'Record with id: {data["id"]} successfully created!')
                 else:
-                    print(pro_land)
                     print(f'dump data: Unplanned status code returned - {response.status_code}')
             else:
                 print('Protected area already exist in strapi')
@@ -137,6 +136,27 @@ def get_or_create_mgmt_area(mArea):
         newMArea = create_mgmt_area_in_strapi(mArea)
 
     return newMArea
+
+
+def get_or_create_section(section):
+    newSection = get_section_from_strapi(section["sectionNumber"])
+
+    if newSection == None:
+        #create new mgmt area
+        newSection = create_section_in_strapi(section)
+
+    return newSection
+
+
+def get_or_create_region(region):
+    newRegion = get_region_from_strapi(region["regionNumber"])
+
+    if newRegion == None:
+        #create new mgmt area
+        newRegion = create_region_in_strapi(region)
+
+    return newRegion
+
 
 ### Transform
 def transform_par_to_proct_land(pro_land):
@@ -230,9 +250,51 @@ def create_site_in_strapi(site):
         if response.status_code == 200:
             result = response.json()
 
-            print(f'Record with id: {data["id"]} successfully created!')
+            print(f'Record with id: {result["id"]} successfully created!')
         else:
             print(f'create site: Unplanned status code returned - {response.status_code}')
+
+        return result
+
+    except:
+        print('Error invoking webservice')
+        raise
+
+
+def create_region_in_strapi(region):
+    api_url = f"{strapi_base}/sites?token={token}"
+    result = None
+
+    try:
+        response = requests.post(api_url, json=region, headers=headers)
+
+        if response.status_code == 200:
+            result = response.json()
+
+            print(f'Record with id: {result["id"]} successfully created!')
+        else:
+            print(f'create region: Unplanned status code returned - {response.status_code}')
+
+        return result
+
+    except:
+        print('Error invoking webservice')
+        raise
+
+
+def create_section_in_strapi(section):
+    api_url = f"{strapi_base}/sites?token={token}"
+    result = None
+
+    try:
+        response = requests.post(api_url, json=section, headers=headers)
+
+        if response.status_code == 200:
+            result = response.json()
+
+            print(f'Record with id: {result["id"]} successfully created!')
+        else:
+            print(f'create section: Unplanned status code returned - {response.status_code}')
 
         return result
 
@@ -245,12 +307,19 @@ def create_mgmt_area_in_strapi(mArea):
     result = None
 
     try:
+        # handle depencies - region, sections
+        if 'region' in mArea:
+            mArea["region"] = get_or_create_region(mArea["region"])
+
+        if 'section' in mArea:
+            mArea["section"] = get_or_create_section(mArea["section"])
+
         response = requests.post(api_url, json=mArea, headers=headers)
 
         if response.status_code == 200:
             result = response.json()
 
-            print(f'Record with id: {data["id"]} successfully created!')
+            print(f'Record with id: {result["id"]} successfully created!')
         else:
             print(f'create mgmt: Unplanned status code returned - {response.status_code}')
 
@@ -312,12 +381,50 @@ def get_mgmt_area_from_strapi(mAreaNumber):
             else:
                 return data[0]
         else:
-            print(f'url: ==== {api_url}')
             print(f'Unable to get mgmt area with code {response.status_code}')
     except:
         print(f'Error invoking webservice - {api_url}')
         raise
 
+
+def get_region_from_strapi(regionNumber):
+    api_url = f"{strapi_base}/regions?regionNumber={regionNumber}"
+    
+    try:
+        response = requests.get(api_url, headers=headers)
+        
+        if response.status_code == 200:
+            data = response.json()
+
+            if len(data) == 0:
+                return None
+            else:
+                return data[0]
+        else:
+            print(f'Unable to region with code {response.status_code}')
+    except:
+        print(f'Error invoking webservice - {api_url}')
+        raise
+
+
+def get_section_from_strapi(sectionNumber):
+    api_url = f"{strapi_base}/sections?sectionNumber={sectionNumber}"
+    
+    try:
+        response = requests.get(api_url, headers=headers)
+        
+        if response.status_code == 200:
+            data = response.json()
+
+            if len(data) == 0:
+                return None
+            else:
+                return data[0]
+        else:
+            print(f'Unable to get section with code {response.status_code}')
+    except:
+        print(f'Error invoking webservice - {api_url}')
+        raise
 
 ### misc
 def clean_data():
