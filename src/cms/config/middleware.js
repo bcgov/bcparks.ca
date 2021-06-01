ac934ccc0fde848d71bc59dd890213e01af83eac
Siva Karunakaran <siva.karunakaran@nttdata.com>
2021-06-01 16:45:02 -0700

    Added cors settings and minor fixes

--- src/cms/config/middleware.js
+++ src/cms/config/middleware.js
@@ -1,8 +1,12 @@
 module.exports = {
   settings: {
     cors: {
-      origin: ["*"],
-      headers: ["*"],
+      origin: [
+        "http://localhost:3000",
+        "https://web-61d198-dev.apps.silver.devops.gov.bc.ca",
+        "https://web-61d198-test.apps.silver.devops.gov.bc.ca",
+        "https://web-61d198-tools.apps.silver.devops.gov.bc.ca",
+      ],
     },
   },
 };
