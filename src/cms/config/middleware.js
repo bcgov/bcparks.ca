8f9460f11215c98a363a76353eb8c1aa196e0f06
Siva Karunakaran <siva.karunakaran@nttdata.com>
2021-06-02 09:15:17 -0700

    Added cors settings

--- src/cms/config/middleware.js
+++ src/cms/config/middleware.js
@@ -6,6 +6,7 @@ module.exports = {
         "https://web-61d198-dev.apps.silver.devops.gov.bc.ca",
         "https://web-61d198-test.apps.silver.devops.gov.bc.ca",
         "https://web-61d198-tools.apps.silver.devops.gov.bc.ca",
+        "https://bcparks.ca/",
       ],
     },
   },
