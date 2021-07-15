6f5d303a186141556872182cbcb9186bd0f80f7e
nileshnad <nilesh.nadkarni@gmail.com>
2021-07-15 10:32:42 -0700

    Additional Updates to resolve conflicts

--- src/cms/api/park-activity/models/park-activity.js
+++ src/cms/api/park-activity/models/park-activity.js
@@ -1,8 +1,46 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#lifecycle-hooks)
  * to customize this model
  */
 
-module.exports = {};
+const updateName = async (data) => {
+  data.name = ":";
+  if (data.protectedArea) {
+    const { orcs = { orcs: "" } } = await strapi.services[
+      "protected-area"
+    ].findOne({
+      id: data.protectedArea,
+    });
+    data.name = orcs;
+  }
+  if (data.site) {
+    const { siteNumber = { siteNumber: null } } = await strapi.services[
+      "site"
+    ].findOne({
+      id: data.site,
+    });
+    if (siteNumber) data.name += `-${siteNumber}`;
+  }
+
+  data.name += ":";
+  if (data.activityType) {
+    const { activityName } = await strapi.services["activity-type"].findOne({
+      id: data.activityType,
+    });
+    data.name += activityName;
+  }
+  return data;
+};
+
+module.exports = {
+  lifecycles: {
+    async beforeCreate(data) {
+      data = await updateName(data);
+    },
+    async beforeUpdate(params, data) {
+      data = await updateName(data);
+    },
+  },
+};
