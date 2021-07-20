48052c8e391a7f74df4131e4147adc47ad4940df
Siva Karunakaran <60021584+siva-ka@users.noreply.github.com>
2021-07-19 18:15:42 -0700

    Revert "Added provider for local media storage, strapi version update"

--- src/cms/config/plugins.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  upload: {
-    provider: "local",
-    providerOptions: {
-      path: env("MEDIA_FILE_PATH") || "media",
-      sizeLimit: 1000000,
-    },
-  },
-});
