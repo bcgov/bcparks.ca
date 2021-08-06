df46b83627ff4b655602c70d9a28fd8934ad76ca
Siva Karunakaran <siva.karunakaran@nttdata.com>
2021-08-05 17:13:24 -0700

    Add standard message to summary

--- src/admin/src/components/composite/advisorySummaryView/AdvisorySummaryView.js
+++ src/admin/src/components/composite/advisorySummaryView/AdvisorySummaryView.js
@@ -69,6 +69,18 @@ export default function AdvisorySummaryView({
           <div className="col-lg-8 col-md-6 col-12">{advisory.description}</div>
         </div>
       )}
+      {advisory.standardMessages && advisory.standardMessages.length > 0 && (
+        <div className="row">
+          <div className="col-lg-4 col-md-6 col-12 ad-label">
+            Standard Messages
+          </div>
+          <div className="col-lg-8 col-md-6 col-12">
+            {advisory.standardMessages.map((m, index) => (
+              <div key={index}>{m.description}</div>
+            ))}
+          </div>
+        </div>
+      )}
       <div className="row">
         <div className="col-lg-4 col-md-6 col-12 ad-label">
           Associated Park(s)
