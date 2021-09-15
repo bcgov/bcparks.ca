3aeea19aa2ccda57005bbe1a43aaeb035d8959eb
BcGovNeal <47604552+BcGovNeal@users.noreply.github.com>
2021-09-15 09:08:29 -0700

    Restore CORS

--- src/cms/config/middleware.js
+++ src/cms/config/middleware.js
@@ -1,6 +1,16 @@
 module.exports = {
   settings: {
-    // Allow CORS on all origin.  CMS API will not just be for BCParks use
-    cors: { enabled: true, headers: "*" },
+    cors: {
+      origin: [
+        "http://localhost:3000",
+        "http://localhost:1337",
+        "https://*-61d198-dev.apps.silver.devops.gov.bc.ca",
+        "https://*-61d198-test.apps.silver.devops.gov.bc.ca",
+        "https://*-61d198-prod.apps.silver.devops.gov.bc.ca",
+        "https://*.bcparks.ca",
+        "https://bcparks.ca",
+        "*",
+      ],
+    },
   },
 };
