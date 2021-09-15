5f088c93eaeb2124b398b65f3347f517e1125b5d
BcGovNeal <47604552+BcGovNeal@users.noreply.github.com>
2021-09-15 12:02:46 -0700

    BC-487 DevOps Update (CMS) (#163)
    * Increase CMS image reusability and update build
    
    * Add CMS and postgres helm charts
    
    * Use git hash for image and deploy
    
    * Fix staging packge.json
    
    * Restore CORS
    
    * Add sqlite con, add default env values in code

--- src/cms/config/database.js
+++ src/cms/config/database.js
@@ -1,22 +1,33 @@
 module.exports = ({ env }) => ({
-  defaultConnection: "default",
+  defaultConnection: env("DATABASE_CLIENT", "default"),
   connections: {
     default: {
       connector: "bookshelf",
       settings: {
+        client: "sqlite",
+        timezone: "UTC",
+        filename: env("DATABASE_FILENAME", ".tmp/data.db"),
+      },
+      options: {
+        useNullAsDefault: true,
+      },
+    },
+    postgres: {
+      connector: "bookshelf",
+      settings: {
         client: "postgres",
-        host: env("DATABASE_HOST"),
-        port: env.int("DATABASE_PORT"),
-        database: env("DATABASE_NAME"),
-        username: env("DATABASE_USERNAME"),
-        password: env("DATABASE_PASSWORD"),
-        ssl: env.bool("DATABASE_SSL"),
+        host: env("DATABASE_HOST", "localhost"),
+        port: env.int("DATABASE_PORT", 5432),
+        database: env("DATABASE_NAME", "cms"),
+        username: env("DATABASE_USERNAME", "postgres"),
+        password: env("DATABASE_PASSWORD", "postgres"),
+        ssl: env.bool("DATABASE_SSL", false),
         timezone: "UTC",
       },
       options: {
         pool: {
-          min: 0,
-          max: 95,
+          min: env.int("DATABASE_MIN_CONNECTIONS", 2),
+          max: env.int("DATABASE_MAX_CONNECTIONS", 10),
           idleTimeoutMillis: 30000,
           createTimeoutMillis: 30000,
           acquireTimeoutMillis: 30000,
