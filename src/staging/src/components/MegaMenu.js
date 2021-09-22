5053cd2ae0e2160cc3768c7dfbc93dd66616592e
Ben Harrington <benjamin.w.harrington@gmail.com>
2021-09-22 08:58:26 -0700

    Revert "Splitting out mobile/desktop versions of menu."
    This reverts commit eed97431b6caf0ce799143f16e384ea2d4abc0b7.

--- src/staging/src/components/MegaMenu.js
+++ src/staging/src/components/MegaMenu.js
@@ -1,21 +1,102 @@
 import React from "react"
 import PropTypes from "prop-types"
-import "../styles/megaMenu/desktop.scss"
-import "../styles/megaMenu/mobile.scss"
-import MobileMegaMenu from "../components/megaMenu/mobileMenu.js"
-import DesktopMegaMenu from "../components/megaMenu/desktopMenu.js"
+import { Link } from "gatsby"
+import "../styles/megamenu.scss"
 
 const MegaMenu = ({ content }) => {
+  const formattedContent = formatMenuTree(content)
+
   return (
-    <div className="nav-container">
-      <div className="nav d-none d-lg-block">
-        <DesktopMegaMenu linkStructure={content} />
-      </div>
-      <div className="nav d-block d-lg-none">
-        <MobileMegaMenu linkStructure={content} />
-      </div>
+    <div className="nav">
+      <nav id="main-nav">
+        <ul className="menu menu-bar">
+          {formattedContent.map(fc => {
+            return generateMenuItem(fc)
+          })}
+        </ul>
+      </nav>
     </div>
   )
+}
+
+function generateMenuItem({ strapiChildren, title, url }) {
+  if (!strapiChildren?.length) {
+    return (
+      <>
+      <li key={title}>
+          <Link to={url ?? '/'} className="menu-link menu-bar-link">{title}</Link>
+      </li>
+      <li className="empty"></li>
+      </>
+    )
+  }
+  if (strapiChildren?.length) {
+    return (
+      <li key={title}>
+        <Link to={url ?? '/'} className="menu-link menu-bar-link" aria-haspopup="true">{title}</Link>
+        <ul className="mega-menu mega-menu--multiLevel"> {/** level 1 */}
+          {strapiChildren.map(c => {
+            let listElement
+            if (!c?.strapiChildren?.length) {
+              listElement = (
+                <li key={`c${c.title}`} className="mega-menu-list">
+                  <Link to={c.url ?? '/'} className="menu-link menu-list-link mega-menu-link">{c.title}</Link>
+                </li>
+              )
+            } else {
+              listElement = (
+                <li key={`c${c.title}`} className="mega-menu-list">
+                  <Link to={c.url ?? '/'} className="menu-link mega-menu-link" aria-haspopup="true">{c.title}</Link>
+                  <ul className="menu menu-list sub-menu-list"> {/** level 2 */}
+                    <li key={`sc${c.title}`} className="sub-menu-list-header mega-menu-list">
+                      <Link to={c.url ?? '/'} className="sub-menu-link">{c.title}</Link>  
+                    </li>
+                    {c.strapiChildren.map(sc => {
+                      return (
+                        <li key={`sc${sc.title}`} className="mega-menu-list">
+                          <Link to={sc.url ?? '/'} className="sub-menu-link">{sc.title}</Link>  
+                        </li>
+                      )
+                    })}
+                  </ul>
+                </li>
+              )
+            }
+            return listElement
+          })}
+        </ul>
+      </li>
+    )
+  }
+}
+
+/**
+ * Nest child menus from the second layer of IA down to the fourth. Used in conjuction with generateMenuItem().
+ * @param {Object[]} menu 
+ * @returns {Object[]}
+ */
+function formatMenuTree(menu) {
+  const home = {
+    strapiId: 1,
+    title: 'Home',
+    url: '/home',
+    pageType: 'Unique',
+    strapiChildren: [],
+    strapiParent: {}
+  }
+  const formattedMenuTree = [home, ...menu.filter(i => i?.strapiParent?.title === 'Homepage')]
+  formattedMenuTree.forEach(menuTree => {
+    if (menuTree.strapiChildren.length) {
+      menuTree.strapiChildren.forEach(child => {
+        const childSubMenus = menu.filter(i => i?.strapiParent?.id === child.id).map(c => {
+          delete c.strapiChildren
+          return { ...c }
+        })
+        return child.strapiChildren = childSubMenus
+      })
+    }
+  })
+  return formattedMenuTree
 }
 
 MegaMenu.propTypes = {
