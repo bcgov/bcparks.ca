4a0a0c3701f85742c89a674e4a9a49b895ffa1f1
nileshnad <nilesh.nadkarni@gmail.com>
2021-10-05 11:46:49 -0700

    added S3 provider

--- src/cms/config/plugins.js
+++ src/cms/config/plugins.js
@@ -1,5 +1,17 @@
-module.exports = {
+module.exports = ({ env }) => ({
   graphql: {
     amountLimit: 2500,
   },
-};
+  upload: {
+    provider: 'aws-s3',
+    providerOptions: {
+      accessKeyId: env('S3_ACCESS_KEY_ID'),
+      secretAccessKey: env('S3_ACCESS_SECRET'),
+      endpoint: env('S3_ENDPOINT'),
+      s3ForcePathStyle : true,
+      params: {
+        Bucket: env('S3_BUCKET'),
+      },
+    },
+  },
+});
