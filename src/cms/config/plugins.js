681304b968e92f42fb2722ef4a0eee17fe8c35a0
BcGovNeal <47604552+BcGovNeal@users.noreply.github.com>
2021-10-05 13:57:53 -0700

    Strapi email config (#193)

--- src/cms/config/plugins.js
+++ src/cms/config/plugins.js
@@ -1,5 +1,17 @@
-module.exports = {
+module.exports = ({ env }) => ({
   graphql: {
     amountLimit: 2500,
   },
-};
+  email: {
+    provider: "nodemailer",
+    providerOptions: {
+      host: env("STRAPI_SMTP_HOST", "apps.smtp.gov.bc.ca"),
+      port: env.int("STRAPI_SMTP_PORT", 25),
+      tls: { rejectUnauthorized: false }, // Needed because of OCIO cert issue
+    },
+    settings: {
+      defaultFrom: env("STRAPI_SMTP_FROM", "noreply@gov.bc.ca"),
+      defaultReplyTo: env("STRAPI_SMTP_REPLY_TO", "noreply@gov.bc.ca"),
+    },
+  },
+});
