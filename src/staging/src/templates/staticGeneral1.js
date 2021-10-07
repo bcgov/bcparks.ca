e47519d8375525e1deae5502ceedd9730f2ccabc
Ben Harrington <Benjamin.Harrington@nttdata.com>
2021-10-07 12:06:05 -0700

    Updating template.

--- src/staging/src/templates/staticGeneral1.js
+++ src/staging/src/templates/staticGeneral1.js
@@ -2,7 +2,6 @@ import React from "react"
 import { graphql } from "gatsby"
 import SEO from "../components/seo"
 import Header from "../components/header"
-import Menu from "../components/menu"
 import Footer from "../components/footer"
 import Zone from "../components/zone"
 import { Container, CssBaseline } from "@material-ui/core"
@@ -11,21 +10,21 @@ import "./staticGeneral1.scss"
 export default function StaticGeneral1Template({ data, pageContext }) {
   const zonesContent = pageContext?.page?.Content?.filter(c => Boolean(c.HTML))
   const meta = pageContext?.page?.Content.find(c => Boolean(c.metaTitle)) || {}
+  const menuContent = data?.allStrapiMenus?.nodes || []
 
   return (
     <>
       <SEO title={meta.metaTitle} description={meta.description} keywords={meta.metaKeywords} />
       <CssBaseline />
-      <Container id="content" maxWidth={false} disableGutters>
-        <Header>
-          {data.strapiWebsites.Header}
-        </Header>
-        <Menu>
-          {data.strapiWebsites.Navigation}
-        </Menu>
+      <Container id="content" className="max-width-override" fixed disableGutters>
+        <Header mode="internal" content={menuContent} />
+      </Container>
+      <Container className="content-width-override" fixed disableGutters>
         <div id="main">
           {zonesContent.map(content => <Zone key={content.id} zoneID={`Zone${content.id}`} Content={content} />)}
         </div>
+      </Container>
+      <Container className="max-width-override" fixed disableGutters>
         <Footer>
           {data.strapiWebsites.Footer}
         </Footer>
@@ -52,13 +51,27 @@ export const query = graphql`
         }
       }
     }
-    allStrapiPages(filter: {Template: {eq: "StaticGeneral1"} }) {
-      totalCount
+    allStrapiMenus(
+      sort: { fields: order, order: ASC }
+      filter: { show: { eq: true } }
+    ) {
       nodes {
+        strapiId
+        title
+        url
+        order
         id
-        Slug
-        Template
-        Content
+        strapiChildren {
+          id
+          title
+          url
+          order
+          parent
+        }
+        strapiParent {
+          id
+          title
+        }
       }
     }
   }
