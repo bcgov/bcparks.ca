ec4d8c9014532f5e23f5f96298f562556ad34a62
Cameron Pettit <71421099+cameronpettit@users.noreply.github.com>
2022-01-24 13:22:20 -0800

    BRS-320 add 404 page (#243)

--- src/staging/src/pages/404.js
+++ src/staging/src/pages/404.js
@@ -1,18 +1,38 @@
 import * as React from "react"
-import { Link } from "gatsby"
-import Layout from "../components/layout"
+import { Link, navigate } from "gatsby"
 import Seo from "../components/seo"
+import BCParksLogo from "../images/bcparks-h-rgb-rev.png"
+import { Button } from "@material-ui/core"
+
+import "../styles/404.scss"
+
+function goHome() {
+  navigate('/');
+}
 
 const NotFoundPage = () => (
-  <Layout>
+  <>
     <Seo title="404: Not found" />
-    <br />
-    <h3>Sorry, this page isn't available</h3>
-    <p>
-      The link you followed may be broken, unavailable or have been removed.
-    </p>
-    <Link to="/">Go back to homepage</Link>
-  </Layout>
+    <div className="not-found">
+      <nav className="navbar navbar-dark" id="desktopNav">
+        <Link to="/">
+          <img className="bc-parks-logo" src={BCParksLogo} alt="BC Parks logo" />
+        </Link>
+      </nav>
+      <div className="not-found-container d-flex align-items-center justify-content-center">
+        <div className="p-3 text-center">
+          <h1>The page you are looking for is coming soon.</h1>
+          <p>The link you followed may be broken, or the page is being developed. Come back soon.</p>
+          <Button variant="contained"
+            className="home-button px-5 py-2"
+            onClick={() => goHome()}>
+            Back Home
+          </Button>
+        </div>
+      </div>
+    </div>
+  </>
 )
 
 export default NotFoundPage
+
