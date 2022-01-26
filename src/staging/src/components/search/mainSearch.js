6c9236d9589b198d9841b13a68896b22de2b7234
DayKirby-OXD <92060158+DayKirby-OXD@users.noreply.github.com>
2022-01-26 10:36:08 -0800

    BRS-313 Update homepage (#247)
    * BRS-313 WIP homepage card layout
    
    * BRS-313 WIP completed test cards
    
    * BRS-313 WIP converted test cards to html
    
    * BRS-313 WIP working html/css direct in home
    
    * BRS-313 added in-code images
    
    * BRS-313 WIP refactor - cards+search more modular
    
    * BRS-313 streamlined mainsearch component
    
    * BRS-313 addressing pr review

--- src/staging/src/components/search/mainSearch.js
+++ src/staging/src/components/search/mainSearch.js
@@ -1,228 +1,70 @@
 import React, { useState } from "react"
 import PropTypes from "prop-types"
-import { TextField, Button, Link, InputAdornment } from "@material-ui/core"
+import { TextField, Button, InputAdornment, InputLabel } from "@material-ui/core"
 import "../../styles/search.scss"
 import { navigate } from "gatsby"
-import SearchFilter from "./searchFilter"
+import SearchIcon from "@material-ui/icons/Search"
 
-const MainSearch = ({ data: { activities, facilities } }) => {
-  const activityItems = activities.map(a => ({
-    label: a.activityName,
-    value: a.activityNumber,
-  }))
+  
+const MainSearch = () => {
 
-  const facilityItems = facilities.map(f => ({
-    label: f.facilityName,
-    value: f.facilityNumber,
-  }))
-  const [openFilter, setOpenFilter] = useState(false)
-  const [quickSearch, setQuickSearch] = useState({
-    camping: false,
-    petFriendly: false,
-    wheelchair: false,
-    marine: false,
-    ecoReserve: false,
-    electricalHookup: false,
-  })
-  const [selectedActivities, setSelectedActivities] = useState([])
-  const [selectedFacilities, setSelectedFacilities] = useState([])
   const [searchText, setSearchText] = useState("")
-
-  const sortOptions = [
-    { value: "rel", label: "Sort by Relevance" },
-    { value: "asc", label: "Sort A-Z" },
-    { value: "desc", label: "Sort Z-A" },
-  ]
-
-  const [sortOption, setSortOption] = useState(sortOptions[0])
-
-  const handleClickOpenFilter = () => {
-    setOpenFilter(true)
-  }
 
   const searchParkFilter = () => {
     navigate("/explore", {
       state: {
-        quickSearch,
-        selectedActivities,
-        selectedFacilities,
-        searchText,
-        sortOption,
+        searchText
       },
     })
   }
 
-  const renderDesktop = () => {
-    return (
-      <div className="park-search-container-inner row align-items-center w-100 no-gutters">
-        <div className="col-12">
-          <div className="row no-gutters">
-            <div className="col-0">
-            </div>
-            <div className="col ml-auto park-search-container-inner-align">
-            <div className="col-12 park-search-intro text-center text-sm-left">
-              <h2 className="heading-white-space">Welcome to BC Parks</h2>
-              <p className="pt-sm-3">
-                Plan your next adventure by searching for campsites and day-use
-                areas around B.C.
-              </p>
-              <TextField
-                id="park-search-text"
-                variant="outlined"
-                placeholder="Search by park name, location, activity..."
-                className="park-search-text-box pr-2"
-                value={searchText}
-                onChange={event => {
-                  setSearchText(event.target.value)
-                }}
-                onKeyPress={ev => {
-                  if (ev.key === "Enter") {
-                    searchParkFilter()
-                    ev.preventDefault()
-                  }
-                }}
-              />
-              <Button
-                  variant="contained"
-                  onClick={searchParkFilter}
-                  className="bcgov-normal-gold mobile-search-element-height"
-              >
-                Search
-              </Button>
-            </div>
-            </div>
-          </div>
-          <div className="row no-gutters"></div>
-          <div className="row no-gutters">
-          <div className="col-0">
-            </div>
-            <div className="col ml-auto park-search-container-inner-align">
-          <div className="col-12 pl-sm-0 pt-sm-3">
-            <Link
-              component="button"
-              className="park-search-filter"
-              onClick={handleClickOpenFilter}
-            >
-              Filters
-            </Link>
-          </div>
-          </div>
-          </div>
-        </div>
-      </div>
-    )
-  }
-
-  const renderMobile = () => {
-    return (
-      <div className="row align-items-center w-100 no-gutters park-search-group">
-        <div className="col-12">
-          <div className="row no-gutters px-3">
-            <div className="col-12 text-center">
-              <h2 className="heading-white-space">Plan your next adventure</h2>
-            </div>
-          </div>
-          <div className="row no-gutters pb-2 px-3">
-            <div className="col-9 pr-1">
-              <TextField
-                  id="park-search-text"
-                  variant="outlined"
-                  placeholder="Search by park name, location, activity..."
-                  className="park-search-text-box mobile-search-element-height"
-                  InputProps={{
-                    startAdornment: (
-                      <InputAdornment position="start">
-                        <i className="fas fa-search"></i>
-                      </InputAdornment>
-                    )
-                  }}
-                  value={searchText}
-                  onChange={event => {
-                    setSearchText(event.target.value)
-                  }}
-                  onKeyPress={ev => {
-                    if (ev.key === "Enter") {
-                      searchParkFilter()
-                      ev.preventDefault()
-                    }
-                  }}
-                />
-            </div>
-            <div className="col-3">
-              <Button
-                  variant="outlined"
-                  onClick={handleClickOpenFilter}
-                  className="bg-transparent rounded text-light mobile-filter mobile-search-element-height"
-              >
-                Filters
-              </Button>
-            </div>
-          </div>
-          <div className="row no-gutters pb-2 px-3">
-            <div className="col-12">
-              <Button
-                  variant="contained"
-                  fullWidth
-                  onClick={() => {
-                    handleClickOpenFilter()
-                    searchParkFilter()
-                  }}
-                  className="bcgov-normal-blue mobile-search-element-height"
-              >
-                Search
-              </Button>
-            </div>
-          </div>
-          <div className="row no-gutters px-3">
-            <div className="col-12">
-              <Button
-                  variant="contained"
-                  href="https://www.discovercamping.ca/"
-                  target="_blank"
-                  rel="norefferer"
-                  fullWidth
-                  onClick={() => {
-                    handleClickOpenFilter()
-                    searchParkFilter()
-                  }}
-                  className="bcgov-normal-gold mobile-search-element-height"
-              >
-                Book a campsite
-              </Button>
-            </div>
-          </div>
-        </div>
-      </div>
-    )
-  }
-
   return (
-    <div className="park-search-container park-search-text-container">
-      <div className="d-none d-lg-block v-align-abs">
-        {renderDesktop()}
+    <div className="parks-search-wrapper">
+      <div className="parks-search-header">
+        Find your next adventure
       </div>
-      <div className="d-block d-lg-none v-align-abs">
-        {renderMobile()}
+      <div className="parks-search-field">
+        <InputLabel className="sr-only" htmlFor="park-search-text">
+            Search
+        </InputLabel>
+        <TextField
+          id="park-search-text"
+          variant="outlined"
+          placeholder="Plan your next adventure by searching for campsites and day use areas"
+          className="park-search-text-box h50p"
+          value={searchText}
+          onChange={event => {
+            setSearchText(event.target.value)
+          }}
+          onKeyPress={ev => {
+            if (ev.key === "Enter") {
+              searchParkFilter()
+              ev.preventDefault()
+            }
+          }}
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <SearchIcon className="search-icon" />
+              </InputAdornment>
+            ),
+          }}
+        />
+        <Button
+            variant="contained"
+            onClick={searchParkFilter}
+            className="parks-search-button"
+        >
+          Search
+        </Button>
       </div>
-      <SearchFilter
-        data={{
-          activityItems,
-          facilityItems,
-          openFilter,
-          setOpenFilter,
-          quickSearch,
-          setQuickSearch,
-          selectedActivities,
-          setSelectedActivities,
-          selectedFacilities,
-          setSelectedFacilities,
-          searchText,
-          setSearchText,
-          sortOption,
-          setSortOption,
-          sortOptions,
-        }}
-      />
+      <div className="parks-search-filter-link"
+        role="button"
+        tabIndex={0}
+        onKeyDown={searchParkFilter}
+        onClick={searchParkFilter}>
+        Search by activity
+      </div>
     </div>
   )
 }
--- src/staging/src/pages/index.js
+++ src/staging/src/pages/index.js
@@ -7,7 +7,20 @@ import MainSearch from "../components/search/mainSearch"
 import { Container } from "@material-ui/core"
 import { useMediaQuery } from "react-responsive"
 
+// Temporary in-code images
+import imgSearchBg from "../images/home/image006.png"
+import imgCampingInfo from "../images/home/image012.png"
+import imgThingsToDo from "../images/home/image013.png"
+import imgAccessibility from "../images/home/image011.png"
+import imgVisitResponsibly from "../images/home/image014.png"
+import imgReconciliation from "../images/home/image007.png"
+import imgWildlife from "../images/home/image010.png"
+import imgConservation from "../images/home/image003.png"
+import imgGetToKnow from "../images/home/image005.png"
+import imgGetInvolved from "../images/home/image004.png"
+
 import "../styles/home.scss"
+import "../styles/gridCard.scss"
 
 export const query = graphql`
   query {
@@ -109,28 +122,233 @@ export default function Home({ data }) {
   const menuContent = data?.allStrapiMenus?.nodes || []
   const isMobile = useMediaQuery({ query: '(max-width: 414px)' })
 
+  const isLiveContent = false;
+
   return (
     <>
-      <Container className="park-search-container-wrapper max-width-override" fixed disableGutters>
-        <Header mode="internal" content={menuContent} />
-        <div className="park-search">
-          <MainSearch
-              data={{
-                activities: data.allStrapiActivityTypes.nodes,
-                facilities: data.allStrapiFacilityTypes.nodes,
-                protectedAreas: data.allStrapiProtectedArea.nodes,
-              }}
-            />
-          <div className="park-search-carousel">
-            <Zone key={6} Content={searchCarousel} />
-          </div>
-        </div>
-      </Container>
-      <Container className="content-width-override" fixed disableGutters={isMobile ? true: false}>
-        <div id="main">
-          {zonesContent.map(content => <Zone key={content.id} zoneID={`Zone${content.id}`} Content={content} />)}
-        </div>
-      </Container>
+      { isLiveContent ? (
+        <>
+          <Container className="park-search-container-wrapper max-width-override" fixed disableGutters>
+            <Header mode="internal" content={menuContent} />
+            <div className="park-search">
+              <MainSearch
+                  data={{
+                    activities: data.allStrapiActivityTypes.nodes,
+                    facilities: data.allStrapiFacilityTypes.nodes,
+                    protectedAreas: data.allStrapiProtectedArea.nodes,
+                  }}
+                />
+              <div className="park-search-carousel">
+                <Zone key={6} Content={searchCarousel}  />
+                <div className="col-12 d-none d-lg-block text-center text-white" id="carousel-down"><i className="fa fa-chevron-down"></i></div>
+              </div>
+            </div>
+          </Container>
+            <Container className="content-width-override" fixed disableGutters={isMobile ? true: false}>
+              <div id="main">
+                {zonesContent.map(content => <Zone key={content.id} zoneID={`Zone${content.id}`} Content={content} />)}
+              </div>
+            </Container>
+          </>
+      ) : (
+        <>
+          <Container className="park-search-container-wrapper max-width-override" fixed disableGutters>
+            <Header mode="internal" content={menuContent} />
+              <div className="park-search">
+                <div id="home-parks-search">
+                  <MainSearch />
+                </div>
+              <div className="park-search-carousel">
+                <img src={imgSearchBg} alt="Mount Robson Park" />
+              </div> 
+            </div>
+          </Container>
+          <Container className="home-content-width-override" fixed disableGutters={isMobile ? true : false}>
+            <div>
+              <div>
+                <div className="home-advisories text-center" id="home-advisories">
+                  <h2 className="zone-header">Advisories</h2>
+                  <div className="zone-hr"><hr/></div>
+                  <p>
+                  Updated Monday to Friday from 8:30 am to 4:30 pm, excluding statutory holidays.
+                  </p>
+                  <div className="home-advisory-buttons row no-gutters">
+                    <div className="home-advisory-button col-12 col-lg-4 pr-sm-2">
+                      <a href="/alerts?type=floods" className="btn btn-primary btn-block">
+                        <span className="text-white">Parks impacted by <span className="font-weight-bold">flooding</span>
+                        <i className="fa fa-chevron-circle-right"></i>
+                        </span>
+                      </a>
+                    </div>
+                    <div className="home-advisory-button col-12 col-lg-4 pr-sm-2">
+                      <a className="btn btn-primary btn-block" href="/alerts?type=wildfires">
+                        <span className="text-white">Parks impacted by <span className="font-weight-bold">wildfires</span> <span className="fa fa-chevron-circle-right"> </span>
+                        </span>
+                      </a>
+                    </div>
+                    <div className="home-advisory-button home-advisory-button--all col-12 col-lg-3 pr-sm-2">
+                      <a className="btn btn-light btn-block" href="/alerts">
+                        <span className="text-primary font-weight-bold">See all advisories </span>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Container>
+          <Container className="home-content-width-override" fixed disableGutters={isMobile ? true: false}>
+            <div id="main">
+              <div className="text-center" id="home-plan-your-trip">
+                <h2 className="zone-header">Plan your trip</h2>
+                <div className="zone-hr"><hr/></div>
+                <p className="zone-content">
+                  <span>There's an adventure waiting for every visitor.</span>
+                </p>
+              </div>
+              <div className="row">
+                <div className="col-12 col-md-8 home-card-wrapper">
+                  <a href="/reserve"className="card grid-card">
+                    <div className="card-img">
+                      <img src={imgCampingInfo} alt="People setting up a tent" />
+                    </div>
+                    <div className="card-body">
+                      <h5 className="card-body-header">
+                        Camping information
+                      </h5>
+                      <p className="card-text">
+                      Reservation policies and fees.
+                      </p>
+                    </div>
+                  </a>
+                </div>
+                <div className="col-12 col-md-4 home-card-wrapper">
+                  <a href="/visiting" className="card grid-card">
+                    <div className="card-img">
+                      <img src={imgThingsToDo} alt="People with binoculars" />
+                    </div>
+                    <div className="card-body">
+                      <h5 className="card-body-header">
+                      Things to do
+                      </h5>
+                      <p className="card-text">
+                      Explore activities and attractions.
+                      </p>
+                    </div>
+                  </a>
+                </div>
+                <div className="col-12 col-md-4 home-card-wrapper">
+                  <a href="/accessibility" className="card grid-card">
+                    <div className="card-img">
+                      <img src={imgAccessibility} alt="A child in a wheelchair" />    
+                    </div>
+                    <div className="card-body">
+                      <h5 className="card-body-header">
+                        Accessibility
+                      </h5>
+                      <p className="card-text">
+                      BC Parks for everyone.
+                      </p>
+                    </div>
+                  </a>
+                </div>
+                <div className="col-12 col-md-8 home-card-wrapper">
+                  <a href="/responsible-recreation" className="card grid-card">
+                    <div className="card-img">
+                      <img src={imgVisitResponsibly} alt="Cleaning up after a dog" />    
+                    </div>
+                    <div className="card-body">
+                      <h5 className="card-body-header">
+                        Visit Responsibly
+                      </h5>
+                      <p className="card-text">
+                      Guideline for a safe and respectful adventure.
+                      </p>
+                    </div>
+                  </a>
+                </div>
+              </div>
+              <div className="home-desktop-card-spacer">&nbsp;</div>
+              <div className="row">
+                <div className="col-12 home-card-wrapper">
+                  <a href="/reconciliation" className="card grid-card grid-card--horz">
+                    <div className="card-img">
+                      <img src={imgReconciliation} alt="A wood carving" />    
+                    </div>
+                    <div className="card-body">
+                      <h5 className="card-body-header">
+                      Indigenous Relations and Reconciliation
+                      </h5>
+                      <div className="card-button">
+                      Learn more <i className="fa fa-chevron-circle-right"></i>
+                      </div>
+                    </div>
+                  </a>
+                </div>
+                <div className="col-12 home-card-wrapper">
+                  <a href="/wildlife-safety" className="card grid-card grid-card--horz">
+                    <div className="card-img">
+                      <img src={imgWildlife} alt="A Bighorn Sheep" />    
+                    </div>
+                    <div className="card-body">
+                      <h5 className="card-body-header">
+                        Wildlife Viewing and Safety
+                      </h5>
+                      <div className="card-button">
+                      Learn more <i className="fa fa-chevron-circle-right"></i>
+                      </div>
+                    </div>
+                  </a>
+                </div>
+                <div className="col-12 home-card-wrapper">
+                  <a href="/conserve" className="card grid-card grid-card--horz">
+                    <div className="card-img">
+                      <img src={imgConservation} alt="A mountain peak" />    
+                    </div>
+                    <div className="card-body">
+                      <h5 className="card-body-header">
+                        Conservation
+                      </h5>
+                      <div className="card-button">
+                      Learn more <i className="fa fa-chevron-circle-right"></i>
+                      </div>
+                    </div>
+                  </a>
+                </div>
+                <div className="col-12 home-card-wrapper">
+                  <a href="/news" className="card grid-card grid-card--horz">
+                    <div className="card-img">
+                      <img src={imgGetToKnow} alt="Family walking on a trail" />    
+                    </div>
+                    <div className="card-body">
+                      <h5 className="card-body-header">
+                        Get to know BC Parks
+                      </h5>
+                      <div className="card-button">
+                      Learn more <i className="fa fa-chevron-circle-right"></i>
+                      </div>
+                    </div>
+                  </a>
+                </div>
+                <div className="col-12 home-card-wrapper">
+                  <a href="/get-involved" className="card grid-card grid-card--horz">
+                    <div className="card-img">
+                      <img src={imgGetInvolved} alt="People holding license plates" />    
+                    </div>
+                    <div className="card-body">
+                      <h5 className="card-body-header">
+                        Get Involved
+                      </h5>
+                      <div className="card-button">
+                      Learn more <i className="fa fa-chevron-circle-right"></i>
+                      </div>
+                    </div>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </Container>
+        </>
+      )}
       <Container className="max-width-override" fixed disableGutters>
         <Footer>
           {data.strapiWebsites.Footer}
