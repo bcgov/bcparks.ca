8fe786289f591755fd284d5c9909d56c8b4c79b5
Cole Maclean <c.maclean@oxd.com>
2022-02-04 08:13:47 -0800

    BRS-303: prettify pagecontent

--- src/staging/src/components/pageContent/pageContent.js
+++ src/staging/src/components/pageContent/pageContent.js
@@ -4,18 +4,24 @@ import LinkCard from "./linkCard"
 
 /*
  * PageContent is a generic switch for different strapi content types.
- */ 
-export default function PageContent({ contentType, content })
-{
+ */
+export default function PageContent({ contentType, content }) {
   if (!content) {
     return null
   }
 
   if (contentType === "parks.link-card") {
-      return (<LinkCard title={content.title} url={content.url} imageUrl={content.imageUrl} imageAltText={content.imageAltText}></LinkCard>)
+    return (
+      <LinkCard
+        title={content.title}
+        url={content.url}
+        imageUrl={content.imageUrl}
+        imageAltText={content.imageAltText}
+      ></LinkCard>
+    )
   }
   if (contentType === "parks.html-area") {
-    return (<HTMLArea isVisible={true}>{content.HTML}</HTMLArea>)
+    return <HTMLArea isVisible={true}>{content.HTML}</HTMLArea>
   }
   return null
 }
