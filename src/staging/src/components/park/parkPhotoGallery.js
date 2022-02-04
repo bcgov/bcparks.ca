4c749e48089ba27ed5dfa5b0bbd6728685cb7a4a
Cole Maclean <c.maclean@oxd.com>
2022-02-04 11:40:43 -0800

    BRS-314: fix park image display
    Add our own version of the gatsby image wrapper

--- src/staging/src/components/park/parkPhotoGallery.js
+++ src/staging/src/components/park/parkPhotoGallery.js
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from "react"
-import { makeStyles } from "@material-ui/core/styles"
+import React, { useState } from "react"
 import { Button, Grid, Box, Divider } from "@material-ui/core"
 import SimpleReactLightbox, { SRLWrapper } from "simple-react-lightbox"
 import PhotoLibraryOutlinedIcon from "@material-ui/icons/PhotoLibraryOutlined"
 import { useLightbox } from "simple-react-lightbox"
+import ParkPhoto from "./parkPhoto"
 
 const ShowPhotos = ({ text, setShowPhotos }) => {
   const { openLightbox } = useLightbox()
@@ -22,30 +22,7 @@ const ShowPhotos = ({ text, setShowPhotos }) => {
   )
 }
 
-const useStyles = makeStyles({
-  bigPhoto: {
-    objectFit: "cover",
-    overflow: "hidden",
-    height: 400,
-    zIndex: 2,
-  },
-  blurPhoto: {
-    objectFit: "cover",
-    overflow: "hidden",
-    height: 400,
-    zIndex: 2,
-    filter: "blur(2px)",
-  },
-  smallPhoto: {
-    objectFit: "cover",
-    overflow: "hidden",
-    height: 196,
-    zIndex: 2,
-  },
-})
-
 export default function ParkPhotoGallery({ photos }) {
-  const classes = useStyles()
   const [showPhoto, setShowPhoto] = useState(false)
   const parkPhotos = photos.map((photo, index) => {
     return {
@@ -54,10 +31,6 @@ export default function ParkPhotoGallery({ photos }) {
       imageUrl: photo.imageUrl,
     }
   })
-
-  useEffect(() => {
-    return
-  }, [showPhoto])
 
   const srlOptions = {
     settings: {
@@ -101,7 +74,6 @@ export default function ParkPhotoGallery({ photos }) {
             <br />
             <Box
               id="park-photo-gallery-container"
-              className={classes.photoGallery}
             >
               <SimpleReactLightbox>
                 <SRLWrapper options={srlOptions}>
@@ -109,15 +81,15 @@ export default function ParkPhotoGallery({ photos }) {
                     <>
                       <Grid item container spacing={1}>
                         <Grid item xs={12} md={6}>
-                          <img
-                            className={classes.bigPhoto}
+                          <ParkPhoto
+                            type="big"
                             src={parkPhotos[0].imageUrl}
                             alt={parkPhotos[0].caption}
                           />
                         </Grid>
                         <Grid item xs={12} md={6} className="show-photo-button">
-                          <img
-                            className={classes.blurPhoto}
+                          <ParkPhoto
+                            type="blur"
                             src={parkPhotos[0].imageUrl}
                             alt={parkPhotos[0].caption}
                           />
@@ -137,15 +109,15 @@ export default function ParkPhotoGallery({ photos }) {
                     <>
                       <Grid item container spacing={1}>
                         <Grid item xs={12} md={6}>
-                          <img
-                            className={classes.bigPhoto}
+                          <ParkPhoto
+                            type="big"
                             src={parkPhotos[0].imageUrl}
                             alt={parkPhotos[0].caption}
                           />
                         </Grid>
                         <Grid item xs={12} md={6} className="show-photo-button">
-                          <img
-                            className={classes.bigPhoto}
+                          <ParkPhoto
+                            type="big"
                             src={parkPhotos[1].imageUrl}
                             alt={parkPhotos[1].caption}
                           />
@@ -165,8 +137,8 @@ export default function ParkPhotoGallery({ photos }) {
                                 key={index}
                                 className={`${showPhoto}? "" : hide-photo`}
                               >
-                                <img
-                                  className={classes.smallPhoto}
+                                <ParkPhoto
+                                  type="small"
                                   src={photo.imageUrl}
                                   alt={photo.caption}
                                   key={index}
@@ -184,8 +156,8 @@ export default function ParkPhotoGallery({ photos }) {
                           {parkPhotos
                             .filter(f => f.index === 0)
                             .map((photo, index) => (
-                              <img
-                                className={classes.bigPhoto}
+                              <ParkPhoto
+                                type="big"
                                 src={photo.imageUrl}
                                 alt={photo.caption}
                                 key={index}
@@ -207,8 +179,8 @@ export default function ParkPhotoGallery({ photos }) {
                               )
                               .map((photo, index) => (
                                 <Grid item xs={6} key={index}>
-                                  <img
-                                    className={classes.smallPhoto}
+                                  <ParkPhoto
+                                    type="small"
                                     src={photo.imageUrl}
                                     alt={photo.caption}
                                     key={index}
@@ -224,8 +196,8 @@ export default function ParkPhotoGallery({ photos }) {
                                   key={index}
                                   className={`${showPhoto}? "" : hide-photo`}
                                 >
-                                  <img
-                                    className={classes.smallPhoto}
+                                  <ParkPhoto
+                                    type="small"
                                     src={photo.imageUrl}
                                     alt={photo.caption}
                                     key={index}
@@ -266,14 +238,13 @@ export default function ParkPhotoGallery({ photos }) {
           >
             <Box
               id="park-photo-gallery-container"
-              className={classes.photoGallery}
             >
               <SimpleReactLightbox>
                 <SRLWrapper options={srlOptions}>
                   <Grid item container spacing={1}>
                     <Grid item xs={12} md={12}>
-                      <img
-                        className={classes.bigPhoto}
+                      <ParkPhoto
+                        type="big"
                         src={parkPhotos[0].imageUrl}
                         alt={parkPhotos[0].caption}
                       />
@@ -292,8 +263,8 @@ export default function ParkPhotoGallery({ photos }) {
                             key={index}
                             className={`${showPhoto}? "" : hide-photo`}
                           >
-                            <img
-                              className={classes.smallPhoto}
+                            <ParkPhoto
+                              type="small"
                               src={photo.imageUrl}
                               alt={photo.caption}
                               key={index}
