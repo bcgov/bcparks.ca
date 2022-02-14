903e290754ef9a26545d75022bf033b352d77da8
cameronpettit <cameron@digitalspace.ca>
2022-02-14 22:37:18 +0000

    BRS-422 404 page updates

--- src/staging/src/pages/404.js
+++ src/staging/src/pages/404.js
@@ -1,14 +1,14 @@
 import * as React from "react"
-import { Link, navigate } from "gatsby"
+import { Link } from "gatsby"
 import Seo from "../components/seo"
 import BCParksLogo from "../images/bcparks-h-rgb-rev.png"
-import { Button } from "@material-ui/core"
-
+import Footer from "../components/footer"
+import ThumbLink from "../components/thumbLink"
 import "../styles/404.scss"
 
-function goHome() {
-  navigate('/');
-}
+const thumbHome = "https://github.com/bcgov/bcparks.ca/blob/main/src/staging/src/images/home/home-thumb.jpg?raw=true"
+const thumbPark = "https://github.com/bcgov/bcparks.ca/blob/main/src/staging/src/images/park/park-thumb.jpg?raw=true"
+const thumbReserve = "https://github.com/bcgov/bcparks.ca/blob/main/src/staging/src/images/reserve/reserve-thumb.jpg?raw=true"
 
 const NotFoundPage = () => (
   <>
@@ -19,16 +19,29 @@ const NotFoundPage = () => (
           <img className="bc-parks-logo" src={BCParksLogo} alt="BC Parks logo" />
         </Link>
       </nav>
-      <div className="not-found-container d-flex align-items-center justify-content-center">
-        <div className="p-3 text-center">
-          <h1>The page you are looking for is coming soon.</h1>
-          <p>The link you followed may be broken, or the page is being developed. Come back soon.</p>
-          <Button variant="contained"
-            className="home-button px-5 py-2"
-            onClick={() => goHome()}>
-            Back Home
-          </Button>
+      <div>
+        <div className="not-found-container d-flex align-items-center justify-content-center container">
+          <div className="p-5">
+            <div className="mx-2">
+              <h1>This page is still being designed.</h1>
+              <h1>Please visit our featured pages below.</h1>
+            </div>
+            <div className="mt-2 d-flex justify-content-center">
+              <div className="row">
+                <div className="col-lg-4 col-md-12 mt-2">
+                  <ThumbLink imageLink={thumbHome} title="Homepage" navLink="/" />
+                </div>
+                <div className="col-lg-4 col-md-12 mt-2">
+                  <ThumbLink imageLink={thumbPark} title="Park Search" navLink="/explore" />
+                </div>
+                <div className="col-lg-4 col-md-12 mt-2">
+                  <ThumbLink imageLink={thumbReserve} title="Reservations" navLink="/reserve" />
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
+        <Footer />
       </div>
     </div>
   </>
