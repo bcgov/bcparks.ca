7ea850a961bb657a297a1b9372059e799597e1ae
cameronpettit <cameron@digitalspace.ca>
2022-02-15 20:28:08 +0000

    BRS-422-2 proper image referencing with Gatsby

--- src/staging/src/pages/404.js
+++ src/staging/src/pages/404.js
@@ -6,10 +6,6 @@ import Footer from "../components/footer"
 import ThumbLink from "../components/thumbLink"
 import "../styles/404.scss"
 
-const thumbHome = "https://github.com/bcgov/bcparks.ca/blob/main/src/staging/src/images/home/home-thumb.jpg?raw=true"
-const thumbPark = "https://github.com/bcgov/bcparks.ca/blob/main/src/staging/src/images/park/park-thumb.jpg?raw=true"
-const thumbReserve = "https://github.com/bcgov/bcparks.ca/blob/main/src/staging/src/images/reserve/reserve-thumb.jpg?raw=true"
-
 const NotFoundPage = () => (
   <>
     <Seo title="404: Not found" />
@@ -21,7 +17,7 @@ const NotFoundPage = () => (
       </nav>
       <div>
         <div className="not-found-container d-flex align-items-center justify-content-center container">
-          <div className="p-5">
+          <div className="p-3">
             <div className="mx-2">
               <h1>This page is still being designed.</h1>
               <h1>Please visit our featured pages below.</h1>
@@ -29,13 +25,13 @@ const NotFoundPage = () => (
             <div className="mt-2 d-flex justify-content-center">
               <div className="row">
                 <div className="col-lg-4 col-md-12 mt-2">
-                  <ThumbLink imageLink={thumbHome} title="Homepage" navLink="/" />
+                  <ThumbLink imageLink="404/home-thumb.jpg" title="Homepage" navLink="/" />
                 </div>
                 <div className="col-lg-4 col-md-12 mt-2">
-                  <ThumbLink imageLink={thumbPark} title="Park Search" navLink="/explore" />
+                  <ThumbLink imageLink="404/park-thumb.jpg" title="Park Search" navLink="/explore" />
                 </div>
                 <div className="col-lg-4 col-md-12 mt-2">
-                  <ThumbLink imageLink={thumbReserve} title="Reservations" navLink="/reserve" />
+                  <ThumbLink imageLink="404/reserve-thumb.jpg" title="Reservations" navLink="/reserve" />
                 </div>
               </div>
             </div>
