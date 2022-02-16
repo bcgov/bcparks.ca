4833d011055fb75021cda44efd4f10a9fe7da330
meredithom <meredith@digitalspace.ca>
2022-02-16 10:30:01 -0800

    BRS-315: filter and search buttons swapped on mobile, filter design updated, headers edited to reflect figma designs, text bolded to reflect figma designs, not-found text and design edited (#277)
    BRS-315: no results found message moved into its own component
    
    BRS-315: now shows 0 results found instead of blank text for header
    
    BRS-315: minor styling update and filters set to be open by default

---
+++ src/staging/src/components/search/noSearchResults.js
@@ -0,0 +1,22 @@
+import React from "react"
+import "../../styles/search.scss"
+
+const  NoSearchResults = () => {
+    return (
+        <div className="no-results-container m20t mb20 p10">
+            <div className="row">
+                <div className="col-12 font-weight-bold">
+                    We’re unable to find any parks that match your criteria
+                </div>
+                <div className="col-12 p20t">
+                    Your filtering options did not return any results.
+                </div>
+                <div className="col-12">
+                Please try refining your inputs or parameters.
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default NoSearchResults;
