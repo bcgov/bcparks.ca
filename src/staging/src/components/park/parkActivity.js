eed8cf6e72b19645c8d563771ba895dc113f9cab
Cole Maclean <c.maclean@oxd.com>
2022-02-17 14:46:26 -0800

    BRS-427: move SVG icons into repo (#283)
    * add svg icons
    * add icon component for SVG icons
    * update park facility and activity details to use new component
    * update seed data with SVG icon names

--- src/staging/src/components/park/parkActivity.js
+++ src/staging/src/components/park/parkActivity.js
@@ -11,16 +11,13 @@ import ExpandMoreIcon from "@material-ui/icons/ExpandMore"
 import Heading from "./heading"
 import HtmlContent from "./htmlContent"
 import Spacer from "./spacer"
+import StaticIcon from "./staticIcon"
 
 export default function ParkActivity({ data }) {
   const activityData = data
-  let expandedInitial = []
-  activityData.forEach((activity, index) => {
-    expandedInitial[index] = false
-  })
-
-  // const [allExpanded, setAllExpanded] = useState(false)
-  const [expanded, setExpanded] = useState(expandedInitial)
+  const [expanded, setExpanded] = useState(
+    Array(activityData.length).fill(false)
+  )
 
   if (activityData.length === 0) return null
 
@@ -29,14 +26,6 @@ export default function ParkActivity({ data }) {
     setExpanded([...expanded])
   }
 
-  // const expandAll = isAllExpanded => {
-  //   let expanded = []
-  //   activityData.forEach((activity, index) => {
-  //     expanded[index] = isAllExpanded
-  //   })
-  //   setExpanded(expanded)
-  // }
-
   return (
     <Grid item xs={12} id="park-activity-container" className="anchor-link">
       <Paper elevation={0}>
@@ -44,63 +33,35 @@ export default function ParkActivity({ data }) {
           <Grid item xs={12}>
             <Heading>Activities</Heading>
           </Grid>
-          {/* <Grid
-            item
-            xs={6}
-            container
-            justifyContent="flex-end"
-            alignItems="flex-start"
-          >
-            <Box m={2}>
-              {activityData.length > 1 && (
-                <Button
-                  color="primary"
-                  onClick={() => {
-                    expandAll(!allExpanded)
-                    setAllExpanded(!allExpanded)
-                  }}
-                >
-                  {allExpanded ? "[collapse all]" : "[expand all]"}
-                </Button>
-              )}
-            </Box>
-          </Grid> */}
         </Grid>
-        {activityData && (
-          <Grid container spacing={1}>
-            {activityData.map((activity, index) => (
-              <Grid key={index} item xs={12}>
-                <Paper>
-                  <Accordion
-                    expanded={expanded[index]}
-                    onChange={handleChange(index)}
+        <Grid container spacing={1}>
+          {activityData.map((activity, index) => (
+            <Grid key={index} item xs={12}>
+              <Paper>
+                <Accordion
+                  expanded={expanded[index]}
+                  onChange={handleChange(index)}
+                >
+                  <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls={activity.activityType.activityName}
+                    id={index}
                   >
-                    <AccordionSummary
-                      expandIcon={<ExpandMoreIcon />}
-                      aria-controls={activity.activityType.activityName}
-                      id={index}
-                    >
-                      <Box mr={1}>
-                        <img
-                          src={activity.activityType.icon}
-                          alt={activity.activityType.icon ? activity.activityType.activityName : ""}
-                          width="48"
-                          height="48"
-                        />
-                      </Box>
-                      <HtmlContent className="pl15 p10t">
-                        {activity.activityType.activityName}
-                      </HtmlContent>
-                    </AccordionSummary>
-                    <AccordionDetails>
-                      <HtmlContent>{activity.description}</HtmlContent>
-                    </AccordionDetails>
-                  </Accordion>
-                </Paper>
-              </Grid>
-            ))}
-          </Grid>
-        )}
+                    <Box mr={1}>
+                      <StaticIcon name={activity.activityType.icon} size={48} />
+                    </Box>
+                    <HtmlContent className="pl15 p10t">
+                      {activity.activityType.activityName}
+                    </HtmlContent>
+                  </AccordionSummary>
+                  <AccordionDetails>
+                    <HtmlContent>{activity.description}</HtmlContent>
+                  </AccordionDetails>
+                </Accordion>
+              </Paper>
+            </Grid>
+          ))}
+        </Grid>
         <Spacer />
       </Paper>
     </Grid>
