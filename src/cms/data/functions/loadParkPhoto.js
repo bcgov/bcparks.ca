416fb5943d018ab3814867c021ca63b50cc8a897
Cole Maclean <c.maclean@oxd.com>
2022-02-17 10:27:07 -0800

    NOBUG: remove photo upload from initial data seed

--- src/cms/data/functions/loadParkPhoto.js
+++ src/cms/data/functions/loadParkPhoto.js
@@ -1,67 +1,6 @@
-const mime = require("mime-types");
 const fs = require("fs");
-const request = require("request");
-const path = require("path");
 
 const loadUtils = require("./loadUtils");
-
-const rootDir = process.cwd();
-const IMAGE_PATH = "\\data\\images";
-
-const urlExists = async (url) =>
-  await new Promise((resolve, reject) =>
-    request
-      .head(url)
-      .on("response", (res) => resolve(res.statusCode.toString()[0] === "2"))
-  );
-
-const downloadImage = async (url, dest) => {
-  const file = fs.createWriteStream(dest);
-
-  await new Promise((resolve, reject) => {
-    request({
-      uri: url,
-      gzip: true,
-    })
-      .pipe(file)
-      .on("finish", async () => {
-        resolve();
-      })
-      .on("error", (error) => {
-        reject(error);
-      });
-  }).catch((error) => {
-    strapi.log.info("error occured - downloadImage", error);
-  });
-};
-
-const loadImage = async (parkId, filepath) => {
-  try {
-    const filename = path.parse(filepath).base;
-    const fileStat = fs.statSync(filepath);
-    const attachment = await strapi.plugins.upload.services.upload.upload({
-      data: {
-        refId: parkId,
-        ref: "park-photo",
-        field: "image",
-      },
-      files: {
-        path: filepath,
-        name: filename,
-        type: mime.lookup(filepath),
-        size: fileStat.size,
-      },
-    });
-    //delete file
-    fs.unlink(filepath, (err) => {
-      if (err) {
-        strapi.log.info(`load image - error deleting ${filepath}`);
-      }
-    });
-  } catch {
-    strapi.log.info("error occured - loadImage");
-  }
-};
 
 const loadParkPhoto = async () => {
   const modelName = "park-photo";
@@ -73,11 +12,6 @@ const loadParkPhoto = async () => {
     const dataSeed = JSON.parse(jsonData)["parkPhotos"];
 
     for await (const data of dataSeed) {
-      const isUrlExists = await urlExists(data.thumbnail);
-      if (!isUrlExists) {
-        break;
-      }
-
       const parkPhoto = {
         orcs: data.orcs,
         orcsSiteNumber: data.orcsSiteNumber,
@@ -89,20 +23,10 @@ const loadParkPhoto = async () => {
         imageUrl: data.image,
         thumbnailUrl: data.thumbnail,
         isActive: data.active === true ? true : false,
-        image: null,
-        thumbnail: null,
       };
 
       try {
-        const result = await strapi.services["park-photo"].create(parkPhoto);
-
-        const filename = result.imageUrl
-          .replace("https://bcparks.ca/photos/", "")
-          .replace(/\//g, "-");
-        const filepath = `${rootDir}${IMAGE_PATH}\\${filename}`;
-
-        await downloadImage(result.imageUrl, filepath);
-        loadImage(result.id, filepath);
+        await strapi.services["park-photo"].create(parkPhoto);
       } catch {
         strapi.log.info("error occured - loadParkPhoto");
       }
