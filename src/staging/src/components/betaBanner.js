8d53be6cd3f1ff32e94df7b84340a0c137f60383
Meredith <meredith@digitalspace.ca>
2022-02-25 21:16:57 +0000

    BRS-458-2: Update to link text colour to improve contrast/readability. Unused style removed.

--- src/staging/src/components/betaBanner.js
+++ src/staging/src/components/betaBanner.js
@@ -1,9 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
-import {
-  Box
-} from "@material-ui/core"
-
+import { Box } from "@material-ui/core"
 import { makeStyles } from "@material-ui/core/styles";
 import "../styles/global.scss"
 
@@ -18,10 +15,8 @@ const useStyles = makeStyles(theme => ({
     justifyContent: "center",
     "& a": {
       marginLeft: "5px",
-      textDecoration: "none"
-    },
-    "& a:hover": {
-      color: "#036"
+      textDecoration: "underline",
+      color: "#003366"
     },
     [theme.breakpoints.down('sm')]: {
       minHeight: '40px',
@@ -29,14 +24,8 @@ const useStyles = makeStyles(theme => ({
       padding: "5px"
     }
   },
-  betaMsg: {
-
-  },
   linkDivider: {
     margin: "0px 4px",
-  },
-  link: {
-    "& a": { color: "#036" }
   },
   infoIcon: {
     fontSize: "1.2rem",
@@ -53,13 +42,13 @@ export default function BetaBanner() {
   return (
     <Box className={classes.betaHeader + " bc-bg-yellow bc-color-blue-dk"}>
       <i className={"fa fa-info-circle " + classes.infoIcon}></i>
-      <Box className={classes.betaMsg}>This site is in<Link to="/intro">beta</Link></Box>
+      <Box>This site is in<Link to="/intro">beta</Link></Box>
       <Box className={classes.linkDivider}>|</Box>
-      <Box className={classes.betaMsg}>
+      <Box>
         <a href="https://chefs.nrs.gov.bc.ca/app/form/submit?f=62ae996e-0f0b-4c2c-a8ae-6a32bb1b046f">Feedback Form</a>
       </Box>
       <Box className={classes.linkDivider}>|</Box>
-      <Box className={classes.betaMsg}>Return to<a href="https://bcparks.ca/">bcparks.ca</a></Box>
+      <Box>Return to<a href="https://bcparks.ca/">bcparks.ca</a></Box>
     </Box>
   )
 }
