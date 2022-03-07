fed3b8c0a30519c39bb45e9929ab5d2c6162b03d
Cole Maclean <c.maclean@oxd.com>
2022-03-07 09:42:42 -0800

    BRS-448: db import/export scripts (#310)

---
+++ tools/syncdb/export-db.ps1
@@ -0,0 +1,49 @@
+Param (
+  [Parameter(Mandatory)]
+  $Project,
+  [Parameter(Mandatory)]
+  $OutputPath
+)
+
+$remoteTempPath="/tmp/db-export"
+$exportFilename="export.sql"
+
+oc project $Project
+
+if (!$?) {
+   Write-Host "Please make sure you are logged in via 'oc login' before executing."
+   Exit 1
+}
+
+# Reading data should be safe on any patroni pod, so just use the first one (zero)
+oc exec bcparks-patroni-0  -- /bin/bash -c "mkdir -p ${remoteTempPath} && \
+    PGUSER=`$APP_USER PGPASSWORD=`$APP_PASSWORD pg_dump \
+        --clean \
+        --if-exists \
+        --no-owner \
+        --table=public.\* \
+        --exclude-table=core_store* \
+        --exclude-table=tokens* \
+        --exclude-table=strapi_\* \
+        --exclude-table=upload_file\* \
+        --exclude-table=users-permissions\* \
+        `$APP_DATABASE > ${remoteTempPath}/${exportFilename}"
+
+if (!$?) {
+   Write-Host "An error occurred."
+   Exit 1
+}
+
+oc rsync bcparks-patroni-0:$remoteTempPath $OutputPath
+
+if (!$?) {
+   Write-Host "An error occurred."
+   Exit 1
+}
+
+oc exec bcparks-patroni-0  -- rm -r $remoteTempPath
+
+if (!$?) {
+   Write-Host "An error occurred."
+   Exit 1
+}
---
+++ tools/syncdb/import-db.ps1
@@ -0,0 +1,56 @@
+Param (
+  [Parameter(Mandatory)]
+  $Project,
+  [Parameter(Mandatory)]
+  $InputPath
+)
+
+$remoteTempPath="/tmp/db-import"
+$exportFilename="export.sql"
+
+oc project $Project
+
+if (!$?) {
+   Write-Host "Please make sure you are logged in via 'oc login' before executing."
+   Exit 1
+}
+
+# Get patroni status to determine the current leader
+# Writes on the replica can cause issues
+$patroniInfo = oc exec bcparks-patroni-0  -- patronictl list --format json | ConvertFrom-Json
+if (!$?) {
+   Write-Host "An error occurred reading patroni info."
+   Exit 1
+}
+
+$leaderName = ''
+Foreach ($item in $patroniInfo) {
+    if ($item.Role -eq 'Leader') {
+        $leaderName = $item.Member
+        break
+    }
+}
+Write-Host "Current patroni leader: ${leaderName}"
+
+oc exec ${leaderName}  -- /bin/bash -c "mkdir -p ${remoteTempPath}"
+
+oc rsync $InputPath ${leaderName}:${remoteTempPath}
+
+if (!$?) {
+   Write-Host "An error occurred."
+   Exit 1
+}
+
+oc exec ${leaderName}  -- /bin/bash -c "PGUSER=`$APP_USER PGPASSWORD=`$APP_PASSWORD psql `$APP_DATABASE < ${remoteTempPath}/${exportFilename}"
+
+if (!$?) {
+   Write-Host "An error occurred."
+   Exit 1
+}
+
+oc exec $leaderName -- rm -r ${remoteTempPath}
+
+if (!$?) {
+   Write-Host "An error occurred."
+   Exit 1
+}
