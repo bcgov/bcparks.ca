9e452ac959595117fb89519a46b4ed7f8b78b71c
cameronpettit <cameron@digitalspace.ca>
2022-03-09 00:05:16 +0000

    BRS-339 Match explore page park status with highest precedence advisory

--- src/staging/src/components/park/parkAccessStatus.js
+++ src/staging/src/components/park/parkAccessStatus.js
@@ -20,18 +20,68 @@ const ICONS = {
   "yellow": yellowStatusIcon,
   "red": redStatusIcon,
 }
+
+const PRECEDENCES = {
+  1: {
+    text: "Full Closure",
+    color: "red"
+  },
+  2: {
+    text: "Partial Closure",
+    color: "yellow"
+  },
+  3: {
+    text: "Inaccessible",
+    color: "yellow"
+  },
+  4: {
+    text: "Warning",
+    color: "yellow"
+  },
+  5: {
+    text: "Closed to the public",
+    color: "yellow"
+  },
+  6: {
+    text: "Restricted; permit required",
+    color: "yellow"
+  },
+  99: {
+    text: "Open",
+    color: "blue"
+  },
+}
+
 export default function ParkAccessStatus({ advisories }) {
   const classes = useStyles()
   let parkStatusIcon = blueStatusIcon
   let parkStatusText = "Open to public access"
 
-  const accessStatuses = advisories.filter(advisory => advisory.accessStatus).map(advisory => {
-    return {
-      precedence: advisory.accessStatus.precedence,
-      color: advisory.accessStatus.color,
-      text: advisory.accessStatus.accessStatus,
+  // unfortunately, incoming advisories from parks details and explore pages are structured differently.
+  // we need to differentiate between the two structures. 
+
+  let accessStatuses = [];
+
+  for (let advisory of advisories) {
+    if (advisory.accessStatus) {
+      if (advisory.accessStatus.precedence) {
+        // advisory is coming from parks details page
+        accessStatuses.push({
+          precedence: advisory.accessStatus.precedence,
+          color: advisory.accessStatus.color,
+          text: advisory.accessStatus.accessStatus,
+        });
+      } else {
+        // advisory is coming from explore page
+        accessStatuses.push({
+          precedence: advisory.accessStatus,
+          color: PRECEDENCES[advisory.accessStatus].color,
+          text: PRECEDENCES[advisory.accessStatus].text,
+        })
+      }
     }
-  })
+  }
+
   accessStatuses.sort((a, b) => {
     return a.precedence - b.precedence
   })
