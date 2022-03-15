5eeafc773d5c5b6a9e2bd77212264e1af910284e
Day Kirby <day@oxd.com>
2022-03-15 10:38:02 -0700

    BRS-361 update footer

--- src/staging/src/components/footer.js
+++ src/staging/src/components/footer.js
@@ -1,189 +1,27 @@
 import React, { useState } from "react"
-import {
-  Grid,
-  Box,
-  InputLabel,
-  TextField
-} from "@material-ui/core"
-import { Helmet } from "react-helmet";
+import { Helmet } from "react-helmet"
 
-import SearchIcon from "@material-ui/icons/Search"
 import bcParksWordmark from "../images/BCParks_Wordmark_White.svg"
 import facebookIcon from "../images/Facebook_Negative.svg"
 import instaIcon from "../images/Instagram_Negative.svg"
 
-import "../styles/global.scss"
-import { makeStyles } from "@material-ui/core/styles"
-
-const useStyles = makeStyles(theme => ({
-  logoContainer: {
-    [theme.breakpoints.down("sm")]: {
-      textAlign: "center",
-    },
-  },
-  logo: {
-    [theme.breakpoints.up("sm")]: {
-      height: "90px",
-    },
-    [theme.breakpoints.down("sm")]: {
-      height: "40px",
-    },
-  },
-  menuList: {
-    paddingTop: "10px",
-    listStyle: "none",
-    paddingLeft: "20px",
-    [theme.breakpoints.up("md")]: {
-      fontSize: "1.05rem"
-    },
-  },
-  menu2: {
-    // need to reorder 2 and 3 in mobile to keep
-    // "stay connected" close to social buttons
-    [theme.breakpoints.down("md")]: {
-      order: 3,
-    },
-    [theme.breakpoints.up("md")]: {
-      order: 2,
-    },
-  },
-  menu3: {
-    [theme.breakpoints.down("md")]: {
-      order: 2,
-    },
-    [theme.breakpoints.up("md")]: {
-      order: 3,
-    },
-  },
-  menu4: {
-    order: 4,
-  },
-  menuHeader: {
-    fontWeight: "bold",
-    color: "#fff",
-  },
-  menuDivider: {
-    marginTop: "1rem",
-    marginBottom: "5px",
-    height: "1px",
-    backgroundColor: "#fcba19",
-    width: "48px",
-  },
-  menuItem: {
-    marginTop: "10px",
-  },
-  socialIcons: {
-    textAlign: "left",
-    [theme.breakpoints.up("md")]: {
-      marginLeft: "auto",
-      width: "33%",
-    },
-  },
-  socialIcon: {
-    display: "inline-block",
-    marginTop: "4px",
-    marginLeft: "20px",
-  },
-  utilityMenu: {
-    display: "block",
-    textAlign: "center",
-    paddingTop: "15px",
-    marginTop: "60px",
-    borderTop: "solid 1px #fff",
-    paddingBottom: "20px",
-    [theme.breakpoints.up("md")]: {
-      textAlign: "left",
-      margin: "60px 25px 0px 25px",
-    },
-  },
-  utilityLink: {
-    fontSize: "0.8rem",
-    margin: "0 10px",
-    display: "inline-block",
-    [theme.breakpoints.up("md")]: {
-      fontSize: "1rem",
-      margin: "20px 40px 10px 0px",
-    },
-  },
-  searchBoxContainer: {
-    margin: "10px 20px 25px 20px",
-    "& .MuiInputLabel-root": {
-      display: "none",
-    },
-  },
-  searchBox: {
-    lineHeight: "50px",
-    height: "50px",
-    width: "100%",
-    maxWidth: "445px",
-    color: "#fff",
-    paddingLeft: "20px",
-    borderRadius: "25px",
-    alignItems: "center",
-    display: "flex",
-    [theme.breakpoints.down("sm")]: {
-      margin: "0 auto",
-    },
-    "& .MuiFormControl-root": { // needed so dynamic underline spans area
-      width: "100%",
-    },
-    "& input": { // needed to make basic input styling invisible
-      background: "none",
-      height: "40px",
-      color: "#fff",
-      fontFamily: "BC Sans, Verdana, Arial, sans serif",
-      fontSize: "1rem",
-      "&::placeholder": {
-        color: "#fff",
-        opacity: 1,
-      },
-    },
-  },
-  searchInput: {
-    fontSize: "0.75rem",
-    width: "80%",
-  },
-  searchIcon: {
-    marginLeft: "auto",
-    marginRight: "10px",
-  },
-  searchMsg: {
-    color: "#fff",
-    fontSize: "0.75rem",
-    fontStyle: "italic",
-    textAlign: "center",
-
-    [theme.breakpoints.down("md")]: {
-      borderBottom: "solid 1px rgba(255,255,255,0.3)",
-      marginBottom: "20px",
-      paddingBottom: "10px",
-      margin: "-15px 40px 0px 40px",
-    },
-    [theme.breakpoints.up("md")]: {
-      maxWidth: "400px",
-      borderBottom: "none",
-      margin: "-15px 0px 0px 20px",
-    },
-  },
-}))
+import "../styles/footer.scss"
 
 function FooterMenu({ items }) {
-  const classes = useStyles()
-
   return (
     <>
-      <ul className={classes.menuList}>
+      <ul className="footer-menu-list list-unstyled pt-3 pl-1 ml-3 ml-md-0 text-white">
         {items.map((item, index) => (
           <li key={index}>
             {item.type === "header" && (
               <>
-                <div className={classes.menuHeader}>{item.display}</div>
-                <div className={classes.menuDivider}>&nbsp;</div>
+                <div className="font-weight-bold">{item.display}</div>
+                <div className="footer-menu-divider">&nbsp;</div>
               </>
             )}
             {item.type === "link" && (
               <>
-                <div key={index} className={classes.menuItem}>
+                <div key={index} className="mt-2">
                   <a href={item.link}>{item.display}</a>
                 </div>
               </>
@@ -196,26 +34,28 @@ function FooterMenu({ items }) {
 }
 
 export default function Footer() {
-  const classes = useStyles()
-
   // Arrays of footer menus
   // TODO replace with data from Strapi
   const menu1 = [
     { type: "header", display: "Get a permit" },
-    { type: "link", display: "Commercial services", link: "/permits" },
+    { type: "link", display: "Commercial services", link: "/commercial-use" },
   ]
   const menu2 = [
     { type: "header", display: "Get involved" },
     { type: "link", display: "Volunteer", link: "/volunteers" },
-    { type: "link", display: "Support BC Parks", link: "/volunteers" },
+    { type: "link", display: "Support BC Parks", link: "/donate" },
   ]
   const menu3 = [
     { type: "header", display: "Stay connected" },
     { type: "link", display: "Contact us", link: "/contact" },
-    { type: "link", display: "News releases", link: "https://engage.gov.bc.ca/bcparksblog/"  },
+    {
+      type: "link",
+      display: "Blog",
+      link: "https://engage.gov.bc.ca/bcparksblog/",
+    },
   ]
   const utilityMenu = [
-    { display: "Sitemap", link: "/sitemap"},    
+    { display: "Sitemap", link: "/sitemap" },
     { display: "Accessibility", link: "/accessibility" },
     { display: "Careers", link: "/careers" },
     { display: "Disclaimer", link: "/disclaimer" },
@@ -226,10 +66,14 @@ export default function Footer() {
   const [searchText, setSearchText] = useState("")
   const [searchMsg, setSearchMsg] = useState("")
 
-
-  const handleSearchTextChange = (str) => { // TODO - search box does nothing
-    setSearchText(str);
-    setSearchMsg("Search utility under construction")
+  const handleSearchTextChange = str => {
+    // TODO - search box does nothing
+    setSearchText(str)
+    if (str) {
+      setSearchMsg("Search utility under construction")
+    } else {
+      setSearchMsg("")
+    }
   }
 
   return (
@@ -240,94 +84,91 @@ export default function Footer() {
           <script src="/_scripts/snowplow.js" />
         </Helmet>
       )}
-      
+
       <footer id="footer">
         <div className="home-footer" id="home-footer">
-          <Box sx={{ margin: "40px 0" }}>
-            <Grid sx={{ flexGrow: 1 }} container>
-              <Grid xs={12} sm={12} md={4} lg={6} item>
-                <Box>
-                  <div className={classes.logoContainer} id="footer-logo">
-                    <a href="/">
-                      <img className={classes.logo} alt="BC Parks logo" src={bcParksWordmark} />
-                    </a>
+          <div className="my-5 by-3">
+            <div className="row">
+              <div className="col col-12 col-md-5">
+                <div className="text-center text-md-left" id="footer-logo">
+                  <a href="/">
+                    <img alt="BC Parks logo" src={bcParksWordmark} />
+                  </a>
+                </div>
+
+                <div className="footer-search-container form-control text-white">
+                  <div className="footer-search-box d-flex">
+                    <span className="fa fa-search form-control-icon"></span>
+                    <input
+                      type="search"
+                      className="form-control"
+                      placeholder="Search BCParks.ca"
+                      aria-label="Search BCParks.ca"
+                      onChange={event => {
+                        handleSearchTextChange(event.target.value)
+                      }}
+                      onKeyPress={ev => {
+                        if (ev.key === "Enter") {
+                          handleSearchTextChange(searchText)
+                          ev.preventDefault()
+                        }
+                      }}
+                    />
                   </div>
-                </Box>
-                <Box>
-                  <div className={classes.searchBoxContainer}>
-                    <div className={classes.searchBox + " bc-bg-blue-md"}>
-                      <InputLabel htmlFor="footer-site-search">
-                        Search
-                      </InputLabel>
-                      <TextField
-                        id="footer-site-search"
-                        aria-label="Search BCParks.ca"
-                        placeholder="Search BCParks.ca"
-                        InputLabelProps={{
-                          style: { color: '#fff'}
-                        }}
-                        value={searchText}
-                        onChange={event => {
-                          handleSearchTextChange(event.target.value)
-                        }} 
-                        onKeyPress={ev => {
-                          if (ev.key === "Enter") {
-                            handleSearchTextChange(searchText)
-                            ev.preventDefault()
-                          }
-                        }}
-                        ></TextField>
-                        <SearchIcon className={classes.searchIcon} />
-                      </div>
-                  </div>
-                  { (searchMsg !== "") && (<div className={classes.searchMsg}>
-                    {searchMsg}
-                  </div>)}
-                </Box>
-              </Grid>
-              <Grid xs={12} sm={12} md={8} lg={6} item>
-                <Grid sx={{ flexGrow: 1 }} container>
-                  <Grid className={classes.menu1} xs={6} sm={6} md={4} lg={4} item>
+                </div>
+                <div className="footer-search-msg text-center font-italic text-white mt-3">
+                  {searchMsg === "" ? <> &nbsp; </> : searchMsg}
+                </div>
+              </div>
+              <div className="col d-none d-lg-block col-lg-1">&nbsp;</div>
+              <div className="col col-12 col-md-6">
+                <div className="row">
+                  <div className="col col-6 col-sm-4">
                     <div>
                       <FooterMenu items={menu1}></FooterMenu>
                     </div>
-                  </Grid>
-                  <Grid className={classes.menu2} xs={6} sm={6} md={4} lg={4} item>
-                    <div>
-                      <FooterMenu items={menu2}></FooterMenu>
-                    </div>
-                  </Grid>
-                  <Grid  className={classes.menu3} xs={6} sm={6} md={4} lg={4} item>
+                  </div>
+                  <div className="col col-6 col-sm-4 order-sm-3">
                     <div>
                       <FooterMenu items={menu3}></FooterMenu>
                     </div>
-                  </Grid>
-                  <Grid className={classes.menu4} xs={6} sm={6} md={12} lg={12} item>
-                    <div className={classes.socialIcons}>
-                      <div className={classes.socialIcon}>
+                  </div>
+                  <div className="col col-6 col-sm-4 order-sm-2">
+                    <div>
+                      <FooterMenu items={menu2}></FooterMenu>
+                    </div>
+                  </div>
+                  <div className="col col-8 d-none d-sm-block order-5">
+                    &nbsp;
+                  </div>
+                  <div className="col col-6 col-sm-4 order-last">
+                    <div className="text-left">
+                      <div className="d-inline-block mt-3 ml-4 ml-md-1">
                         <a href="https://www.facebook.com/YourBCParks/">
                           <img src={facebookIcon} alt="Facebook" />
                         </a>
                       </div>
-                      <div className={classes.socialIcon}>
+                      <div className="d-inline-block mt-3 ml-4">
                         <a href="https://www.instagram.com/yourbcparks">
                           <img src={instaIcon} alt="Instagram" />
                         </a>
                       </div>
                     </div>
-                  </Grid>
-                </Grid>
-              </Grid>
-            </Grid>
-
-            <div className={classes.utilityMenu}>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="text-center py-3 mt-5 border-top border-white">
               {utilityMenu.map((item, index) => (
-                <div className={classes.utilityLink} key={index}>
+                <div
+                  className="footer-utility-link mx-1 mx-sm-2 mx-md-3 d-inline-block"
+                  key={index}
+                >
                   <a href={item.link}>{item.display}</a>
                 </div>
               ))}
             </div>
-          </Box>
+          </div>
         </div>
       </footer>
     </>
