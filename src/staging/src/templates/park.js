a063a03568efd1faecf1c5a11b3b093965a95885
Day Kirby <day@oxd.com>
2022-04-04 12:27:24 -0700

    BWT-194 updated parks detail menu

--- src/staging/src/templates/park.js
+++ src/staging/src/templates/park.js
@@ -14,7 +14,7 @@ import {
 import { makeStyles } from "@material-ui/core/styles"
 import useScrollSpy from "react-use-scrollspy"
 
-import { capitalizeFirstLetter } from "../utils/helpers";
+import { capitalizeFirstLetter } from "../utils/helpers"
 
 import Footer from "../components/footer"
 import Header from "../components/header"
