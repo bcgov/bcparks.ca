1beb3fae83a2c48620733ac40a3222a7e207450c
Mayur Marakana <mayur.marakana@gmail.com>
2022-04-06 08:20:21 -0700

    BRS-434 remove unused helper function

--- src/staging/src/utils/helpers.js
+++ src/staging/src/utils/helpers.js
@@ -1,5 +1,1 @@
 export const capitalizeFirstLetter = (str) => str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
-
-export const sortByKey = (key) => {
-    return (a, b) => (a[key] > b[key]) ? 1 : ((b[key] > a[key]) ? -1 : 0);
-};
