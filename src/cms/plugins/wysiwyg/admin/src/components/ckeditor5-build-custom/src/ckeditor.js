519bfe8aead288cc01be767337d794f71182d3bd
Foley Lynn <foley@oxd.com>
2022-10-20 15:30:38 -0700

    CM-69: rebuilt CKEditor and cleaned up stray files (#394)
    CM-88: fix for page header image not taking the full width

--- src/cms/plugins/wysiwyg/admin/src/components/ckeditor5-build-custom/src/ckeditor.js
+++ src/cms/plugins/wysiwyg/admin/src/components/ckeditor5-build-custom/src/ckeditor.js
@@ -3,6 +3,7 @@
  * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
  */
 import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor.js';
+import AutoLink from '@ckeditor/ckeditor5-link/src/autolink.js';
 import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote.js';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold.js';
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials.js';
@@ -11,21 +12,21 @@ import Heading from '@ckeditor/ckeditor5-heading/src/heading.js';
 import HorizontalLine from '@ckeditor/ckeditor5-horizontal-line/src/horizontalline.js';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic.js';
 import Link from '@ckeditor/ckeditor5-link/src/link.js';
-import AutoLink from '@ckeditor/ckeditor5-link/src/autolink.js';
 import List from '@ckeditor/ckeditor5-list/src/list.js';
 import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed.js';
 import MediaEmbedToolbar from '@ckeditor/ckeditor5-media-embed/src/mediaembedtoolbar.js';
 import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph.js';
+import RemoveFormat from '@ckeditor/ckeditor5-remove-format/src/removeformat.js';
 import SourceEditing from '@ckeditor/ckeditor5-source-editing/src/sourceediting.js';
-import Style from '@ckeditor/ckeditor5-style/src/style.js';
+import Style from '@ckeditor/ckeditor5-style/src/style';
 import Table from '@ckeditor/ckeditor5-table/src/table.js';
 import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline.js';
-import RemoveFormat from '@ckeditor/ckeditor5-remove-format/src/removeformat.js';
 
 class Editor extends ClassicEditor {}
 
 // Plugins to include in the build.
 Editor.builtinPlugins = [
+	AutoLink,
 	BlockQuote,
 	Bold,
 	Essentials,
@@ -34,37 +35,38 @@ Editor.builtinPlugins = [
 	HorizontalLine,
 	Italic,
 	Link,
-	AutoLink,
 	List,
 	MediaEmbed,
 	MediaEmbedToolbar,
 	Paragraph,
+	RemoveFormat,
 	SourceEditing,
 	Style,
 	Table,
-	Underline,
-	RemoveFormat
+	Underline
 ];
 
 // Editor configuration.
 Editor.defaultConfig = {
 	toolbar: {
 		items: [
-			'sourceEditing',
 			'heading',
 			'style',
 			'|',
 			'bold',
 			'italic',
 			'underline',
+			'link',
 			'bulletedList',
 			'numberedList',
-			'link',
-			'horizontalLine',
 			'|',
+			'horizontalLine',
 			'blockQuote',
 			'insertTable',
-			'mediaEmbed'
+			'mediaEmbed',
+			'|',
+			'removeFormat',
+			'sourceEditing'
 		]
 	},
 	language: 'en'
