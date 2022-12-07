826fa7765f5b9d9cfe5f2e6c39992f0ef4b935d8
Ayumi Tanaka <90415568+ayumi-oxd@users.noreply.github.com>
2022-12-07 15:01:01 -0800

    CM-154: Add park-sub-page in Gatsby (#438)

--- src/staging/gatsby-config.js
+++ src/staging/gatsby-config.js
@@ -37,6 +37,7 @@ module.exports = {
           "park-operation",
           "access-statuses",
           "park-operation-sub-areas",
+          "park-sub-page"
         ],
         queryLimit: -1,
       },
