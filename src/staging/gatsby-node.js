38f0bcf0d3384effe9e8507797fa8aadbce2a6ec
Mike Olund <mike@oxd.com>
2022-12-17 09:50:56 -0800

    NOBUG: revert GraphQL fix part 2

--- src/staging/gatsby-node.js
+++ src/staging/gatsby-node.js
@@ -241,7 +241,6 @@ exports.createSchemaCustomization = ({ actions }) => {
     siteName: String
     siteNumber: Int
     orcsSiteNumber: String
-    content: [Node]!
     protectedArea: StrapiProtectedArea
     parkActivities: [StrapiParkActivities]
     parkFacilities: [StrapiParkFacilities]
