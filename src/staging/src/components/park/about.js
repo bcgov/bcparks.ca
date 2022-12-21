88fe6821c603874a0de6ef83a0c707082806fbc3
ayumi <ayumi@oxd.com>
2022-12-21 15:56:49 -0800

    CM-206: Update park page styling

--- src/staging/src/components/park/about.js
+++ src/staging/src/components/park/about.js
@@ -25,15 +25,21 @@ export default function About({
       <Paper elevation={0}>
         <Heading>{capitalizeFirstLetter(`Learn more about this ${parkType}`)}</Heading>
         {(park.totalArea || park.establishedDate) && (
-          <ul>
-            <li>
-              <strong>Date established:</strong> {formattedEstablishedDate}
-            </li>
-            <li>
-              <strong>Size:</strong> {park.totalArea} hectares
-            </li>
-          </ul>
+          <>
+            <h3>{capitalizeFirstLetter(`${park.type} details`)}</h3>
+            <div className="about-details">
+              <ul>
+                <li>
+                  <strong>Date established:</strong> {formattedEstablishedDate}
+                </li>
+                <li>
+                  <strong>Size:</strong> {park.totalArea} hectares
+                </li>
+              </ul>
+            </div>
+          </>
         )}
+        {(park.totalArea || park.establishedDate) && park.parkContact && <Spacer />}
         {park.parkContact && (
           <>
             <h3 className="heading">{capitalizeFirstLetter(`${parkType} contact`)}</h3>
@@ -43,7 +49,7 @@ export default function About({
         {park.parkContact && park.natureAndCulture && <Spacer />}
         {park.natureAndCulture && (
           <>
-            <h3 className="heading">Nature & culture</h3>
+            <h3>Nature & culture</h3>
             <HtmlContent>{park.natureAndCulture}</HtmlContent>
           </>
         )}
