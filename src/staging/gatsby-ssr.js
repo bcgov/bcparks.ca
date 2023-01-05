3723b1e5fef9f9f0cc63d4d5723712df5d23888f
Mike Olund <mike@oxd.com>
2023-01-05 10:22:10 -0800

    NOBUG: Reduce Gatsby HTML file size by removing inline styles

--- src/staging/gatsby-ssr.js
+++ src/staging/gatsby-ssr.js
@@ -4,4 +4,20 @@
  * See: https://www.gatsbyjs.com/docs/ssr-apis/
  */
 
-// You can delete this file if you're not using it
+// Reduce Gatsby HTML file size by removing inline styles
+// see https://kinderas.com/technology/21/08/28/gatsby-stylesheet-over-inline-styles
+export const onPreRenderHTML = ({ getHeadComponents }) => {
+    if (process.env.NODE_ENV !== "production") return;
+
+    getHeadComponents().forEach((el) => {
+        if (el.type === "style" && el.props["data-href"] && el.props["data-identity"] === "gatsby-global-css") {
+            el.type = "link";
+            el.props.href = el.props["data-href"];
+            el.props.rel = "stylesheet";
+            el.props.type = "text/css";
+
+            delete el.props["data-href"];
+            delete el.props.dangerouslySetInnerHTML;
+        }
+    });
+};
