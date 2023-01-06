558b0246cd7a6a74e99fb4976e754f49c20bb21e
ayumi <ayumi@oxd.com>
2023-01-05 16:02:51 -0800

    NOBUG: Add validation and error message for slugs

--- src/cms/api/site/models/site.js
+++ src/cms/api/site/models/site.js
@@ -5,4 +5,20 @@
  * to customize this model
  */
 
-module.exports = {};
+const regex = new RegExp("^[a-z0-9/]+(?:-[a-z0-9/]+)*$|^$");
+const slugValidator = async (data) => {
+    if (!regex.test(data.slug)) {
+      throw strapi.errors.badRequest('Please enter letters, numbers, hyphens, or slashes for slug. No spaces.');
+    }
+}
+
+module.exports = {
+    lifecycles: {
+        beforeCreate: async (data) => {
+            slugValidator(data)
+        },
+        beforeUpdate: async (data) => {
+            slugValidator(data)
+        },
+    },
+};
