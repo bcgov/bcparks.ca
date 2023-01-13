4059815b5586f9334b20ced6bfbacb2423a865e0
Ayumi Tanaka <90415568+ayumi-oxd@users.noreply.github.com>
2023-01-12 16:04:27 -0800

    CM-282: Add special notes section (#515)

--- src/staging/src/components/park/specialNote.js
+++ src/staging/src/components/park/specialNote.js
@@ -5,22 +5,19 @@ import Heading from "./heading"
 import HtmlContent from "./htmlContent"
 import Spacer from "./spacer"
 
-// TODO: this component needs to be converted to bootstrap but
-// it should be done at the same time as the other sections
-// to match spacing
-export default function SafetyInfo({ safetyInfo }) {
+export default function SpecialNote({ specialNotes }) {
   return (
     <>
       <Grid
         item
         xs={12}
-        id="park-safety-info-container"
+        id="park-special-notes-container"
         className="anchor-link"
       >
         <Paper elevation={0}>
-          <Heading>Safety info</Heading>
-          {safetyInfo && (
-            <HtmlContent>{safetyInfo}</HtmlContent>
+          <Heading>Special notes</Heading>
+          {specialNotes && (
+            <HtmlContent>{specialNotes}</HtmlContent>
           )}
           <Spacer />
         </Paper>
