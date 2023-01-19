8a5cd995fd57247f43e906b809a50dfb2dab43aa
Natalia Sabadysh <natalia@oxd.com>
2023-01-19 14:07:54 -0700

    fix(hook): added default value for the props

--- src/staging/src/templates/staticContent1.js
+++ src/staging/src/templates/staticContent1.js
@@ -46,7 +46,6 @@ export default function StaticContent1({ pageContext }) {
   `)
 
   const pageContent = pageContext?.page?.Content // array of content components in page
-  console.log('+++')
   const contentHtml = pageContext?.page?.Content.find((item) => item.introHtml);
   const { introHtml } = useContent(contentHtml)
 
