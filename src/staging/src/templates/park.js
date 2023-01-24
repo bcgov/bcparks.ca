6f13e2ad99cf29a45908054f4799216bb102697a
ayumi <ayumi@oxd.com>
2023-01-24 11:10:32 -0800

    CM-90: Add classname to the park and site content

--- src/staging/src/templates/park.js
+++ src/staging/src/templates/park.js
@@ -354,7 +354,7 @@ export default function ParkTemplate({ data }) {
               sm={12}
               md={9}
               lg={9}
-              className={classes.parkContent}
+              className={`${classes.parkContent} main-content`}
             >
               {menuItems[0].visible && (
                 <div ref={parkOverviewRef} className="full-width">
