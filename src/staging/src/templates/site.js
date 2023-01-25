09a97bfc893f3d2ad0a8f44a4dc08af572363cbe
ayumi <ayumi@oxd.com>
2023-01-25 14:57:33 -0800

    CM-90: Add styling to makeStyle

--- src/staging/src/templates/site.js
+++ src/staging/src/templates/site.js
@@ -309,7 +309,7 @@ export default function ParkTemplate({ data }) {
               sm={12}
               md={9}
               lg={9}
-              className={`${classes.parkContent} main-content`}
+              className={classes.parkContent}
             >
               {menuItems[0].visible && (
                 <div ref={parkOverviewRef} className="full-width">
