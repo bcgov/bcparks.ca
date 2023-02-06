b5259b87f1a690d17c65b5090152950450038b87
ayumi <ayumi@oxd.com>
2023-02-06 10:13:05 -0800

    CM-377: Delete graphql sorting

--- src/staging/src/pages/find-a-park.js
+++ src/staging/src/pages/find-a-park.js
@@ -51,7 +51,6 @@ export const query = graphql`
       }
     }
     allStrapiActivityTypes(
-      sort: { fields: activityName }
       filter: { isActive: { eq: true } }
     ) {
       totalCount
