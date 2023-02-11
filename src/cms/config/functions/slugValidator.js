1201957104936caa25eb4697b20f89fedc7a9ded
ayumi <ayumi@oxd.com>
2023-02-10 18:12:44 -0800

    CM-455: Add slash and dash combined condition

--- src/cms/config/functions/slugValidator.js
+++ src/cms/config/functions/slugValidator.js
@@ -2,9 +2,9 @@ module.exports = {
   // checks for valid characters and consecutive forward slashes
   slugCharacterValidator: function (slug) {
     const regex = new RegExp("^[a-z0-9\-\/]+(?:-[a-z0-9\-\/]+)*$|^$");
-      if (!regex.test(slug)) {
-        throw strapi.errors.badRequest('Please enter lower case letters, numbers, hyphens, or slashes for slugs. No spaces.');
-      }
+    if (!regex.test(slug)) {
+      throw strapi.errors.badRequest('Please enter lower case letters, numbers, hyphens, or slashes for slugs. No spaces.');
+    }
     const regex2 = new RegExp("^(?!.*\/\/)[a-z0-9\-\/]+(?:-[a-z0-9\-\/]+)*$|^$");
     if (!regex2.test(slug)) {
       throw strapi.errors.badRequest('Consecutive forward slashes are not allowed in slugs.');
@@ -26,6 +26,9 @@ module.exports = {
   slugNoLeadingDashValidator: function (slug) {
     if (slug?.slice(0, 1) === '-') {
       throw strapi.errors.badRequest('Do not add dash to the beginning of slugs.');
+    }
+    if (slug.includes('/-')) {
+      throw strapi.errors.badRequest('Do not add dash after a slash.');
     }
   },
   // a trailing slash is required
