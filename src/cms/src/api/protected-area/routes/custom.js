d6e97e61b0da435f08277cfad443047b3a773b88
Natalia Sabadysh <nataliasabadysh@gmail.com>
2023-02-23 12:35:07 -0700

    protecteda-area, add custom routes

---
+++ src/cms/src/api/protected-area/routes/custom.js
@@ -0,0 +1,24 @@
+module.exports = {
+  routes: [
+    {
+      method: "GET",
+      path: "/protected-areas/count",
+      handler: "protected-area.count",
+    },
+    {
+      method: "GET",
+      path: "/protected-areas/names",
+      handler: "protected-area.names",
+    },
+    {
+      method: "GET",
+      path: "/protected-areas/items",
+      handler: "protected-area.items",
+    },
+    {
+      method: "GET",
+      path: "/protected-areas/status",
+      handler: "protected-area.status",
+    },
+  ],
+};
