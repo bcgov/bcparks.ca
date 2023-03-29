406e369cccfd7dd2e839ff8c1c6cf4307cc2c0d4
Mike Olund <mike@oxd.com>
2023-03-28 22:44:37 -0700

    Add /api to the base URL in the staff portal

--- src/admin/src/axios_config.js
+++ src/admin/src/axios_config.js
@@ -7,7 +7,7 @@ axios.defaults.headers.post["Content-Type"] = "application/json";
 axios.defaults.headers.get["Content-Type"] = "application/json";
 
 const cmsAxios = axios.create({
-  baseURL: config.REACT_APP_CMS_BASE_URL
+  baseURL: config.REACT_APP_CMS_BASE_URL + "/api"
 });
 
 export { cmsAxios, axios };
