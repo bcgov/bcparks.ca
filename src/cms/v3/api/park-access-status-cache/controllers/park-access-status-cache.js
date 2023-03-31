852fc461369355dd6eb6e5d7727800bdc2ec0f91
Mike Olund <mike@oxd.com>
2023-03-31 09:08:33 -0700

    Delete the Strapi v3 folder

--- src/cms/v3/api/park-access-status-cache/controllers/park-access-status-cache.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-module.exports = {
-  async find(ctx) {
-    console.log("Park access status cache find:", ctx);
-    return strapi.services["park-access-status-cache"].find({ _limit: 1200 });
-  },
-};
--- src/cms/v3/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || "953ff98c-d088-47f3-b1d8-61da6904624b",
-};
