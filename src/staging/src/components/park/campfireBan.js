faf4bb294a42d3c310bd7c90bc0e9696dd07508d
Ayumi Tanaka <90415568+ayumi-oxd@users.noreply.github.com>
2023-04-03 10:21:09 -0700

    CM-558: Added Campfire Ban icon and campfireBanEffectiveDate field (#614)
    * CM-558: Change fireZone.fireCentre to fireZone.fireCentreNumber
    
    * CM-558: Add campfire ban icon
    
    * CM-558: Small fix
    
    * CM-558: Push back to fireZone.fireCentre
    
    * CM-558: Update campfire icon alt text

--- src/staging/src/components/park/campfireBan.js
+++ src/staging/src/components/park/campfireBan.js
@@ -1,52 +1,34 @@
 import React from "react"
-import { Grid, Card, CardHeader, Avatar } from "@material-ui/core"
+import { Card, CardHeader, Avatar } from "@material-ui/core"
 import { makeStyles } from "@material-ui/core/styles"
 
-import campfireBanIcon from "../../images/park/fire-ban.png"
+import campfireBanIcon from "../../images/park/campfire-ban-48.png"
 
 const useStyles = makeStyles({
   card: {
     border: "none",
     boxShadow: "none",
-    backgroundColor: "#e1ecf4",
-  },
-  topGrid: {
-    backgroundColor: "#e1ecf4",
   },
 })
 
-export default function CampfireBan({ data }) {
+export default function CampfireBan() {
   const classes = useStyles()
-  const { hasCampfireBan, hasSmokingBan } = data
-
-  let fireBans = []
-  let title = null
-  if (hasCampfireBan === "Y") fireBans.push("Campfire")
-  if (hasSmokingBan === "Y") fireBans.push("Smoking")
-  if (hasCampfireBan === "Y" || hasSmokingBan === "Y")
-    title = `${fireBans.join(" and ")} ban in effect`
-
-  if (!title) return null
 
   return (
-    <>
-      <Grid container item xs={12} sm={6} md={4} className={classes.topGrid}>
-        <Card className={classes.card}>
-          {title && (
-            <CardHeader
-              avatar={
-                <Avatar
-                  variant="square"
-                  src={campfireBanIcon}
-                  aria-label={title}
-                  alt=""
-                />
-              }
-              title={title}
-            />
-          )}
-        </Card>
-      </Grid>
-    </>
+    <Card className={classes.card}>
+      <CardHeader
+        className="access-icon"
+        avatar={
+          <Avatar
+            variant="square"
+            src={campfireBanIcon}
+            aria-label="campfires prohibited"
+            className="park-overview-icon"
+            alt="Campfires are prohibited"
+          />
+        }
+        title="No campfires"
+      />
+    </Card>
   )
 }
