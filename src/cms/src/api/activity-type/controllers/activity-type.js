389a6f252a0b90e6481881ed68d8bbfa2986a822
ayumi <ayumi@oxd.com>
2023-04-10 13:57:32 -0700

    CM-579: WIP - activity and facility change won't be saved

--- src/cms/src/api/activity-type/controllers/activity-type.js
+++ src/cms/src/api/activity-type/controllers/activity-type.js
@@ -6,4 +6,15 @@
 
 const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController("api::activity-type.activity-type");
+module.exports = createCoreController(
+    "api::activity-type.activity-type",
+    ({ strapi }) => ({
+        async update(ctx) {
+            strapi.plugins[
+                "rest-cache"
+            ].services.cacheConfig.clearByUid('api::protected-area.protected-area')
+            const response = await super.update(ctx);
+            return response;
+        }
+    })
+);
--- src/cms/src/api/facility-type/controllers/facility-type.js
+++ src/cms/src/api/facility-type/controllers/facility-type.js
@@ -6,4 +6,15 @@
 
 const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController("api::facility-type.facility-type");
+module.exports = createCoreController(
+    "api::facility-type.facility-type",
+    ({ strapi }) => ({
+        async update(ctx) {
+            strapi.plugins[
+                "rest-cache"
+            ].services.cacheConfig.clearByUid('api::protected-area.protected-area')
+            const response = await super.update(ctx);
+            return response;
+        }
+    })
+);
