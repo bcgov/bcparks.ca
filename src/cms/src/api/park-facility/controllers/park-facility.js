c5272c81db8f97798a1c9bfad7ed632bf3ce959f
ayumi <ayumi@oxd.com>
2023-04-10 16:43:57 -0700

    CM-579: WIP 3 - activity and facility change won't be saved

--- src/cms/src/api/park-facility/controllers/park-facility.js
+++ src/cms/src/api/park-facility/controllers/park-facility.js
@@ -6,15 +6,4 @@
 
 const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController(
-    "api::park-facility.park-facility",
-    ({ strapi }) => ({
-        async update(ctx) {
-            strapi.plugins[
-                "rest-cache"
-            ].services.cacheConfig.clearByUid('api::protected-area.protected-area')
-            const response = await super.update(ctx);
-            return response;
-        }
-    })
-);
+module.exports = createCoreController("api::park-facility.park-facility");
