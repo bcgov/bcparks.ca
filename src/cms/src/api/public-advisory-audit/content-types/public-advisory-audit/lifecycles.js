9ce6e40b21822da0d64049a06910c88ab117150e
Mike Olund <mike@oxd.com>
2023-04-21 10:33:22 -0700

    Fix error

--- src/cms/src/api/public-advisory-audit/content-types/public-advisory-audit/lifecycles.js
+++ src/cms/src/api/public-advisory-audit/content-types/public-advisory-audit/lifecycles.js
@@ -36,7 +36,7 @@ const createPublicAdvisoryAudit = async (data) => {
   data.isLatestRevision = false;
 
   try {
-    await strapi.entityService.create('api::public-advisory.public-advisory', { data: data })
+    await strapi.entityService.create('api::public-advisory-audit.public-advisory-audit', { data: data })
   } catch (error) {
     strapi.log.error(
       `error creating public-advisory-audit ${data.advisoryNumber}...`,
