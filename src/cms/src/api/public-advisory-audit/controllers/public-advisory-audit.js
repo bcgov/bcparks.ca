b8bdacfabae26ddddcbb0152ef2edb585a68c0be
Michael Olund <mike@oxd.com>
2023-04-24 16:02:01 -0700

    CM-621: Fixed cache invalidation (#652)

--- src/cms/src/api/public-advisory-audit/controllers/public-advisory-audit.js
+++ src/cms/src/api/public-advisory-audit/controllers/public-advisory-audit.js
@@ -1,32 +1,30 @@
 "use strict";
-const { sanitize } = require('@strapi/utils');
-
-
 /**
  * public-advisory-audit controller
  */
 
+const { sanitize } = require('@strapi/utils');
 const { createCoreController } = require("@strapi/strapi").factories;
+
+const clearRestCache = async function () {
+  const cachePlugin = strapi.plugins["rest-cache"];
+  if (cachePlugin) {
+    // clear the redis rest-cache when updates are made from the staff portal
+    await cachePlugin.services.cacheStore.clearByUid('api::protected-area.protected-area');
+    await cachePlugin.services.cacheStore.clearByUid('api::public-advisory.public-advisory');
+  }
+}
 
 module.exports = createCoreController(
   "api::public-advisory-audit.public-advisory-audit",
   ({ strapi }) => ({
     async create(ctx) {
-      const cachePlugin = strapi.plugins["rest-cache"];
-      if (cachePlugin) {
-        // clear the redis rest-cache when updates are made from the staff portal
-        cachePlugin.services.cacheStore.clearByUid('api::protected-area.protected-area');
-      }
+      await clearRestCache();
       const response = await super.create(ctx);
       return response;
     },
     async update(ctx) {
-      const cachePlugin = strapi.plugins["rest-cache"];
-      if (cachePlugin) {
-        // clear the redis rest-cache when updates are made from the staff portal
-        cachePlugin.services.cacheStore.clearByUid('api::protected-area.protected-area');
-        cachePlugin.services.cacheStore.clearByUid('api::public-advisory.public-advisory');
-      }
+      await clearRestCache();
       const response = await super.update(ctx);
       return response;
     },
