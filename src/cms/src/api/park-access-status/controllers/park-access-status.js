415286cbac6d8b35322fe00e013c3e576f512a20
Michael Olund <mike@oxd.com>
2023-04-27 16:13:56 -0700

    NOBUG: remove park-access-statuses-cache (#662)

--- src/cms/src/api/park-access-status/controllers/park-access-status.js
+++ src/cms/src/api/park-access-status/controllers/park-access-status.js
@@ -12,11 +12,9 @@ module.exports = createCoreController(
   "api::park-access-status.park-access-status",
   ({ strapi }) => ({
     async find(ctx) {
-      console.log("Park access status find:", ctx.request.query);
       let parkAccessStatuses;
-      // Performance checking to prevent falling down - there's a cached endpoint to use instead.  See /park-access-statuses-cache
-      if (ctx?.request?.query?.limit > 10) {
-        ctx.request.query.limit = 10;
+      if (ctx?.request?.query?.limit > 100 || !ctx?.request?.query?.limit) {
+        ctx.request.query.limit = 100;
       }
       parkAccessStatuses = await customStatus.getProtectedAreaStatus(ctx);
       return parkAccessStatuses;
