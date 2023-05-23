6025619a7ae43e0262e3defd07460bcddd7ad4da
ayumi <ayumi@oxd.com>
2023-05-23 11:32:50 -0700

    CM-633: Update the emergency text

--- src/gatsby/src/components/advisories/advisoryLegend.js
+++ src/gatsby/src/components/advisories/advisoryLegend.js
@@ -46,8 +46,8 @@ const AdvisoryLegend = () => {
           )
         })}
       </div>
-      <div className="mb-4 mt-3 px-md-5 mx-md-5 font-italic text-center">
-        Advisories are updated Monday to Friday 8:30 to 4:30, excluding statutory holidays.<br />
+      <div className="mb-4 mt-3 px-md-5 mx-md-5 text-center">
+        Advisories are updated Monday to Friday 8:30am to 4:30pm, excluding statutory holidays.<br />
         Get up-to-date provincial emergency information for wildfires, floods, and highways from:<br />
         <a href="https://www2.gov.bc.ca/gov/content/safety/wildfire-status">
           BC Wildfire Service
