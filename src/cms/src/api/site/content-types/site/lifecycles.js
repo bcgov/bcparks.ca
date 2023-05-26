cecc4f3622d9b2189c01369f078357bb8acdd223
ayumi <ayumi@oxd.com>
2023-05-26 10:00:46 -0700

    CM-700: Add custom validation to sites

--- src/cms/src/api/site/content-types/site/lifecycles.js
+++ src/cms/src/api/site/content-types/site/lifecycles.js
@@ -1,4 +1,6 @@
 "use strict";
+const utils = require('@strapi/utils');
+const { ApplicationError } = utils.errors;
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#lifecycle-hooks)
@@ -6,20 +8,37 @@
  */
 
 const validator = require("../../../../helpers/slugValidator.js");
+const hasProtectedArea = async (where) => {
+  if (where) {
+    try {
+      const site = await strapi.entityService.findOne(
+        "api::site.site", where.id, { populate: '*' }
+      )
+      const protectedArea = site.protectedArea
+      if (protectedArea === null) {
+        throw new ApplicationError('Please add protectedArea relation.');
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+}
 
 module.exports = {
-    beforeCreate(event) {
-        const { data, where, select, populate } = event.params;
-        validator.slugCharacterValidator(data.slug)
-        validator.slugNoLeadingSlashValidator(data.slug)
-        validator.slugNoLeadingDashValidator(data.slug)
-        validator.slugNoTrailingDashValidator(data.slug)
-    },
-    beforeUpdate(event) {
-        const { data, where, select, populate } = event.params;
-        validator.slugCharacterValidator(data.slug)
-        validator.slugNoLeadingSlashValidator(data.slug)
-        validator.slugNoLeadingDashValidator(data.slug)
-        validator.slugNoTrailingDashValidator(data.slug)
-    }
+  async beforeCreate(event) {
+    const { data, where, select, populate } = event.params;
+    hasProtectedArea(where)
+    validator.slugCharacterValidator(data.slug)
+    validator.slugNoLeadingSlashValidator(data.slug)
+    validator.slugNoLeadingDashValidator(data.slug)
+    validator.slugNoTrailingDashValidator(data.slug)
+  },
+  async beforeUpdate(event) {
+    const { data, where, select, populate } = event.params;
+    hasProtectedArea(where)
+    validator.slugCharacterValidator(data.slug)
+    validator.slugNoLeadingSlashValidator(data.slug)
+    validator.slugNoLeadingDashValidator(data.slug)
+    validator.slugNoTrailingDashValidator(data.slug)
+  }
 };
