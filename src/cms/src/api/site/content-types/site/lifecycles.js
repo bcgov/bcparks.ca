1dda1e13c78f8f6bec24abe371d087ac689c8892
ayumi <ayumi@oxd.com>
2023-05-29 10:41:50 -0700

    CM-700: Update validator for site and park sub page

--- src/cms/src/api/site/content-types/site/lifecycles.js
+++ src/cms/src/api/site/content-types/site/lifecycles.js
@@ -1,33 +1,27 @@
 "use strict";
-const utils = require('@strapi/utils');
-const { ApplicationError } = utils.errors;
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#lifecycle-hooks)
  * to customize this model
  */
 
-const validator = require("../../../../helpers/slugValidator.js");
+const validator = require("../../../../helpers/validator.js");
 
 module.exports = {
-  async beforeCreate(event) {
-    const { data, where, select, populate } = event.params;
-    if (data.protectedArea.connect.length === 0) {
-      throw new ApplicationError('Please add protectedArea relation.');
+    async beforeCreate(event) {
+        const { data, where, select, populate } = event.params;
+        validator.protectedAreaConnectValidator(data.protectedArea)
+        validator.slugCharacterValidator(data.slug)
+        validator.slugNoLeadingSlashValidator(data.slug)
+        validator.slugNoLeadingDashValidator(data.slug)
+        validator.slugNoTrailingDashValidator(data.slug)
+    },
+    async beforeUpdate(event) {
+        const { data, where, select, populate } = event.params;
+        validator.protectedAreaDisconnectValidator(data.protectedArea)
+        validator.slugCharacterValidator(data.slug)
+        validator.slugNoLeadingSlashValidator(data.slug)
+        validator.slugNoLeadingDashValidator(data.slug)
+        validator.slugNoTrailingDashValidator(data.slug)
     }
-    validator.slugCharacterValidator(data.slug)
-    validator.slugNoLeadingSlashValidator(data.slug)
-    validator.slugNoLeadingDashValidator(data.slug)
-    validator.slugNoTrailingDashValidator(data.slug)
-  },
-  async beforeUpdate(event) {
-    const { data, where, select, populate } = event.params;
-    if (data.protectedArea.disconnect.length > 0) {
-      throw new ApplicationError('Please add protectedArea relation.');
-    }
-    validator.slugCharacterValidator(data.slug)
-    validator.slugNoLeadingSlashValidator(data.slug)
-    validator.slugNoLeadingDashValidator(data.slug)
-    validator.slugNoTrailingDashValidator(data.slug)
-  }
 };
