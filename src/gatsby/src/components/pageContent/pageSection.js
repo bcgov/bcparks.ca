b10a76cf14544b5b708d0123f9c31bebd5ddb12c
Michael Olund <mike@oxd.com>
2023-06-01 15:51:25 -0700

    CM-723: Bugfix for page section ID's changed from int to guid w/ Strapi4 update (#743)

--- src/gatsby/src/components/pageContent/pageSection.js
+++ src/gatsby/src/components/pageContent/pageSection.js
@@ -6,12 +6,13 @@ import "../../styles/pageContent/pageSection.scss"
 
 const slugify = require("slugify")
 
-export default function PageSection({ sectionTitle, sectionId, sectionHtml}) {
+export default function PageSection({ sectionTitle, sectionId, sectionStrapiId, sectionHtml }) {
     const titleId = slugify(sectionTitle).toLowerCase()
 
     return (
         <>
             <span id={`page-section-${sectionId}`}></span>
+            <span id={`page-section-${sectionStrapiId}`}></span>
             <div className="page-section" id={titleId}>
                 <h2 className="page-section-title">{sectionTitle}</h2>
                 <HTMLArea className="page-section-html" isVisible={true}>
