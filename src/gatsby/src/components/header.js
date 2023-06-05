1f8d0279c84037e4be8eca07cc49985f38ca6a09
ayumi <ayumi@oxd.com>
2023-06-05 12:03:37 -0700

    CM-724: Remove banner and update background colour

--- src/gatsby/src/components/header.js
+++ src/gatsby/src/components/header.js
@@ -1,7 +1,6 @@
 import React from "react"
 
 import SkipToContent from "./skipToContent"
-import BetaBanner from "../components/betaBanner"
 import MegaMenu from "./megaMenu.js"
 
 export default function Header({ content = [] }) {
@@ -9,7 +8,6 @@ export default function Header({ content = [] }) {
   return (
     <>
       <SkipToContent></SkipToContent>
-      <BetaBanner></BetaBanner>
       <MegaMenu content={content} menuMode="responsive" />
     </>
   )  
