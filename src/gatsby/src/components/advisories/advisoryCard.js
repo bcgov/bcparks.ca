a5ad692b816626c238c40863325e9aa28f5467a4
ayumi <ayumi@oxd.com>
2023-06-08 16:23:42 -0700

    CM-719: Add fireCentres to active advisories page

--- src/gatsby/src/components/advisories/advisoryCard.js
+++ src/gatsby/src/components/advisories/advisoryCard.js
@@ -1,6 +1,4 @@
 import React, { useState } from "react"
-import Accordion from "react-bootstrap/Accordion"
-import Card from "react-bootstrap/Card"
 import Col from "react-bootstrap/Col"
 import Row from "react-bootstrap/Row"
 import { Link } from "gatsby"
@@ -19,11 +17,7 @@ const AdvisoryCard = ({ advisory, index }) => {
         key={advisory.id}
         thing={advisory.isFirstInYear.toString()}
       >
-        <Col
-          onClick={() => {
-            setOpen(!open)
-          }}
-        >
+        <Col>
           <div
             className={
               (advisory.isFirstInYear || index === 0) &&
@@ -55,137 +49,131 @@ const AdvisoryCard = ({ advisory, index }) => {
               Ongoing
             </div>
           </div>
-          <Accordion aria-controls={advisory.cardTitle} id={advisory.id}>
-            <Accordion.Toggle
-              as={Card}
-              eventKey="0"
-              className={
-                (advisory.description ? "" : "noDetails ") + "advisoryCard"
-              }
-            >
-              <Row className="p-2">
-                <div className="d-none d-md-block p-2">
-                  <div className="dateArea" aria-label={advisory.alertMsg}>
-                    <div className={"dateCircle " + advisory.alertClass}>
-                      <div className="dateDate">
-                        {advisory.advisoryDateObj.dateStr}
-                      </div>
-                      <div className="dateMonth">
-                        {advisory.advisoryDateObj.monthStr}
-                      </div>
-                      <div
-                        className={
-                          advisory.advisoryDateObj.dateUnknown
-                            ? "hidden"
-                            : "d-md-none"
-                        }
-                      >
-                        {advisory.advisoryDateObj.str}
-                      </div>
-                      <div
-                        className={
-                          advisory.advisoryDateObj.dateUnknown
-                            ? "dateUnknown d-md-none"
-                            : "hidden"
-                        }
-                      >
-                        Ongoing
-                      </div>
-                    </div>
+          <Row className="advisoryCard no-gutters p-2">
+            <div className="d-none d-md-block p-2">
+              <div className="dateArea" aria-label={advisory.alertMsg}>
+                <div className={"dateCircle " + advisory.alertClass}>
+                  <div className="dateDate">
+                    {advisory.advisoryDateObj.dateStr}
+                  </div>
+                  <div className="dateMonth">
+                    {advisory.advisoryDateObj.monthStr}
+                  </div>
+                  <div
+                    className={
+                      advisory.advisoryDateObj.dateUnknown
+                        ? "hidden"
+                        : "d-md-none"
+                    }
+                  >
+                    {advisory.advisoryDateObj.str}
+                  </div>
+                  <div
+                    className={
+                      advisory.advisoryDateObj.dateUnknown
+                        ? "dateUnknown d-md-none"
+                        : "hidden"
+                    }
+                  >
+                    Ongoing
                   </div>
                 </div>
-                <Col>
-                  <div className="d-flex justify-content-between align-items-center p-3">
-                    <div className="contentArea">
-                      {advisory.eventType && (
-                        <div className="cardTitle">
-                          {advisory.eventType.eventType}
-                        </div>
-                      )}
+              </div>
+            </div>
+            <Col>
+              <div className="d-flex justify-content-between align-items-center p-3">
+                <div className="contentArea">
+                  {advisory.eventType && (
+                    <div className="cardTitle">
+                      {advisory.eventType.eventType}
+                    </div>
+                  )}
+                  <div>
+                    {advisory.fireCentres.length > 0 && advisory.fireCentres.map(
+                      (fireCentre, index) => (
+                        <Link
+                          className="parkLink badge badge-pill badge-primary mb-2 mr-2"
+                          key={index}
+                        >
+                          {fireCentre.fireCentreName}
+                        </Link>
+                      ))}
+                  </div>
+                  <div>
+                    {open && (
                       <div>
                         {advisory.protectedAreas.length > 0 &&
                           advisory.protectedAreas
                             .filter(
                               park => park.publishedAt && park.isDisplayed
                             )
-                          .map((par, index) => (
+                            .map((par, index) => (
                               <Link
-                                className="parkLink my-2 badge badge-pill badge-light"
-                                to={`/${
-                                  par.slug
-                                    ? par.slug
-                                    : par.protectedAreaName
-                                        .toLowerCase()
-                                        .replace(/ /g, "-")
-                                }`}
+                                className="parkLink badge badge-pill badge-light mb-2 mr-2"
+                                to={`/${par.slug}`}
                                 key={index}
                               >
-                                  {par.protectedAreaName}
+                                {par.protectedAreaName}
                               </Link>
-                            ))}
-                      </div>
-                      <div>
-                        <HTMLArea isVisible>{advisory.title}</HTMLArea>
-                      </div>
-                    </div>
-                    <div
-                      className={
-                        advisory.description
-                          ? "expandIcon d-flex align-items-center"
-                          : "hidden"
-                      }
-                    >
-                      <i
-                        className={
-                          (open ? "open " : "close ") + "fa fa-angle-down"
+                            ))
                         }
-                      ></i>
-                    </div>
+                      </div>
+                    )}
+                    <button 
+                      className="btn btn-link"
+                      onClick={() => setOpen(!open)}
+                    >
+                      {open
+                        ? `Hide park${advisory.protectedAreas.length > 1 ? "s" : ""} affected`
+                        : `Show all ${advisory.protectedAreas.length} park${advisory.protectedAreas.length > 1 ? "s" : ""} affected`
+                      }
+                    </button>
                   </div>
-                  <Accordion.Collapse eventKey="0">
-                    <div>
-                      {advisory.description && (
-                        <>
-                          <div className="detailsArea p-3">
-                            {advisory.isEffectiveDateDisplayed &&
-                              advisory.effectiveDate && (
-                                <>
-                                  <br />
-                                  <p>
-                                    In effect {advisory.effectiveDateObj.str}
-                                    {advisory.isEndDateDisplayed &&
-                                      advisory.endDate && (
-                                        <>
-                                          {" to "}
-                                          {advisory.endDateObj.str}
-                                        </>
-                                      )}
-                                  </p>
-                                </>
-                              )}
-                            <HTMLArea isVisible>
-                              {advisory.description}
-                            </HTMLArea>
-                            {advisory.links?.map(({ title, url, id }) => (
-                              <a
-                                href={url}
-                                style={{display: 'block'}}
-                                target="_blank"
-                                rel="noopener noreferrer"
-                                key={id}
-                              >
-                                {title}
-                              </a>
-                            ))}
-                          </div>
-                        </>
-                      )}
+                  <div>
+                    <HTMLArea isVisible>{advisory.title}</HTMLArea>
+                  </div>
+                </div>
+              </div>
+              <div>
+                {advisory.description && (
+                  <>
+                    <div className="detailsArea p-3">
+                      {advisory.isEffectiveDateDisplayed &&
+                        advisory.effectiveDate && (
+                          <>
+                            <br />
+                            <p>
+                              In effect {advisory.effectiveDateObj.str}
+                              {advisory.isEndDateDisplayed &&
+                                advisory.endDate && (
+                                  <>
+                                    {" to "}
+                                    {advisory.endDateObj.str}
+                                  </>
+                                )}
+                            </p>
+                          </>
+                        )}
+                      <HTMLArea isVisible>
+                        {advisory.description}
+                      </HTMLArea>
+                      {advisory.links?.map(({ title, url, id }) => (
+                        <a
+                          href={url}
+                          style={{ display: 'block' }}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                          key={id}
+                        >
+                          {title}
+                        </a>
+                      ))}
                     </div>
-                  </Accordion.Collapse>
-                </Col>
-              </Row>
-            </Accordion.Toggle>
-          </Accordion>
+                  </>
+                )}
+              </div>
+            </Col>
+          </Row>
         </Col>
       </Row>
     </>
