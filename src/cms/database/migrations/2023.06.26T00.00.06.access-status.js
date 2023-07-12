bb1ebb516cea724e241a1ea78bbd6eac54c4896f
Michael Olund <mike@oxd.com>
2023-07-12 10:32:27 -0700

    CM-652: Update access statuses (#833)

--- src/cms/database/migrations/2023.06.26T00.00.06.access-status.js
+++ src/cms/database/migrations/2023.06.26T00.00.06.access-status.js
@@ -4,31 +4,31 @@ async function up(knex) {
 
     if (await knex.schema.hasTable('access_statuses')) {
 
-        await knex('access_statuses').where({ access_status: 'Full closure' }).update({ precedence: 1, color: 'red', group_label: 'Closed' });
+        await knex('access_statuses').where({ id: 1 }).update({ precedence: 1, color: 'red', group_label: 'Closed' });
 
-        await knex('access_statuses').where({ access_status: 'Park is under evacuation order' }).update({ precedence: 2, color: 'red', group_label: 'Closed', access_status: 'Under evacuation order' });
+        await knex('access_statuses').where({ id: 12 }).update({ precedence: 2, color: 'red', group_label: 'Closed', access_status: 'Under evacuation order' });
 
-        await knex('access_statuses').where({ access_status: 'Restricted; permit required' }).update({ precedence: 3, color: 'red', group_label: 'Permit required', access_status: 'Restricted: permit required' });
+        await knex('access_statuses').where({ id: 7 }).update({ precedence: 3, color: 'red', group_label: 'Permit required', access_status: 'Restricted: permit required' });
 
-        await knex('access_statuses').where({ access_status: 'Partial closure' }).update({ precedence: 4, color: 'yellow', group_label: 'Limited access' });
+        await knex('access_statuses').where({ id: 2 }).update({ precedence: 4, color: 'yellow', group_label: 'Limited access' });
 
-        await knex('access_statuses').where({ access_status: 'Park is under evacuation alert' }).update({ precedence: 5, color: 'yellow', group_label: 'Limited access', access_status: 'Under evacuation alert' });
+        await knex('access_statuses').where({ id: 11 }).update({ precedence: 5, color: 'yellow', group_label: 'Limited access', access_status: 'Under evacuation alert' });
 
-        await knex('access_statuses').where({ access_status: 'Park is within a fire perimeter' }).update({ precedence: 6, color: 'yellow', group_label: 'Visit with caution', access_status: 'Within a fire perimeter' });
+        await knex('access_statuses').where({ id: 10 }).update({ precedence: 6, color: 'yellow', group_label: 'Visit with caution', access_status: 'Within a fire perimeter' });
 
-        await knex('access_statuses').where({ access_status: 'Wildfire nearby' }).update({ precedence: 7, color: 'yellow', group_label: 'Visit with caution' });
+        await knex('access_statuses').where({ id: 13 }).update({ precedence: 7, color: 'yellow', group_label: 'Visit with caution' });
 
-        await knex('access_statuses').where({ access_status: 'Inaccessible' }).update({ precedence: 100, color: 'red', group_label: 'DUPLICATE' });
+        await knex('access_statuses').where({ id: 4 }).update({ precedence: 8, color: 'yellow', group_label: 'Limited access', access_status: 'Inaccessible' });
 
-        await knex('access_statuses').where({ access_status: 'Park is inaccessible' }).update({ precedence: 8, color: 'yellow', group_label: 'Limited access', access_status: 'Inaccessible' });
+        await knex('access_statuses').where({ id: 3 }).update({ precedence: 9, color: 'yellow', group_label: 'Visit with caution' });
 
-        await knex('access_statuses').where({ access_status: 'Warning' }).update({ precedence: 9, color: 'yellow', group_label: 'Visit with caution' });
+        await knex('access_statuses').where({ id: 8 }).update({ precedence: 10, color: 'yellow', group_label: 'Limited access' });
 
-        await knex('access_statuses').where({ access_status: 'Trail access affected' }).update({ precedence: 10, color: 'yellow', group_label: 'Limited access' });
+        await knex('access_statuses').where({ id: 6 }).update({ precedence: 99, color: 'blue', group_label: 'Open' });
 
-        await knex('access_statuses').where({ access_status: 'Open' }).update({ precedence: 99, color: 'blue', group_label: 'Open' });
+        await knex('access_statuses').where({ id: 9 }).update({ precedence: 100, color: 'red', group_label: 'Limited access' });
 
-        await knex('access_statuses').where({ access_status: 'Closed to the public' }).update({ precedence: 100, color: 'red', group_label: 'DUPLICATE' });
+        await knex('access_statuses').where({ id: 5 }).update({ precedence: 100, color: 'red', group_label: 'Closed' });
     }
 }
 
