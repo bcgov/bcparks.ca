55db1a906cd1b4c8b3be7b3c0e7c0deec22bcd6d
Michael Olund <mike@oxd.com>
2023-08-31 15:10:53 -0700

    CM-904: Create and populate Search-regions (#953)

---
+++ src/cms/src/api/search-region/controllers/search-region.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * search-region controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::search-region.search-region');
---
+++ src/cms/src/api/search-region/routes/search-region.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * search-region router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::search-region.search-region');
