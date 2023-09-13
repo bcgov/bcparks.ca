e13fb9537f34f8d8d8b7b7010280cba41d4e57a5
ayumi <ayumi@oxd.com>
2023-09-13 12:00:53 -0700

    CM-808: Add en-ca to toLocaleString method

--- src/gatsby/src/components/park/about.js
+++ src/gatsby/src/components/park/about.js
@@ -43,16 +43,16 @@ export default function About({
                   <strong>Size:</strong>
                   {marineProtectedArea === "Y" ?
                     <>
-                      {" "}{park.totalArea.toLocaleString()} hectares
+                      {" "}{park.totalArea.toLocaleString("en-CA")} hectares
                       {(park.uplandArea || park.marineArea) && ' ('}
-                      {park.uplandArea && park.uplandArea.toLocaleString() + ' ha upland'}
+                      {park.uplandArea && park.uplandArea.toLocaleString("en-CA") + ' ha upland'}
                       {(park.uplandArea && park.marineArea) && ' and '}
-                      {park.marineArea && park.marineArea.toLocaleString() + ' ha foreshore'}
+                      {park.marineArea && park.marineArea.toLocaleString("en-CA") + ' ha foreshore'}
                       {(park.uplandArea || park.marineArea) && ')'}
                     </>
                    :
                     <>
-                      {" "}{park.totalArea.toLocaleString()} hectares
+                      {" "}{park.totalArea.toLocaleString("en-CA")} hectares
                     </>
                   }
                 </li>
