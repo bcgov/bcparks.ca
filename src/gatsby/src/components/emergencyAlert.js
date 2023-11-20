7eb583fcf699471e2230eb9a9395dfd925fc68e7
ayumi <ayumi@oxd.com>
2023-11-20 10:43:07 -0800

    CM-1025: Update emergency alert styling

--- src/gatsby/src/components/emergencyAlert.js
+++ src/gatsby/src/components/emergencyAlert.js
@@ -1,6 +1,7 @@
-import React from "react"
+import React, { useState } from "react"
 import { useStaticQuery, graphql } from "gatsby"
-import HtmlContent from "./park/htmlContent"
+import ErrorIcon from '@mui/icons-material/Error'
+import CloseIcon from '@mui/icons-material/Close'
 
 const EmergencyAlert = () => {
   const data = useStaticQuery(graphql`
@@ -10,12 +11,8 @@ const EmergencyAlert = () => {
       ) {
         nodes {
           colour
-          description {
-            data {
-              description
-            }
-          }
-          link {
+          description
+          links {
             linkText
             url
           }
@@ -24,20 +21,44 @@ const EmergencyAlert = () => {
     }
   `)
   const alerts = data?.allStrapiEmergencyAlert?.nodes || []
+  // display only one alert even if there are multiple active alerts
+  const alert = alerts[0]
+  const [show, setShow] = useState(true)
+
+  const handleClick = () => {
+    setShow(false)
+  }
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter" || e.key === " ") {
+      handleClick()
+    }
+  }
 
   return (
-    alerts.map((alert, index) => (
-      <div key={index} className={`emergency-alert alert-bg-${alert.colour.toLowerCase()}`}>
+    show && (
+      <div className={`emergency-alert alert-bg-${alert.colour.toLowerCase()}`}>
         <div className="alert-container">
-          <HtmlContent>{alert.description.data.description}</HtmlContent>
-          {alert.link.map((l, index) => (
-            <span key={index}>
-              |<a href={l.url}>{l.linkText}</a>
-            </span>
-          ))}
+          <ErrorIcon className="warning-icon" />
+          <p>
+            {alert.description}
+            {alert.links.map((l, index) => (
+              <span key={index}>
+                <span className="partition">|</span>
+                <a href={l.url}>{l.linkText}</a>
+              </span>
+            ))}
+          </p>
+          <button
+            tabIndex={0}
+            onClick={handleClick}
+            onKeyDown={handleKeyDown}
+            className="btn btn-link"
+          >
+            <CloseIcon className="close-icon" />
+          </button>
         </div>
       </div>
-    ))
+    )
   )
 }
 
