86b17870e3247a20e602d42f24299ea4d8929eb7
Mike Olund <mike@oxd.com>
2023-11-21 15:36:53 -0800

    CM-1078: Add fields for park gate in Park Operation

---
+++ src/cms/src/api/park-operation-date/controllers/park-operation-date.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * park-operation-date controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::park-operation-date.park-operation-date');
---
+++ src/cms/src/api/park-operation-date/services/park-operation-date.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * park-operation-date service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::park-operation-date.park-operation-date');
