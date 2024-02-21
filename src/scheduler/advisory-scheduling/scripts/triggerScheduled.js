157eae037dcfde07da012bb1be49b4674afe3897
Mike Olund <mike@oxd.com>
2024-02-20 17:01:30 -0800

    CM-1250: Removed the Strapi cron job

---
+++ src/scheduler/advisory-scheduling/scripts/triggerScheduled.js
@@ -0,0 +1,14 @@
+const { cmsAxios } = require("../../shared/axiosConfig");
+const { getLogger } = require('../../shared/logging');
+
+/**
+ * Updates public advisories set for automatic publishing or expiry
+ */
+exports.triggerAdvisories = async function () {
+  const logger = getLogger();
+  try {
+    await cmsAxios.post("/api/public-advisories/trigger-scheduled", {});
+  } catch (error) {
+    logger.error(`advisory-scheduling/scripts/triggerScheduled.js failed: ${error}`);
+  }
+};
