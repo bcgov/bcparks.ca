42a7cdd0958179a65268ff491503fb07523dd0a3
Mike Olund <mike@oxd.com>
2024-02-20 17:25:01 -0800

    CM-1250: Fixed build

--- src/cms/config/server.js
+++ src/cms/config/server.js
@@ -1,13 +1,7 @@
-const cronTasks = require("./cron-tasks");
-
 module.exports = ({ env }) => ({
   host: env("STRAPI_HOST", "0.0.0.0"),
   port: env.int("STRAPI_PORT", 1337),
   url: env("STRAPI_EXTERNAL_URL", "http://localhost:1337"),
-  cron: {
-    enabled: env.bool("CRON_ENABLED", true),
-    tasks: cronTasks,
-  },
   app: {
     keys: env.array('APP_KEYS'),
   },
