3a712427b91f2244eac221346df9d198c54f0826
ayumi <ayumi@oxd.com>
2024-02-27 13:56:14 -0800

    CM-1282: Add small fix

--- src/gatsby/src/components/search/parkCard.js
+++ src/gatsby/src/components/search/parkCard.js
@@ -7,7 +7,7 @@ import ExpandCircleDownIcon from '@mui/icons-material/ExpandCircleDown'
 import ParkAccessStatus from "../../components/park/parkAccessStatus"
 
 import parksLogo from "../../images/park-card.png"
-import campingIcon from "../../../static/icons/vehicle-accessible-camping.svg"
+import campingIcon from "../../../static/icons/frontcountry-camping.svg"
 import backcountryCampingIcon from "../../../static/icons/wilderness-camping.svg"
 import hikingIcon from "../../../static/icons/hiking.svg"
 import picincIcon from "../../../static/icons/picnic-areas.svg"
