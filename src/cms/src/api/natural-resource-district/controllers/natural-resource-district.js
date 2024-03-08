ff826896c42ac5838371b58509ceb42211ec71fa
Mike Olund <mike@oxd.com>
2024-03-08 15:41:45 -0800

    CM-1223: Add natural resource district data

---
+++ src/cms/src/api/natural-resource-district/controllers/natural-resource-district.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * natural-resource-district controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::natural-resource-district.natural-resource-district');
