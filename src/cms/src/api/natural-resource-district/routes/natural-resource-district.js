198ca3e4037a4231906988d1214a21c776d2a925
Michael Olund <mike@oxd.com>
2024-03-15 11:22:27 -0700

    CM-1223: Add natural resource district data (#1266)

---
+++ src/cms/src/api/natural-resource-district/routes/natural-resource-district.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * natural-resource-district router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::natural-resource-district.natural-resource-district');
---
+++ src/cms/src/api/natural-resource-district/services/natural-resource-district.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * natural-resource-district service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::natural-resource-district.natural-resource-district');
