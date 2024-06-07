1016f8be8fae8a162056f92c4fc69728a8e4b54b
Ayumi Tanaka <90415568+ayumi-oxd@users.noreply.github.com>
2024-06-07 11:10:11 -0700

    CMS-27: Update park overview to highlights in this park (#1320)
    * CMS-27: Update park overview to highlights in this park
    
    * CMS-27: Change toggle button wording

--- src/gatsby/src/components/park/parkOverview.js
+++ src/gatsby/src/components/park/parkOverview.js
@@ -1,9 +1,5 @@
 import React, { useState, useEffect, useRef } from "react"
-
-import { capitalizeFirstLetter } from "../../utils/helpers";
-
 import HtmlContent from "./htmlContent"
-
 import * as cheerio from 'cheerio';
 
 const PREFIX = 'park-overview';
@@ -16,37 +12,50 @@ const classes = {
 export default function ParkOverview({ data: parkOverview, type }) {
   const [expanded, setExpanded] = useState(false)
   const [height, setHeight] = useState(0)
+  const [sectionHeight, setSectionHeight] = useState(0)
   const ref = useRef(null)
-
-  useEffect(() => {
-    setHeight(ref.current.clientHeight)
-  }, [expanded])
-
   const isLong = height > 259
 
   const $ = cheerio.load(parkOverview);
   $('a').attr('tabindex', '-1')
   const collapsedParkOverview = $.html()
+  const hasHr = $('hr').length > 0
+
+  useEffect(() => {
+    setHeight(ref.current.clientHeight)
+  }, [expanded])
+
+  useEffect(() => {
+    const h2 = document.querySelector('h2.section-heading');
+    const hr = document.querySelector('hr');
+    // height from the <h2> to the <hr>
+    const height = hr?.getBoundingClientRect().top - h2.getBoundingClientRect().top;
+    setSectionHeight(height)
+  }, [])
 
   return (
     <div id="park-overview-container" className="anchor-link">
-      <div className={expanded ? classes.expanded : classes.collapsed} ref={ref}>
-        <h2 className="section-heading">{capitalizeFirstLetter(`${type} overview`)}</h2>
+      <div
+        ref={ref}
+        className={expanded ? classes.expanded : classes.collapsed}
+        style={{ maxHeight: expanded ? "none" : `${hasHr ? sectionHeight : 260}px` }}
+      >
+        <h2 className="section-heading">Highlights in this {type}</h2>
         <HtmlContent className="park-overview-html">
           {expanded ? parkOverview : collapsedParkOverview}
         </HtmlContent>
       </div>
-      {isLong && 
+      {(hasHr || isLong) &&
         <a
           href="#park-overview-container"
           className="park-overview-link expand-icon"
           onClick={() => {
             setExpanded(!expanded)
           }}>
-        {expanded ? "Read less" : "Read more"}
-        <i className={`fa fa-angle-down ${expanded ? "open" : "close"}`}></i>
-      </a>
+          Show {expanded ? "less" : "more"} highlights
+          <i className={`fa fa-angle-down ${expanded ? "open" : "close"}`}></i>
+        </a>
       }
-      </div>
+    </div>
   );
 }
