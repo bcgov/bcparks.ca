d90cc611f361b404d31c37677550a665805a4897
ayumi <ayumi@oxd.com>
2024-06-20 10:19:20 -0700

    CMS-225: Remove website collection

--- src/cms/src/api/website/controllers/website.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-/**
- * website controller
- */
-
-const { createCoreController } = require("@strapi/strapi").factories;
-
-module.exports = createCoreController("api::website.website");
