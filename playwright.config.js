c56a34cdf48fd7e63969abd9c6021ecf3a452395
adrianneOXD <107157705+adrianneOXD@users.noreply.github.com>
2024-07-26 10:51:02 -0700

    Updating test and playwright config page

--- playwright.config.js
+++ playwright.config.js
@@ -11,6 +11,7 @@ const { defineConfig, devices } = require('@playwright/test');
  * @see https://playwright.dev/docs/test-configuration
  */
 module.exports = defineConfig({
+  timeout: 60000,
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
