b00db2d59fb4def71913a008721992d6d195919c
adrianneOXD <107157705+adrianneOXD@users.noreply.github.com>
2024-07-31 11:09:45 -0700

    Adding firefox

--- playwright.config.js
+++ playwright.config.js
@@ -37,13 +37,13 @@ module.exports = defineConfig({
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
-    }
-/*
+    },
+
     {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
-    },
-
+    }
+/*
     {
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
