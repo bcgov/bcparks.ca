bf5c7d349eb1e4170571880f243b65e4d61402d3
Ayumi Tanaka <ayumi@oxd.com>
2024-07-31 13:59:33 -0700

    CMS-236: Add another id to the about section subheading as a fallback

--- src/gatsby/src/components/park/about.js
+++ src/gatsby/src/components/park/about.js
@@ -5,6 +5,8 @@ import HtmlContent from "./htmlContent"
 export default function About({ parkType, natureAndCulture, biogeoclimaticZones, terrestrialEcosections, marineEcosections }) {
   return (
     <div id="about-this-park" className="anchor-link" >
+      {/* id="park-about-container" should be removed once it's removed from the contents */}
+      <span id="park-about-container"></span>
       {/* id="park-nature-and-culture-container" should be removed once it's removed from the contents */}
       <h2 id="park-nature-and-culture-container" className="section-heading">
         About this {parkType}
