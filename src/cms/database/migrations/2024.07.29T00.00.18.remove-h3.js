c29d13e407192799d5e7ccac6dd2e405ce43db9e
Mike Olund <mike@oxd.com>
2024-07-31 08:05:56 -0700

    CMS-33: Remove <p>&nbsp;</p> from content

--- src/cms/database/migrations/2024.07.29T00.00.18.remove-h3.js
+++ src/cms/database/migrations/2024.07.29T00.00.18.remove-h3.js
@@ -24,6 +24,11 @@ async function up(knex) {
     await knex.raw(`update park_activities set description = replace(description, '<h4', '<h5') where (description like '%<h3%' and description like '%<h4%') and description not like '%<h5%'`);
     await knex.raw(`update park_activities set description = replace(description, '<h3', '<h4') where description like '%<h3%' and description not like '%<h4%'`);
     await knex.raw(`update park_activities set description = replace(description, '</h3', '</h4') where description like '%</h3%' and description not like '%</h4%'`);
+
+    // remove empty lines
+    await knex.raw(`update park_camping_types set description = replace(description,'<p>&nbsp;</p>','') where description like '%<p>&nbsp;</p>%'`);
+    await knex.raw(`update park_facilities set description = replace(description,'<p>&nbsp;</p>','') where description like '%<p>&nbsp;</p>%'`);
+    await knex.raw(`update park_activities set description = replace(description,'<p>&nbsp;</p>','') where description like '%<p>&nbsp;</p>%'`);
   }
 }
 
