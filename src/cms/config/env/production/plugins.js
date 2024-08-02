73c538905fa3528a7c3937cbf0b7366b659d0963
Michael Olund <mike@oxd.com>
2024-08-02 16:11:12 -0700

    CMS-276: Update plugins.js

--- src/cms/config/env/production/plugins.js
+++ src/cms/config/env/production/plugins.js
@@ -7,8 +7,9 @@ module.exports = ({ env }) => ({
           accessKeyId: env("S3_ACCESS_KEY_ID"),
           secretAccessKey: env("S3_ACCESS_SECRET"),
         },
-        endpoint: "https://" + env("S3_ENDPOINT"),
-        s3ForcePathStyle: true,
+        region: "cannot-be-blank",
+        endpoint: `https://${env("S3_ENDPOINT")}`,
+        forcePathStyle: true,
         params: {
           Bucket: env("S3_BUCKET"),
         },
