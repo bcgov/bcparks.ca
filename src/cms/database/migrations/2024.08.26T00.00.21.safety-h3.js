90d6860d4ee49b1bf5d9fc149286a9b886df0ea6
Mike Olund <mike@oxd.com>
2024-08-26 11:13:52 -0700

    CMS-371: Fix tests

--- src/cms/database/migrations/2024.08.26T00.00.21.safety-h3.js
+++ src/cms/database/migrations/2024.08.26T00.00.21.safety-h3.js
@@ -2,19 +2,21 @@
 
 async function up(knex) {
   // turn h3 into h4 and turn h4 into h5
-  await knex.raw(`update protected_areas set safety_info = replace(safety_info, '<h3', '<h4') where safety_info like '%<h3%' and safety_info not like '%<h4%'`);
-  await knex.raw(`update protected_areas set safety_info = replace(safety_info, '</h3', '</h4') where safety_info like '%</h3%' and safety_info not like '%</h4%'`);
-  await knex.raw(`update protected_areas set safety_info = replace(safety_info, '</h4', '</h5') where (safety_info like '%</h3%' and safety_info like '%</h4%') and safety_info not like '%</h5%'`);
-  await knex.raw(`update protected_areas set safety_info = replace(safety_info, '<h4', '<h5') where (safety_info like '%<h3%' and safety_info like '%<h4%') and safety_info not like '%<h5%'`);
-  await knex.raw(`update protected_areas set safety_info = replace(safety_info, '<h3', '<h4') where safety_info like '%<h3%' and safety_info not like '%<h4%'`);
-  await knex.raw(`update protected_areas set safety_info = replace(safety_info, '</h3', '</h4') where safety_info like '%</h3%' and safety_info not like '%</h4%'`);
+  if (await knex.schema.hasTable('protected_areas')) {
+    await knex.raw(`update protected_areas set safety_info = replace(safety_info, '<h3', '<h4') where safety_info like '%<h3%' and safety_info not like '%<h4%'`);
+    await knex.raw(`update protected_areas set safety_info = replace(safety_info, '</h3', '</h4') where safety_info like '%</h3%' and safety_info not like '%</h4%'`);
+    await knex.raw(`update protected_areas set safety_info = replace(safety_info, '</h4', '</h5') where (safety_info like '%</h3%' and safety_info like '%</h4%') and safety_info not like '%</h5%'`);
+    await knex.raw(`update protected_areas set safety_info = replace(safety_info, '<h4', '<h5') where (safety_info like '%<h3%' and safety_info like '%<h4%') and safety_info not like '%<h5%'`);
+    await knex.raw(`update protected_areas set safety_info = replace(safety_info, '<h3', '<h4') where safety_info like '%<h3%' and safety_info not like '%<h4%'`);
+    await knex.raw(`update protected_areas set safety_info = replace(safety_info, '</h3', '</h4') where safety_info like '%</h3%' and safety_info not like '%</h4%'`);
 
-  await knex.raw(`update protected_areas set special_notes = replace(special_notes, '<h3', '<h4') where special_notes like '%<h3%' and special_notes not like '%<h4%'`);
-  await knex.raw(`update protected_areas set special_notes = replace(special_notes, '</h3', '</h4') where special_notes like '%</h3%' and special_notes not like '%</h4%'`);
-  await knex.raw(`update protected_areas set special_notes = replace(special_notes, '</h4', '</h5') where (special_notes like '%</h3%' and special_notes like '%</h4%') and special_notes not like '%</h5%'`);
-  await knex.raw(`update protected_areas set special_notes = replace(special_notes, '<h4', '<h5') where (special_notes like '%<h3%' and special_notes like '%<h4%') and special_notes not like '%<h5%'`);
-  await knex.raw(`update protected_areas set special_notes = replace(special_notes, '<h3', '<h4') where special_notes like '%<h3%' and special_notes not like '%<h4%'`);
-  await knex.raw(`update protected_areas set special_notes = replace(special_notes, '</h3', '</h4') where special_notes like '%</h3%' and special_notes not like '%</h4%'`);
+    await knex.raw(`update protected_areas set special_notes = replace(special_notes, '<h3', '<h4') where special_notes like '%<h3%' and special_notes not like '%<h4%'`);
+    await knex.raw(`update protected_areas set special_notes = replace(special_notes, '</h3', '</h4') where special_notes like '%</h3%' and special_notes not like '%</h4%'`);
+    await knex.raw(`update protected_areas set special_notes = replace(special_notes, '</h4', '</h5') where (special_notes like '%</h3%' and special_notes like '%</h4%') and special_notes not like '%</h5%'`);
+    await knex.raw(`update protected_areas set special_notes = replace(special_notes, '<h4', '<h5') where (special_notes like '%<h3%' and special_notes like '%<h4%') and special_notes not like '%<h5%'`);
+    await knex.raw(`update protected_areas set special_notes = replace(special_notes, '<h3', '<h4') where special_notes like '%<h3%' and special_notes not like '%<h4%'`);
+    await knex.raw(`update protected_areas set special_notes = replace(special_notes, '</h3', '</h4') where special_notes like '%</h3%' and special_notes not like '%</h4%'`);
+  }
 }
 
 module.exports = { up };
