cd648811fa21728121baa27d0018701102438713
Ayumi Tanaka <90415568+ayumi-oxd@users.noreply.github.com>
2024-08-27 11:55:14 -0700

    CMS-279: Add reservation buttons in camping section (#1437)
    * CMS-279: Add reservation buttons in camping section
    
    * CMS-279: Update reservation buttons label
    
    * CMS-370: Move dates of operation from camping accordion

--- src/gatsby/src/components/park/parkDates.js
+++ src/gatsby/src/components/park/parkDates.js
@@ -8,6 +8,88 @@ import { faChevronUp, faChevronDown } from "@fortawesome/free-solid-svg-icons"
 import HtmlContent from "./htmlContent"
 import SubArea from "./subArea"
 
+export const ReservationButtons = ({ campingTypeCode, parkOperation }) => {
+  const reservationUrlRules = {
+    "backcountry-camping": {
+      buttons: [
+        {
+          label: "Get permit",
+          fieldName: "backcountryPermitUrl"
+        },
+        {
+          label: "Make a reservation",
+          fieldName: "backcountryReservationUrl"
+        },
+        {
+          label: "Reserve canoe circuit",
+          fieldName: "canoeCircuitReservationUrl"
+        },
+        {
+          label: "Book groupsite",
+          fieldName: "backcountryGroupReservationUrl"
+        },
+        {
+          label: "Book shelter",
+          fieldName: "backcountryShelterReservationUrl"
+        }
+      ]
+    },
+    "wilderness-camping": {
+      buttons: [
+        {
+          label: "Book wilderness area",
+          fieldName: "backcountryWildernessReservationUrl"
+        }
+      ]
+    },
+    "frontcountry-camping": {
+      buttons: [
+        {
+          label: "Book camping",
+          fieldName: "frontcountryReservationUrl"
+        }
+      ]
+    },
+    "group-camping": {
+      buttons: [
+        {
+          label: "Book groupsite",
+          fieldName: "frontcountryGroupReservationUrl"
+        }
+      ]
+    },
+    "cabins-huts": {
+      buttons: [
+        {
+          label: "Book cabin",
+          fieldName: "frontcountryCabinReservationUrl"
+        }
+      ]
+    }
+  }
+
+  const getReservationButtons = (code) => {
+    if (reservationUrlRules[code]) {
+      return reservationUrlRules[code].buttons
+    }
+  }
+
+  return (
+    getReservationButtons(campingTypeCode)?.length > 0 &&
+    getReservationButtons(campingTypeCode).map((button, index) => (
+      parkOperation && parkOperation[button.fieldName] && (
+        <a
+          key={index}
+          href={parkOperation[button.fieldName]}
+          className="btn btn-secondary"
+        >
+          {button.label}
+        </a>
+      )
+    ))
+  )
+}
+
 export const AccordionList = ({ eventKey, subArea, openAccordions, toggleAccordion, itemCount }) => {
   return (
     <Accordion
@@ -48,7 +130,7 @@ export const AccordionList = ({ eventKey, subArea, openAccordions, toggleAccordi
   )
 }
 
-export default function ParkDates({ data }) {
+export default function ParkDates({ data, parkOperation }) {
   const subAreas = data.subAreas.sort((a, b) => (a.parkSubArea >= b.parkSubArea ? 1 : -1))
   const [openAccordions, setOpenAccordions] = useState({})
 
@@ -80,13 +162,22 @@ export default function ParkDates({ data }) {
     }
   }, [subAreas.length])
 
+  if (subAreas.length === 0) return null
+
   return (
     <>
       {subAreas.length > 0 && (
         <>
-          <h4>
-            {data.campingType.pluralName}
-          </h4>
+          <Row className="align-items-center my-4">
+            <Col>
+              <h4 className="mb-0">
+                {data.campingType.pluralName}
+              </h4>
+            </Col>
+            <Col className="reservation-button-container" xs="12" sm="auto">
+              <ReservationButtons campingTypeCode={data.campingType.campingTypeCode} parkOperation={parkOperation} />
+            </Col>
+          </Row>
           <Row className="mb-5">
             <Col>
               <>
--- src/gatsby/src/templates/park.js
+++ src/gatsby/src/templates/park.js
@@ -413,7 +413,10 @@ export default function ParkTemplate({ data }) {
             )}
             {menuItems[5].visible && (
               <div ref={facilityRef} className="w-100">
-                <ParkFacility data={activeFacilities} />
+                <ParkFacility
+                  data={activeFacilities}
+                  groupPicnicReservationUrl={park.parkOperation?.groupPicnicReservationUrl}
+                />
               </div>
             )}
             {menuItems[6].visible && (
@@ -615,8 +618,17 @@ export const query = graphql`
         hasDayUsePass
         hasFirstComeFirstServed
         reservationUrl
-        backcountryPermitUrl
         dayUsePassUrl
+        frontcountryReservationUrl
+        frontcountryGroupReservationUrl
+        frontcountryCabinReservationUrl
+        backcountryReservationUrl
+        backcountryPermitUrl
+        backcountryGroupReservationUrl
+        backcountryWildernessReservationUrl
+        backcountryShelterReservationUrl
+        canoeCircuitReservationUrl
+        groupPicnicReservationUrl
         hasParkGate
         offSeasonUse
         openNote
