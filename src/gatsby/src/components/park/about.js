b8160298127ea7f3d7e45c2c4ef44d15a04471dd
Ayumi Tanaka <ayumi@oxd.com>
2024-08-27 09:25:28 -0700

    CMS-354: Update about section

--- src/gatsby/src/components/park/about.js
+++ src/gatsby/src/components/park/about.js
@@ -127,6 +127,47 @@ export default function About({
       <h2 id="park-nature-and-culture-container" className="section-heading">
         About this {parkType}
       </h2>
+      <div>
+        {biogeoclimaticZones?.length > 0 && (
+          <p className="ecological-list">
+            <a href="https://catalogue.data.gov.bc.ca/dataset/bec-zones-generalized-1-2m-">
+              Biogeoclimatic zone:
+            </a>
+            {biogeoclimaticZones.map((bioZone, index) => (
+              <span key={index}>
+                {bioZone.zone}
+                {biogeoclimaticZones.length > 1 && index + 1 !== biogeoclimaticZones.length && (", ")}
+              </span>
+            ))}
+          </p>
+        )}
+        {terrestrialEcosections?.length > 0 && (
+          <p className="ecological-list">
+            <a href="https://catalogue.data.gov.bc.ca/dataset/ecosections-ecoregion-ecosystem-classification-of-british-columbia">
+              Terrestrial ecosection:
+            </a>
+            {terrestrialEcosections.map((terreSection, index) => (
+              <span key={index}>
+                {terreSection.terrestrialEcosection}
+                {terrestrialEcosections.length > 1 && index + 1 !== terrestrialEcosections.length && (",")}
+              </span>
+            ))}
+          </p>
+        )}
+        {marineEcosections?.length > 0 && (
+          <p className="ecological-list">
+            <a href="https://catalogue.data.gov.bc.ca/dataset/marine-ecosections-coastal-resource-information-management-system-crims">
+              Marine ecosection:
+            </a>
+            {marineEcosections.map((marineSection, index) => (
+              <span key={index}>
+                {marineSection.marineEcosection}
+                {marineEcosections.length > 1 && index + 1 !== marineEcosections.length && (",")}
+              </span>
+            ))}
+          </p>
+        )}
+      </div>
       {/* display conservation/culturalHeritage/history/wildlife accordion, otherwise display natureAndCulture */}
       {dataList.length > 0 ? (
         <Row>
@@ -163,59 +204,6 @@ export default function About({
       ) : (
         <HtmlContent>{natureAndCulture}</HtmlContent>
       )}
-      <ul>
-        {biogeoclimaticZones?.length > 0 && (
-          <li className="ecological-list">
-            <strong>
-              <a href="https://catalogue.data.gov.bc.ca/dataset/bec-zones-generalized-1-2m-">
-                Biogeoclimatic zone:
-              </a>
-            </strong>
-            <ul>
-              {biogeoclimaticZones.map((bioZone, index) => (
-                <li key={index}>
-                  {bioZone.zone}
-                  {biogeoclimaticZones.length > 1 && index + 1 !== biogeoclimaticZones.length && (",")}
-                </li>
-              ))}
-            </ul>
-          </li>
-        )}
-        {terrestrialEcosections?.length > 0 && (
-          <li className="ecological-list">
-            <strong>
-              <a href="https://catalogue.data.gov.bc.ca/dataset/ecosections-ecoregion-ecosystem-classification-of-british-columbia">
-                Terrestrial ecosection:
-              </a>
-            </strong>
-            <ul>
-              {terrestrialEcosections.map((terreSection, index) => (
-                <li key={index}>
-                  {terreSection.terrestrialEcosection}
-                  {terrestrialEcosections.length > 1 && index + 1 !== terrestrialEcosections.length && (",")}
-                </li>
-              ))}
-            </ul>
-          </li>
-        )}
-        {marineEcosections?.length > 0 && (
-          <li className="ecological-list">
-            <strong>
-              <a href="https://catalogue.data.gov.bc.ca/dataset/marine-ecosections-coastal-resource-information-management-system-crims">
-                Marine ecosection:
-              </a>
-            </strong>
-            <ul>
-              {marineEcosections.map((marineSection, index) => (
-                <li key={index}>
-                  {marineSection.marineEcosection}
-                  {marineEcosections.length > 1 && index + 1 !== marineEcosections.length && (",")}
-                </li>
-              ))}
-            </ul>
-          </li>
-        )}
-      </ul>
     </div>
   )
 }
