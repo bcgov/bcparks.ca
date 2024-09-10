275f4928d54000b4536eb9b3e12a6bcc46972149
Ayumi Tanaka <90415568+ayumi-oxd@users.noreply.github.com>
2024-09-09 17:29:20 -0700

    CMS-361: Update page menu on mobile (#1465)
    * CMS-361: Update page menu on mobile
    
    * CMS-361: Small fix

--- src/gatsby/src/components/pageContent/pageMenu.js
+++ src/gatsby/src/components/pageContent/pageMenu.js
@@ -41,7 +41,7 @@ export default function PageMenu({ pageSections, activeSection, menuStyle }) {
           onChange={handleSectionChange}
           title="mobile-navigation"
         >
-          <option value={0}>Table of Contents</option>
+          <option value="" disabled>Table of Contents</option>
           {pageSections.filter(s => s.visible).map(
             section =>
               <option key={section.sectionIndex} value={section.sectionIndex}>
