33fa262d785d94f6183f8bce298b271c00549b19
adrianneOXD <107157705+adrianneOXD@users.noreply.github.com>
2024-10-01 16:27:49 -0700

    Adding a comment on automated test manuji branch at 4:27PM PST

--- tests/find_a_park_page.spec.js
+++ tests/find_a_park_page.spec.js
@@ -184,5 +184,6 @@ test.describe('Find a park page tests', async ()=>{
     });
 
     // Adrianne testing at 4:07PM PST
+    // Adrianne testing at 4:27PM PST on automated-tests-manuji branch
 });
 
