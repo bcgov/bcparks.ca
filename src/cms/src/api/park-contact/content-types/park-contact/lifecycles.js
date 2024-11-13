5a6da58465fe95a732a54241f29a05bfe837e4a6
Ayumi Tanaka <ayumi@oxd.com>
2024-11-12 18:07:53 -0800

    CMS-472: Update park contact lifecycle in Strapi

--- src/cms/src/api/park-contact/content-types/park-contact/lifecycles.js
+++ src/cms/src/api/park-contact/content-types/park-contact/lifecycles.js
@@ -6,12 +6,24 @@ const updateName = async (data, where) => {
         const parkContact = await strapi.entityService.findOne(
             "api::park-contact.park-contact", id, { populate: '*' }
         )
-        const protectedArea = parkContact.protectedArea
+        let protectedArea = parkContact.protectedArea
         const parkOperatorContact = parkContact.parkOperatorContact
+
+        // Check if new protectedArea is being added
+        if (data?.protectedArea?.connect?.length > 0) {
+          protectedArea = await strapi.entityService.findOne(
+            "api::protected-area.protected-area", data?.protectedArea.connect[0].id
+          )
+        // Check if current protectedArea is being removed
+        } else if (data?.protectedArea?.disconnect?.length > 0) {
+          protectedArea = { orcs: 0 }
+        }
 
         data.name = ""
         if (protectedArea) {
             data.name = protectedArea.orcs
+        } else {
+            data.name = 0
         }
         if (parkOperatorContact) {
             data.name += ":"
