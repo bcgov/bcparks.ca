b9bd7633fbf185d6fb04068dda9d2c31418172d8
Ayumi Tanaka <ayumi@oxd.com>
2024-11-25 17:45:21 -0800

    CMS-297: Add gateOpen24Hours to parkOperation

--- src/gatsby/src/templates/park.js
+++ src/gatsby/src/templates/park.js
@@ -670,6 +670,7 @@ export const query = graphql`
         gateCloseTime
         gateOpensAtDawn
         gateClosesAtDusk
+        gateOpen24Hours
         hasGroupPicnicReservations
         hasCanoeCircuitReservations
         hasFrontcountryReservations
