1f6a99bf1e12a8601a67df8cb9f12d4844d6c085
Ayumi Tanaka <ayumi@oxd.com>
2024-12-11 09:21:30 -0800

    CMS-559: Add time zone to emergency-alert time fields

--- src/cms/src/api/emergency-alert/content-types/emergency-alert/lifecycles.js
+++ src/cms/src/api/emergency-alert/content-types/emergency-alert/lifecycles.js
@@ -1,18 +1,22 @@
 "use strict";
 
+const formatDateToPacificTime = (dateString) => {
+  const date = new Date(dateString)
+  const localDate = date.toLocaleDateString('en-CA', { timeZone: 'America/Los_Angeles' })
+  return new Date(localDate).toISOString().split('T')[0]
+}
+
 module.exports = {
     beforeCreate(event) {
         const { data } = event.params
-        const date = new Date(data.createdAt)
-        const createdDate = date.toISOString().split('T')[0]
+        const createdDate = formatDateToPacificTime(data.createdAt)
         if (data.isActive === true) {
             data.activeDate = createdDate
         }
     },
     beforeUpdate(event) {
         const { data } = event.params
-        const date = new Date(data.updatedAt)
-        const updatedDate = date.toISOString().split('T')[0]
+        const updatedDate = formatDateToPacificTime(data.updatedAt)
         if (data.isActive === true) {
             data.activeDate = updatedDate
         }
