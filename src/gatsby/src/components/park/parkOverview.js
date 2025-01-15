c273bdd4120e030a7348e32d72ae4a9c5abcfc3c
Ayumi Tanaka <ayumi@oxd.com>
2025-01-14 18:13:26 -0800

    CMS-34: Add park name audio type

--- src/gatsby/src/components/park/parkOverview.js
+++ src/gatsby/src/components/park/parkOverview.js
@@ -19,6 +19,7 @@ export default function ParkOverview({ description, type, audio }) {
   const hasHr = $('hr').length > 0
   const hrAtEnd = description.trim().endsWith('<hr>')
   const hasExpandCondition = (hasHr || isLong) && !isMedium && !hrAtEnd
+  const hasAudio = audio?.title && audio?.url && audio?.credit
 
   useEffect(() => {
     if (!hasHr && ref.current.clientHeight > 260) {
@@ -48,7 +49,7 @@ export default function ParkOverview({ description, type, audio }) {
         <HtmlContent ariaHidden={!expanded} className="park-overview-html">
           {expanded ? description : collapsedDescription}
         </HtmlContent>
-        <AudioButton audio={audio} />
+        {hasAudio && <AudioButton audio={audio} />}
       </div>
       {hasExpandCondition &&
         <button
