910c9a6947fed05d396dbe0c3bf8b9f67fe0262f
Ayumi Tanaka <ayumi@oxd.com>
2025-02-05 13:10:17 -0800

    CMS-34: Update relation between audio clip and protected area

--- src/gatsby/gatsby-config.js
+++ src/gatsby/gatsby-config.js
@@ -57,6 +57,7 @@ module.exports = {
           "search-city",
           "management-document",
           "emergency-alert",
+          "audio-clip",
           {
             singularName: "page",
             queryParams: {
