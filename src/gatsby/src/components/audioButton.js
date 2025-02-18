abb7ed6ee6a483115fe0a74c3d1f69ac62dae1fb
Ayumi Tanaka <ayumi@oxd.com>
2025-02-18 12:39:00 -0800

    CMS-34: Update audio transcript url function

--- src/gatsby/src/components/audioButton.js
+++ src/gatsby/src/components/audioButton.js
@@ -50,27 +50,42 @@ export default function AudioButton({ audio }) {
     div.innerHTML = html
     return div.textContent || div.innerText || ""
   }
-  const createVttContent = transcript => {
+  const createVttContent = (transcript, duration) => {
     const plainTextTranscript = stripHtmlTags(transcript)
-    return `WEBVTT\n\n1\n00:00:00.000 --> 00:00:10.000\n${plainTextTranscript}`
+    return `WEBVTT\n\n1\n00:00:00.000 --> ${duration}\n${plainTextTranscript}`
   }
   const createBlobUrl = content => {
     const blob = new Blob([content], { type: "text/vtt" })
     return URL.createObjectURL(blob)
   }
+  // format duration in seconds to HH:MM:SS:mmm
+  // e.g. 14.689... > 00:00:14.690
+  const formatDuration = time => {
+    const minutes = Math.floor(time / 60)
+    const seconds = time % 60
+    return `00:${minutes.toString().padStart(2, "0")}:
+      ${seconds.toFixed(3).padStart(6, "0")}`
+  }
 
   // effects
   useEffect(() => {
-    if (hasTranscript) {
-      const vttContent = createVttContent(audio.transcript.data.transcript)
-      const url = createBlobUrl(vttContent)
-      setTrackSrc(url)
-      return () => {
-        URL.revokeObjectURL(url)
-      }
+    if (hasTranscript && audio?.url) {
+      const audioElement = new Audio(audio.url)
+      audioElement.addEventListener("loadedmetadata", () => {
+        const duration = formatDuration(audioElement.duration)
+        const vttContent = createVttContent(
+          audio.transcript.data.transcript,
+          duration
+        )
+        const url = createBlobUrl(vttContent)
+        setTrackSrc(url)
+        return () => {
+          URL.revokeObjectURL(url)
+        }
+      })
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [hasTranscript])
+  }, [hasTranscript, audio?.url])
 
   return (
     <>
