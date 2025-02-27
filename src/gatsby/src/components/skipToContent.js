c4506f61ab34729125423625d4ed3187665f2b00
Ayumi Tanaka <ayumi@oxd.com>
2025-02-27 14:16:43 -0800

    CMS-668: Hide 'Skip to main content' on mobile

--- src/gatsby/src/components/skipToContent.js
+++ src/gatsby/src/components/skipToContent.js
@@ -2,6 +2,6 @@ import React from "react"
 
 export default function SkipToContent() {
   return (
-    <a className="visually-hidden-focusable" href="#main-content">Skip to main content</a>
+    <a className="d-none d-lg-inline-block visually-hidden-focusable" href="#main-content">Skip to main content</a>
   )
 }
