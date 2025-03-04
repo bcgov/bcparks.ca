a0849795d76e96ddd06f8ad66b6f2783f25418ea
Ayumi Tanaka <90415568+ayumi-oxd@users.noreply.github.com>
2025-03-04 12:06:07 -0800

    CMS-682: Add park feature date collection (#1574)

---
+++ src/cms/src/api/park-feature-date/routes/park-feature-date.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * park-feature-date router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::park-feature-date.park-feature-date');
