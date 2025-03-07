6231fe0d0bc5aab647f38ca05693ac94e249d802
Ayumi Tanaka <90415568+ayumi-oxd@users.noreply.github.com>
2025-03-07 11:51:00 -0800

    CMS-668: Fix a broken link accessibility error (#1590)

--- src/gatsby/src/components/header.js
+++ src/gatsby/src/components/header.js
@@ -3,12 +3,13 @@ import React from "react"
 import SkipToContent from "./skipToContent"
 import MegaMenu from "./megaMenu.js"
 import EmergencyAlert from "./emergencyAlert"
+import { useScreenSize } from "../utils/helpers"
 
 export default function Header({ content = [] }) {
-
+  const screenSize = useScreenSize()
   return (
     <>
-      <SkipToContent></SkipToContent>
+      {screenSize.width > 991 && <SkipToContent></SkipToContent>}
       <EmergencyAlert />
       <MegaMenu content={content} menuMode="responsive" />
     </>
