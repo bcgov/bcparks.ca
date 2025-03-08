ad4ae33317c7cb9d37fda897e1bffc5067d9c08d
Ayumi Tanaka <ayumi@oxd.com>
2025-03-07 16:52:00 -0800

    CMS-646: Update table of contents

--- src/gatsby/src/components/pageContent/pageMenu.js
+++ src/gatsby/src/components/pageContent/pageMenu.js
@@ -3,43 +3,33 @@ import React from "react"
 import "../../styles/pageContent/pageMenu.scss"
 
 export default function PageMenu({ pageSections, activeSection, menuStyle }) {
-  if (menuStyle === "nav") {
-    return (
-      <nav className="navbar">
-        <nav id="section-navbar" className="nav">
-          {pageSections.filter(s => s.visible).map(section => (
-            <a
-              className="nav-link"
-              active-section={
-                activeSection === section.sectionIndex ? "true" : "false"
-              }
-              key={section.sectionIndex}
-              href={section.link}
-            >
-              {section.display}
-            </a>
-          ))}
-        </nav>
-      </nav>
-    )
-  }
-
-  if (menuStyle === "list") {
-    return (
-      <div className="section-list-container">
-        <p><b>On this page</b></p>
-        <ul>
-          {pageSections.filter(section => section.visible).map(section => (
+  return (
+    <div className="page-menu--container">
+      <div className="menu-header">
+        <b>On this page</b>
+      </div>
+      <ul>
+        {pageSections
+          .filter(section => section.visible)
+          .map(section => (
             <li key={section.sectionIndex}>
-              <a href={section.link}>
-                {section.display}
-              </a>
+              {menuStyle === "nav" && (
+                <a
+                  active-section={
+                    activeSection === section.sectionIndex ? "true" : "false"
+                  }
+                  key={section.sectionIndex}
+                  href={section.link}
+                >
+                  {section.display}
+                </a>
+              )}
+              {menuStyle === "list" && (
+                <a href={section.link}>{section.display}</a>
+              )}
             </li>
           ))}
-        </ul>
-      </div>
-    )
-  }
-
-  return null
+      </ul>
+    </div>
+  )
 }
