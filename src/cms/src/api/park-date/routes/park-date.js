ca05681b606d82084cc9727af033854ae462b275
Ayumi Tanaka <90415568+ayumi-oxd@users.noreply.github.com>
2025-09-09 08:49:00 -0700

    CMS-1038: Create park-date and park-date-type collection (#1642)

---
+++ src/cms/src/api/park-date/routes/park-date.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * park-date router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::park-date.park-date');
---
+++ src/cms/src/api/park-date/services/park-date.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * park-date service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::park-date.park-date');
