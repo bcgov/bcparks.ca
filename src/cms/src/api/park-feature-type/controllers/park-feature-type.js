d59927d98fe84431097591094afecda8ce438821
Ayumi Tanaka <90415568+ayumi-oxd@users.noreply.github.com>
2025-09-16 15:21:01 -0700

    CMS-1154: Create park-feature and park-feature-type collection (#1644)
    * CMS-1154: Create park-feature and park-feature-type collection
    
    * CMS-1154: Add an area relation to feature
    
    * CMS-1154: Add orcsFeatureNumber to park-feature
    
    * CMS-1154: Add featureId to park-feature

---
+++ src/cms/src/api/park-feature-type/controllers/park-feature-type.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * park-feature-type controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::park-feature-type.park-feature-type');
---
+++ src/cms/src/api/park-feature/controllers/park-feature.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * park-feature controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::park-feature.park-feature');
