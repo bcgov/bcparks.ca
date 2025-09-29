7d6e0a70dfcc7b5efb09de07322d0e3621f1d7ab
Michael Olund <mike@oxd.com>
2025-09-29 16:54:43 -0700

    CMS-1154: Fix incorrectly named groupsSites field (#1653)

--- src/cms/database/migrations/2025.09.26T00.00.29.park-areas-group-sites-rename.js
+++ src/cms/database/migrations/2025.09.26T00.00.29.park-areas-group-sites-rename.js
@@ -2,15 +2,21 @@
 
 /*
  Renames the "group_site" column to "group_sites" in the "park_areas"
- table. This migration only affects environments where the column is
- incorrectly named "group_site". If the column already has the correct
- name, no changes will be made.
+ and "park_features" tables. This migration only affects environments
+ where the column is incorrectly named "group_site". If the column
+ already has the correct name, no changes will be made.
 */
 
 module.exports = {
   async up(knex) {
     if (await knex.schema.hasColumn("park_areas", "group_site")) {
       await knex.schema.table("park_areas", (table) => {
+        table.renameColumn("group_site", "group_sites");
+      });
+    }
+
+    if (await knex.schema.hasColumn("park_features", "group_site")) {
+      await knex.schema.table("park_features", (table) => {
         table.renameColumn("group_site", "group_sites");
       });
     }
