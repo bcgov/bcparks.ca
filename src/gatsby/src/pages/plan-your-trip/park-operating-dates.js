d50d124c53d6d834df904b8588af2438448693b8
Duncan MacKenzie <duncan@oxd.com>
2025-10-28 15:08:56 -0700

    CMS-1232: Swap display order in template

--- src/gatsby/src/pages/plan-your-trip/park-operating-dates.js
+++ src/gatsby/src/pages/plan-your-trip/park-operating-dates.js
@@ -428,23 +428,20 @@ const ParkOperatingDatesPage = () => {
             </div>
           </div>
 
-          {/*
-            Display loading indicator while loading initial data,
-            or if the user changes the filter before the full dataset is loaded
-          */}
-          {(isInitialLoading || (isLoadingSubAreas && currentFilter !== "A")) ? (
-            // display the loading bar if subareas are loading
-            <div className="mt-5">
-              Loading...
-              <ProgressBar animated now={100} className="mt-2" />
-            </div>
-          ) : subAreaLoadError ? (
+          {subAreaLoadError ? (
             // display the error message if subareas failed to load
             <div className="mt-5">
               <h2 className="sub-heading">Something went wrong. Please try again later.</h2>
               <p>If the problem continues, please contact{" "}
                 <a href="mailto:parkinfo@gov.bc.ca">parkinfo@gov.bc.ca</a>.
               </p>
+            </div>
+          ) : (isInitialLoading || (isLoadingSubAreas && currentFilter !== "A")) ? (
+            // Display loading indicator while loading initial data,
+            // or if the user changes the filter before the full dataset is loaded
+            <div className="mt-5">
+              Loading...
+              <ProgressBar animated now={100} className="mt-2" />
             </div>
           ) : (
             // display the list of parks if subareas are loaded
