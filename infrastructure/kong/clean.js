bfbb43b4d51a3dbd92af11232d50d08b4aa493f5
Ayumi Tanaka <ayumi@oxd.com>
2025-12-09 15:28:29 -0800

    NOBUG: Update kong

--- infrastructure/kong/clean.js
+++ infrastructure/kong/clean.js
@@ -1,7 +1,7 @@
 /**
  * Helper script to help clean up the auto generated src/cms/extensions/documentation/documentation/1.0.0/full_documentation.json
  * for the Kong gwa client.
- * 
+ *
  * Mainly used to remove non-publicly used Strapi endpoints in the OpenAPI spec
  */
 
@@ -25,7 +25,7 @@ Object.keys(paths).forEach((item) => {
   Object.keys(paths[item])
     .filter((key) => ["post", "delete", "put"].includes(key))
     .forEach((key) => delete paths[item][key]);
-  
+
   pathsToDelete.forEach((match) => {
     if (item.startsWith(match)) {
       delete paths[item];
@@ -34,7 +34,16 @@ Object.keys(paths).forEach((item) => {
 });
 
 delete data.tags;
-data.components.schemas.Website.properties.homepage.properties.data.properties.attributes.properties.Content.type = "object";
+
+// Fix Website schema if it exists (may not exist in newer Strapi versions)
+if (
+  data.components?.schemas?.Website?.properties?.homepage?.properties?.data?.properties?.attributes
+    ?.properties?.Content
+) {
+  data.components.schemas.Website.properties.homepage.properties.data.properties.attributes.properties.Content.type =
+    "object";
+}
+
 data.info.title = "BCPARKS";
 data.servers[0].url = "https://bcparks.api.gov.bc.ca/api";
 data.servers[0].description = "BC Parks API";
