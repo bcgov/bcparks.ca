cc73cd82e028928f6f7218963dc0444e7db10f86
Ayumi Tanaka <ayumi@oxd.com>
2026-01-12 16:52:13 -0800

    CMS-1374: Update custom api endpoint

--- src/cms/src/api/protected-area/controllers/protected-area.js
+++ src/cms/src/api/protected-area/controllers/protected-area.js
@@ -11,25 +11,34 @@ const customSearch = require("../custom/protected-area-search");
 module.exports = createCoreController(
   "api::protected-area.protected-area",
   ({ strapi }) => ({
+    // find a protected area by ORCS number
+    // overrides default findOne to use ORCS as the identifier instead of documentId
     async findOne(ctx) {
       const { id } = ctx.params;
-      // look up the protected area by the orcs
-      const entities = await strapi.documents("api::protected-area.protected-area").findMany({
-        filters: { orcs: id },
-        fields: ["id"]
-      });
-      if (entities.length === 0) {
-        return ctx.badRequest(404);
+
+      const entity = await strapi
+        .documents("api::protected-area.protected-area")
+        .findFirst({
+          filters: { orcs: id },
+          ...ctx.query,
+        });
+      if (!entity) {
+        return ctx.notFound();
       }
-      let entity = await strapi.service("api::protected-area.protected-area").findOne(entities[0].id, ctx.query);
+
       return await this.sanitizeOutput(entity, ctx);
     },
 
+    // get lightweight list of all protected areas
+    // returns only id, orcs, and protectedAreaName for client app
     async items() {
-      // custom route for light weight park details used in client app
       const entities = await strapi
-        .service("api::protected-area.protected-area")
-        .items();
+        .documents("api::protected-area.protected-area")
+        .findMany({
+          fields: ["id", "orcs", "protectedAreaName"],
+          limit: 2000,
+        });
+
       return entities.map((entity) => {
         const { id, orcs, protectedAreaName } = entity;
         return { id, orcs, protectedAreaName };
@@ -46,6 +55,6 @@ module.exports = createCoreController(
 
     async autocomplete(ctx) {
       return await customSearch.parkAutocomplete(ctx);
-    }
-  })
+    },
+  }),
 );
--- src/cms/src/api/protected-area/services/protected-area.js
+++ src/cms/src/api/protected-area/services/protected-area.js
@@ -6,16 +6,18 @@
 
 const { createCoreService } = require("@strapi/strapi").factories;
 
-module.exports = createCoreService("api::protected-area.protected-area", ({ strapi }) => ({
-  async items() {
-    const results = await strapi.db.query('api::protected-area.protected-area').findMany(
-      {
-        select: ["id", "orcs", "protectedAreaName"],
-        limit: 2000,
-        orderBy: ['protectedAreaName'],
-      }
-    );
-    return results;
-  }
-})
+module.exports = createCoreService(
+  "api::protected-area.protected-area",
+  ({ strapi }) => ({
+    async items() {
+      const results = await strapi
+        .documents("api::protected-area.protected-area")
+        .findMany({
+          fields: ["id", "orcs", "protectedAreaName"],
+          limit: 2000,
+          sort: ["protectedAreaName"],
+        });
+      return results;
+    },
+  }),
 );
