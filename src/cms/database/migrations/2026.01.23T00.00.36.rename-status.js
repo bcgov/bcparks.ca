48c233349f078023591b095e175c973b1fcdefde
Mike Olund <mike@oxd.com>
2026-01-27 14:25:00 -0800

    CMS-1422: Fix tests

--- src/cms/database/migrations/2026.01.23T00.00.36.rename-status.js
+++ src/cms/database/migrations/2026.01.23T00.00.36.rename-status.js
@@ -15,21 +15,23 @@ module.exports = {
       });
     }
 
-    await knex.raw(
-      "UPDATE protected_areas SET legal_status = 'Active' WHERE legal_status IS NULL or legal_status = '' or legal_status = 'A';"
-    );
+    if (await knex.schema.hasTable("protected_areas")) {
+      await knex.raw(
+        "UPDATE protected_areas SET legal_status = 'Active' WHERE legal_status IS NULL or legal_status = '' or legal_status = 'A';"
+      );
 
-    await knex.raw(
-      "UPDATE protected_areas SET legal_status = 'Repealed' WHERE legal_status = 'R';"
-    );
+      await knex.raw(
+        "UPDATE protected_areas SET legal_status = 'Repealed' WHERE legal_status = 'R';"
+      );
 
-    await knex.raw(
-      "UPDATE protected_areas SET legal_status = 'Closed' WHERE legal_status = 'C';"
-    );
+      await knex.raw(
+        "UPDATE protected_areas SET legal_status = 'Closed' WHERE legal_status = 'C';"
+      );
 
-    await knex.raw(
-      "UPDATE protected_areas SET legal_status = 'Transitioned' WHERE legal_status = 'T';"
-    );
+      await knex.raw(
+        "UPDATE protected_areas SET legal_status = 'Transitioned' WHERE legal_status = 'T';"
+      );
+    }
 
     // Rename and update "sites" table
     if (await knex.schema.hasColumn("sites", "status")) {
@@ -38,20 +40,22 @@ module.exports = {
       });
     }
 
-    await knex.raw(
-      "UPDATE sites SET legal_status = 'Active' WHERE legal_status IS NULL or legal_status = '' or legal_status = 'A';"
-    );
+    if (await knex.schema.hasTable("sites")) {
+      await knex.raw(
+        "UPDATE sites SET legal_status = 'Active' WHERE legal_status IS NULL or legal_status = '' or legal_status = 'A';"
+      );
 
-    await knex.raw(
-      "UPDATE sites SET legal_status = 'Repealed' WHERE legal_status = 'R';"
-    );
+      await knex.raw(
+        "UPDATE sites SET legal_status = 'Repealed' WHERE legal_status = 'R';"
+      );
 
-    await knex.raw(
-      "UPDATE sites SET legal_status = 'Closed' WHERE legal_status = 'C';"
-    );
+      await knex.raw(
+        "UPDATE sites SET legal_status = 'Closed' WHERE legal_status = 'C';"
+      );
 
-    await knex.raw(
-      "UPDATE sites SET legal_status = 'Transitioned' WHERE legal_status = 'T';"
-    );
+      await knex.raw(
+        "UPDATE sites SET legal_status = 'Transitioned' WHERE legal_status = 'T';"
+      );
+    }
   },
 };
