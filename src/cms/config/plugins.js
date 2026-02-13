f6974fe132d84bf7219948495321c306ca5870f5
Michael Olund <mike@oxd.com>
2026-02-12 20:07:26 -0800

    NOBUG: Extend redis timeout (#1701)
    * CMS-000: Extend redis timeout
    
    * Reverse line for memory cache
    
    * GitHub copilot feedback fixes

--- src/cms/config/plugins.js
+++ src/cms/config/plugins.js
@@ -64,14 +64,16 @@ module.exports = ({ env }) => {
       config: {
         provider: {
           name: env("STRAPI_CACHE_TYPE", "memory"),
+          getTimeout: env.int("REST_CACHE_GET_TIMEOUT", 5000),
           options: {
             max: 32767,
             connection: "default",
+            ttl: env.int("STRAPI_CACHE_TTL", 300000),
           },
         },
         strategy: {
           enableEtagSupport: true,
-          logs: true,
+          debug: env.bool("REST_CACHE_DEBUG", false),
           clearRelatedCache: true,
           maxAge: env.int("STRAPI_CACHE_TTL", 300000),
           hitpass: false, // don't bypass the cache for requests with a cookie or authorization header
