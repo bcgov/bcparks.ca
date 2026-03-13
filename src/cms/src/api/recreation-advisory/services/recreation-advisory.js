98a76d417989ceb3dfbf071d0f4e96348d498936
Mike Olund <mike@oxd.com>
2026-03-13 15:04:59 -0700

    CMS-1570: Remove recreation-advisory tables

--- src/cms/src/api/recreation-advisory/services/recreation-advisory.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * recreation-advisory service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::recreation-advisory.recreation-advisory');
