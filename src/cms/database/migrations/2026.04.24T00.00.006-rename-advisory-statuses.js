275e9135b2c574f26b95ad692ab79ec2cb7ef1c8
Michael Olund <mike@oxd.com>
2026-04-27 14:14:58 -0700

    CMS-1701: Change casing on HQ Review to HQ review (#1782)

--- src/cms/database/migrations/2026.04.24T00.00.006-rename-advisory-statuses.js
+++ src/cms/database/migrations/2026.04.24T00.00.006-rename-advisory-statuses.js
@@ -7,7 +7,7 @@ module.exports = {
         `UPDATE advisory_statuses SET advisory_status = 'Unpublished', code = 'UNP' WHERE code = 'INA';`
       );
       await knex.raw(
-        `UPDATE advisory_statuses SET advisory_status = 'HQ Review', code = 'HQR' WHERE code = 'ARQ';`
+        `UPDATE advisory_statuses SET advisory_status = 'HQ review', code = 'HQR' WHERE code = 'ARQ';`
       );
       await knex.raw(
         `UPDATE advisory_statuses SET advisory_status = 'Scheduled', code = 'SCH' WHERE code = 'APR';`
