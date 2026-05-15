7cc21593325da3a8a39d175aad04b2456e42340c
Mike Olund <mike@oxd.com>
2026-05-15 09:18:38 -0700

    CMS-1717: Process all "elastic batch-queue parks" tasks together

--- src/scheduler/elasticsearch/scripts/batchQueueParks.js
+++ src/scheduler/elasticsearch/scripts/batchQueueParks.js
@@ -12,57 +12,63 @@ exports.batchQueueParks = async function () {
   let queue;
 
   // get items from the queue with the action 'elastic batch-queue parks'
-  try {
-    queue = await readQueue("elastic batch-queue parks");
-  } catch (error) {
-    logger.error(
-      `batchQueueParks() failed while retrieving 'elastic batch-queue parks' tasks: ${error}`,
-    );
-    return;
-  }
-
-  for (const task of queue) {
-    const query = qs.stringify(
-      {
-        filters: {
-          documentId: { $in: task.jsonData.parkDocumentIds },
-        },
-        fields: ["orcs"],
-      },
-      { encodeValuesOnly: true },
-    );
-
-    let parks;
+  do {
     try {
-      parks = await cmsAxios.get(`/api/protected-areas?${query}`);
+      queue = await readQueue("elastic batch-queue parks");
     } catch (error) {
-      logger.error(`batchQueueParks() failed while retrieving parks: ${error}`);
+      logger.error(
+        `batchQueueParks() failed while retrieving 'elastic batch-queue parks' tasks: ${error}`,
+      );
       return;
     }
 
-    let isError = false;
-    for (const park of parks?.data?.data || []) {
+    for (const task of queue) {
+      const query = qs.stringify(
+        {
+          filters: {
+            documentId: { $in: task.jsonData.parkDocumentIds },
+          },
+          fields: ["orcs"],
+        },
+        { encodeValuesOnly: true },
+      );
+
+      let parks;
       try {
-        if (!(await existsInQueue("elastic index park", park.orcs))) {
-          const addedToQueue = await addToQueue({
-            data: { action: "elastic index park", numericData: park.orcs },
-          });
-          if (!addedToQueue) {
-            isError = true;
-          }
-        }
+        parks = await cmsAxios.get(`/api/protected-areas?${query}`);
       } catch (error) {
-        isError = true;
-        logger.error(`batchQueueParks() failed while adding park ${park.orcs} to queue: ${error}`);
-      }
-    }
-    if (!isError) {
-      try {
-        await removeFromQueue([task.documentId]);
-      } catch (error) {
-        logger.error(`batchQueueParks() failed while removing batch-queue task from queue: ${error}`);
+        logger.error(`batchQueueParks() failed while retrieving parks: ${error}`);
         return;
       }
+
+      let isError = false;
+      for (const park of parks?.data?.data || []) {
+        try {
+          if (!(await existsInQueue("elastic index park", park.orcs))) {
+            const addedToQueue = await addToQueue({
+              data: { action: "elastic index park", numericData: park.orcs },
+            });
+            if (!addedToQueue) {
+              isError = true;
+            }
+          }
+        } catch (error) {
+          isError = true;
+          logger.error(
+            `batchQueueParks() failed while adding park ${park.orcs} to queue: ${error}`,
+          );
+        }
+      }
+      if (!isError) {
+        try {
+          await removeFromQueue([task.documentId]);
+        } catch (error) {
+          logger.error(
+            `batchQueueParks() failed while removing batch-queue task from queue: ${error}`,
+          );
+          return;
+        }
+      }
     }
-  }
+  } while (queue.length > 0);
 };
