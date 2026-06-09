c7aff2ed5375b8768be940e8b862e13c420e380e
Mike Olund <mike@oxd.com>
2026-06-08 21:43:06 -0700

    CMS-1751: Set access-status scope

---
+++ src/cms/database/migrations/2026.06.08T00.00.016-access-statuses.js
@@ -0,0 +1,30 @@
+"use strict";
+
+/*
+This migration updates the scope field on existing records in the access_statuses
+table to be "BCP" or "Both" based data provided in a spreadsheet in Jira.
+*/
+
+exports.up = async function (knex) {
+  // check if the acccess_statuses table exists and if it contains a "scope" column
+  if (await knex.schema.hasTable("access_statuses")) {
+    // if the scope column doesn't exist then we'll create it now
+    if (!(await knex.schema.hasColumn("access_statuses", "scope"))) {
+      await knex.schema.table("access_statuses", (table) => {
+        table.string("scope");
+      });
+    }
+
+    await knex("access_statuses").update({ scope: "Both" });
+
+    await knex.raw(`
+        UPDATE access_statuses
+        SET scope = 'BCP'
+        WHERE access_status IN (
+          'Restricted: permit required',
+          'Open to public',
+          'Seasonal restrictions'
+        )
+    `);
+  }
+};
