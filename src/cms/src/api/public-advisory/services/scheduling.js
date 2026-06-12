48dfbf688fd1747d75f21b75d58e633fa48898e3
Ayumi Tanaka <90415568+ayumi-oxd@users.noreply.github.com>
2026-06-12 11:42:10 -0700

    NOBUG: Update unpublishedByName when advisory gets expired (#1861)
    * NOBUG: Update unpublishedByName when advisory gets expired
    
    * NOBUG: Fix code format
    
    * NOBUG: Remove removalDate

--- src/cms/src/api/public-advisory/services/scheduling.js
+++ src/cms/src/api/public-advisory/services/scheduling.js
@@ -77,9 +77,10 @@ module.exports = ({ strapi }) => ({
                 advisoryStatus: {
                   id: advisoryStatusMap["UNP"].id,
                 },
-                removalDate: new Date(),
                 modifiedByName: "system",
                 modifiedDate: new Date(),
+                unpublishedByName: "system",
+                unpublishedDate: new Date(),
               },
             })
             .catch((error) => {
@@ -128,7 +129,6 @@ module.exports = ({ strapi }) => ({
               },
               modifiedByName: "system",
               modifiedDate: new Date(),
-              removalDate: null,
             },
           })
           .then(async (advisory) => {
