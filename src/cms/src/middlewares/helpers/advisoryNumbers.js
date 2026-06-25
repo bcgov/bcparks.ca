4df8abacb7f6f677eee12ae444b2e8a893e0c7b2
Michael Olund <mike@oxd.com>
2026-06-24 17:08:56 -0700

    CMS-1807: Fix for duplicate revision numbers (#1878)

--- src/cms/src/middlewares/helpers/advisoryNumbers.js
+++ src/cms/src/middlewares/helpers/advisoryNumbers.js
@@ -11,23 +11,6 @@ async function getNextAdvisoryNumber() {
   return ++maxAdvisoryNumber;
 }
 
-async function getNextRevisionNumber(advisoryNumber) {
-  const results = await strapi
-    .documents("api::public-advisory-audit.public-advisory-audit")
-    .findMany({
-      filters: {
-        advisoryNumber,
-      },
-      sort: { revisionNumber: "DESC" },
-      limit: 1,
-      fields: ["revisionNumber"],
-    });
-  let maxRevisionNumber = results.length > 0 ? results[0].revisionNumber : 0;
-  if (!maxRevisionNumber || maxRevisionNumber < 0) maxRevisionNumber = 0;
-  return ++maxRevisionNumber;
-}
-
 module.exports = {
   getNextAdvisoryNumber,
-  getNextRevisionNumber,
 };
