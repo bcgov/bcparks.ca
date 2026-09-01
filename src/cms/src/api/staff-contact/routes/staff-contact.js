50647f3645818376cd855d226b1a55d8234031e7
Mike Olund <mike@oxd.com>
2026-09-01 15:26:57 -0700

    CMS-470: Add staff-contacts collection to Strapi

---
+++ src/cms/src/api/staff-contact/routes/staff-contact.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * staff-contact router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::staff-contact.staff-contact');
---
+++ src/cms/src/api/staff-contact/services/staff-contact.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * staff-contact service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::staff-contact.staff-contact');
