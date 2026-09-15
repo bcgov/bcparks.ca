2258c5ec971d5faa12eb85993b3e83892b674ade
Ayumi Tanaka <ayumi@oxd.com>
2026-09-15 16:19:00 -0700

    CMS-1543: Remove precedence filter

--- src/cms/src/api/protected-area/custom/protected-area-status.js
+++ src/cms/src/api/protected-area/custom/protected-area-status.js
@@ -131,6 +131,7 @@ const getPublishedPublicAdvisories = async () => {
     .findMany({
       sort: "id",
       limit: -1,
+      status: "published",
       populate: {
         protectedAreas: { fields: ["orcs"] },
         accessStatus: {
@@ -143,13 +144,6 @@ const getPublishedPublicAdvisories = async () => {
         },
         eventType: { fields: ["eventType"] },
         links: { populate: { type: { fields: ["type"] } } },
-      },
-      filters: {
-        accessStatus: {
-          precedence: {
-            $lt: 120,
-          },
-        },
       },
     });
 };
